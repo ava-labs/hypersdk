6a62332183a8d163fe0b5a92e7c91f04cd68e155
Patrick O'Grady <prohb125@gmail.com>
2023-02-21 20:43:26 -0800

    update transfer to have assets

--- examples/tokenvm/actions/transfer.go
+++ examples/tokenvm/actions/transfer.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ava-labs/hypersdk/consts"
 	"github.com/ava-labs/hypersdk/crypto"
 	"github.com/ava-labs/hypersdk/examples/tokenvm/auth"
-	"github.com/ava-labs/hypersdk/examples/tokenvm/genesis"
 	"github.com/ava-labs/hypersdk/examples/tokenvm/storage"
 	"github.com/ava-labs/hypersdk/utils"
 )
@@ -23,14 +22,17 @@ type Transfer struct {
 	// To is the recipient of the [Value].
 	To crypto.PublicKey `json:"to"`
 
+	// Asset to transfer to [To].
+	Asset ids.ID
+
 	// Amount are transferred to [To].
 	Value uint64 `json:"value"`
 }
 
 func (t *Transfer) StateKeys(rauth chain.Auth) [][]byte {
 	return [][]byte{
-		storage.PrefixBalanceKey(auth.GetActor(rauth)),
-		storage.PrefixBalanceKey(t.To),
+		storage.PrefixBalanceKey(auth.GetActor(rauth), t.Asset),
+		storage.PrefixBalanceKey(t.To, t.Asset),
 	}
 }
 
@@ -47,47 +49,36 @@ func (t *Transfer) Execute(
 	if t.Value == 0 {
 		return &chain.Result{Success: false, Units: unitsUsed, Output: OutputValueZero}, nil
 	}
-	stateLockup, err := genesis.GetStateLockup(r)
-	if err != nil {
+	if err := storage.SubBalance(ctx, db, actor, t.Asset, t.Value); err != nil {
 		return &chain.Result{Success: false, Units: unitsUsed, Output: utils.ErrBytes(err)}, nil
 	}
-	if err := storage.SubUnlockedBalance(ctx, db, actor, t.Value); err != nil {
-		return &chain.Result{Success: false, Units: unitsUsed, Output: utils.ErrBytes(err)}, nil
-	}
-	alreadyExists, err := storage.AddUnlockedBalance(ctx, db, t.To, t.Value, false)
-	if err != nil {
-		return &chain.Result{Success: false, Units: unitsUsed, Output: utils.ErrBytes(err)}, nil
-	}
-	if alreadyExists {
-		return &chain.Result{Success: true, Units: unitsUsed}, nil
-	}
-	// new accounts must lock funds for balance and perms
-	if err := storage.LockBalance(ctx, db, t.To, stateLockup*2); err != nil {
-		return &chain.Result{Success: false, Units: unitsUsed, Output: utils.ErrBytes(err)}, nil
-	}
-	// new accounts have default perms
-	if err := storage.SetPermissions(ctx, db, t.To, t.To, consts.MaxUint8, consts.MaxUint8); err != nil {
+	if err := storage.AddBalance(ctx, db, t.To, t.Asset, t.Value); err != nil {
 		return &chain.Result{Success: false, Units: unitsUsed, Output: utils.ErrBytes(err)}, nil
 	}
 	return &chain.Result{Success: true, Units: unitsUsed}, nil
 }
 
 func (*Transfer) MaxUnits(chain.Rules) uint64 {
-	return crypto.PublicKeyLen + consts.Uint64Len
+	// We use size as the price of this transaction but we could just as easily
+	// use any other calculation.
+	return crypto.PublicKeyLen + consts.IDLen + consts.Uint64Len
 }
 
 func (t *Transfer) Marshal(p *codec.Packer) {
 	p.PackPublicKey(t.To)
+	p.PackID(t.Asset)
 	p.PackUint64(t.Value)
 }
 
 func UnmarshalTransfer(p *codec.Packer) (chain.Action, error) {
 	var transfer Transfer
 	p.UnpackPublicKey(&transfer.To)
+	p.UnpackID(false, &transfer.Asset) // empty ID is the native asset
 	transfer.Value = p.UnpackUint64(true)
 	return &transfer, p.Err()
 }
 
 func (*Transfer) ValidRange(chain.Rules) (int64, int64) {
+	// Returning -1, -1 means that the action is always valid.
 	return -1, -1
 }
