d7b133579f535dfeec0b6cc3679ad8d438865591
Sam Liokumovich <65994425+samliok@users.noreply.github.com>
2023-02-26 17:58:47 -0800

    Update EMap to use Heap Package (#55)
    * remove binary heap
    
    * tests + lint
    
    * remove bucketHeap struct
    
    * set bh to 120

--- emap/emap.go
+++ emap/emap.go
@@ -4,69 +4,16 @@
 package emap
 
 import (
-	"container/heap"
-	"fmt"
 	"sync"
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/set"
+	"github.com/ava-labs/hypersdk/heap"
 )
 
 type bucket struct {
 	t     int64    // Timestamp
 	items []ids.ID // Array of AvalancheGo ids
-}
-
-// A bucketHeap implements the heap interface to construct a priority queue.
-// The heap structure is maintained by the less function which
-// sorts the heap in ascending order by a buckets timestamp [t].
-type bucketHeap struct {
-	buckets []*bucket
-}
-
-// Len returns the length of the buckets array in bh.
-func (bh *bucketHeap) Len() int {
-	return len(bh.buckets)
-}
-
-// Less returns whether i's timestamp is less than j's.
-func (bh *bucketHeap) Less(i, j int) bool {
-	return bh.buckets[i].t < bh.buckets[j].t
-}
-
-// Swap swaps the i and jth bucket in bh.
-func (bh *bucketHeap) Swap(i, j int) {
-	bh.buckets[i], bh.buckets[j] = bh.buckets[j], bh.buckets[i]
-}
-
-// Pushes a *bucket to the end of bh.buckets.
-// Panics if the parameter passed in is not a *bucket.
-func (bh *bucketHeap) Push(x interface{}) {
-	entry, ok := x.(*bucket)
-	if !ok {
-		panic(fmt.Errorf("unexpected %T, expected *bucket", x))
-	}
-	bh.buckets = append(bh.buckets, entry)
-}
-
-// Pop returns the highest priority bucket in bh and deletes it
-// from the buckets array. It is called by heap.Pop() which sets the highest
-// priority bucket to the last slot before calling Pop.
-func (bh *bucketHeap) Pop() interface{} {
-	n := len(bh.buckets)
-	item := bh.buckets[n-1]
-	bh.buckets[n-1] = nil // avoid memory leak
-	bh.buckets = bh.buckets[0 : n-1]
-	return item
-}
-
-// Peek returns the first bucket in bh. If bh has no
-// buckets, Peek returns nil.
-func (bh *bucketHeap) Peek() *bucket {
-	if len(bh.buckets) == 0 {
-		return nil
-	}
-	return bh.buckets[0]
 }
 
 // Item defines an interface accepted by EMap
@@ -81,7 +28,7 @@ type Item interface {
 type EMap[T Item] struct {
 	mu sync.RWMutex
 
-	bh    *bucketHeap
+	bh    *heap.Heap[*bucket, int64]
 	seen  set.Set[ids.ID]   // Stores a set of unique tx ids
 	times map[int64]*bucket // Uses timestamp as keys to map to buckets of ids.
 }
@@ -91,9 +38,7 @@ func NewEMap[T Item]() *EMap[T] {
 	return &EMap[T]{
 		seen:  set.Set[ids.ID]{},
 		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
+		bh:    heap.New[*bucket, int64](120, true),
 	}
 }
 
@@ -135,7 +80,12 @@ func (e *EMap[T]) add(id ids.ID, t int64) {
 		items: []ids.ID{id},
 	}
 	e.times[t] = b
-	heap.Push(e.bh, b)
+	e.bh.Push(&heap.Entry[*bucket, int64]{
+		ID:    id,
+		Val:   t,
+		Item:  b,
+		Index: e.bh.Len(),
+	})
 }
 
 // SetMin removes all buckets with a lower
@@ -146,17 +96,17 @@ func (e *EMap[T]) SetMin(t int64) []ids.ID {
 
 	evicted := []ids.ID{}
 	for {
-		b := e.bh.Peek()
-		if b == nil || b.t >= t {
+		b := e.bh.First()
+		if b == nil || b.Val >= t {
 			break
 		}
-		heap.Pop(e.bh)
-		for _, id := range b.items {
+		e.bh.Pop()
+		for _, id := range b.Item.items {
 			e.seen.Remove(id)
 			evicted = append(evicted, id)
 		}
 		// Delete from times map
-		delete(e.times, b.t)
+		delete(e.times, b.Val)
 	}
 	return evicted
 }
--- emap/emap_test.go
+++ emap/emap_test.go
@@ -3,11 +3,11 @@
 package emap
 
 import (
-	"container/heap"
 	"testing"
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/set"
+	"github.com/ava-labs/hypersdk/heap"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,180 +19,13 @@ type TestTx struct {
 func (tx *TestTx) ID() ids.ID    { return tx.id }
 func (tx *TestTx) Expiry() int64 { return tx.t }
 
-func TestBucketHeapLen(t *testing.T) {
-	require := require.New(t)
-	// Create new bucket
-	bh := &bucketHeap{
-		buckets: []*bucket{
-			{
-				t:     1,
-				items: []ids.ID{},
-			},
-		},
-	}
-	require.Equal(bh.Len(), 1, "Length was not equal to one.")
-}
-
-func TestBucketHeapLess(t *testing.T) {
-	require := require.New(t)
-	// Create new bucket
-	bh := &bucketHeap{
-		buckets: []*bucket{
-			{
-				t:     1,
-				items: []ids.ID{},
-			},
-			{
-				t:     2,
-				items: []ids.ID{},
-			},
-		},
-	}
-
-	require.True(bh.Less(0, 1), "Less function returned incorrectly.")
-	require.False(bh.Less(1, 0), "Less function returned incorrectly.")
-}
-
-func TestBucketHeapSwap(t *testing.T) {
-	require := require.New(t)
-	firstBucket := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	secondBucket := bucket{
-		t:     2,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{
-			&firstBucket,
-			&secondBucket,
-		},
-	}
-	// Perform Swap
-	bh.Swap(0, 1)
-	require.Equal(bh.buckets[0], &secondBucket, "Buckets swapped incorrectly.")
-	require.Equal(bh.buckets[1], &firstBucket, "Buckets swapped incorrectly.")
-}
-
-func TestBucketHeapPush(t *testing.T) {
-	require := require.New(t)
-
-	b := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{},
-	}
-	// Push
-	heap.Push(bh, &b)
-	require.Equal(bh.buckets[0], &b, "Buckets pushed incorrectly")
-}
-
-func TestBucketHeapPushPanics(t *testing.T) {
-	require := require.New(t)
-
-	b := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{},
-	}
-	// Push
-	f := func() { heap.Push(bh, b) }
-	require.Panics(f, "Did not panic after incorrect interface push.")
-}
-
-func TestBucketHeapPop(t *testing.T) {
-	require := require.New(t)
-	firstBucket := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	secondBucket := bucket{
-		t:     2,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{
-			&firstBucket,
-			&secondBucket,
-		},
-	}
-	popped := heap.Pop(bh)
-	require.Equal(bh.Len(), 1, "Pop did not adjust length correctly.")
-	require.Equal(&firstBucket, popped, "Pop did not return correct bucket.")
-}
-
-func TestBucketHeapPeek(t *testing.T) {
-	require := require.New(t)
-	firstBucket := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	secondBucket := bucket{
-		t:     2,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{
-			&firstBucket,
-			&secondBucket,
-		},
-	}
-	peeked := bh.Peek()
-	require.Equal(bh.Len(), 2, "Peek changed the length of buckets.")
-	require.Equal(&firstBucket, peeked, "Peek did not return correct bucket.")
-}
-
-func TestBucketHeapPeekEmpty(t *testing.T) {
-	require := require.New(t)
-	bh := &bucketHeap{
-		buckets: []*bucket{},
-	}
-	peeked := bh.Peek()
-	require.Nil(peeked, "Peek was not nil.")
-}
-
-func TestBucketHeapPushPop(t *testing.T) {
-	// Pushes buckets in unordered fashion then checks pop returns correctly
-	require := require.New(t)
-	firstBucket := bucket{
-		t:     1,
-		items: []ids.ID{},
-	}
-	secondBucket := bucket{
-		t:     2,
-		items: []ids.ID{},
-	}
-	thirdBucket := bucket{
-		t:     3,
-		items: []ids.ID{},
-	}
-	bh := &bucketHeap{
-		buckets: []*bucket{},
-	}
-	heap.Push(bh, &secondBucket)
-	heap.Push(bh, &firstBucket)
-	heap.Push(bh, &thirdBucket)
-
-	require.Equal(bh.Len(), 3, "BH not pushed correctly.")
-	require.Equal(&firstBucket, heap.Pop(bh), "Pop returned incorrect value")
-	require.Equal(&secondBucket, heap.Pop(bh), "Pop returned incorrect value")
-	require.Equal(&thirdBucket, heap.Pop(bh), "Pop returned incorrect value")
-}
-
 func TestEmapNew(t *testing.T) {
 	require := require.New(t)
 	e := NewEMap[*TestTx]()
 	emptyE := &EMap[*TestTx]{
 		seen:  set.Set[ids.ID]{},
 		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
+		bh:    heap.New[*bucket, int64](0, true),
 	}
 	require.Equal(emptyE.seen, e.seen, "Emap did not return an empty emap struct.")
 	require.Equal(emptyE.times, e.times, "Emap did not return an empty emap struct.")
@@ -201,13 +34,7 @@ func TestEmapNew(t *testing.T) {
 
 func TestEmapAddIDGenesis(t *testing.T) {
 	require := require.New(t)
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
 	var timestamp int64 = 0
 	id := ids.GenerateTestID()
 	tx := &TestTx{
@@ -216,24 +43,21 @@ func TestEmapAddIDGenesis(t *testing.T) {
 	}
 	txs := []*TestTx{tx}
 	e.Add(txs)
-	// seen was updated
+	// Seen was updated
 	_, okSeen := e.seen[tx.ID()]
 	require.False(okSeen, "Genesis timestamp was incorrectly added")
-	// get bucket
+	// Get bucket
 	_, okBucket := e.times[0]
 	require.False(okBucket, "Genesis timestamp found in bucket")
+
+	// Check bucket heap was not updated
+	require.False(e.bh.Has(id), "BH has ID")
 }
 
 func TestEmapAddIDNewBucket(t *testing.T) {
 	require := require.New(t)
 
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
 	var timestamp int64 = 1
 
 	id := ids.GenerateTestID()
@@ -251,18 +75,15 @@ func TestEmapAddIDNewBucket(t *testing.T) {
 	b, okBucket := e.times[timestamp]
 	require.True(okBucket, "Could not find time bucket")
 	require.Equal(len(b.items), 1, "Bucket length is incorrect")
+
+	// Check bucket heap was updated
+	require.True(e.bh.Has(id), "BH does not have ID")
 }
 
 func TestEmapAddIDExists(t *testing.T) {
 	require := require.New(t)
+	e := NewEMap[*TestTx]()
 
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
 	var timestamp int64 = 1
 	id := ids.GenerateTestID()
 	tx1 := &TestTx{
@@ -273,6 +94,12 @@ func TestEmapAddIDExists(t *testing.T) {
 	e.Add(txs)
 	_, okSeen := e.seen[tx1.ID()]
 	require.True(okSeen, "Could not find id in seen map")
+
+	entry, ok := e.bh.Get(id)
+	// Check bh
+	require.True(ok, "BH does not have ID")
+	require.Equal(entry.Val, tx1.t, "BH incorrectly set val.")
+
 	tx2 := &TestTx{
 		t:  timestamp * 3,
 		id: id,
@@ -284,18 +111,18 @@ func TestEmapAddIDExists(t *testing.T) {
 	b, okBucket := e.times[timestamp]
 	require.True(okBucket, "Could not find time bucket")
 	require.Equal(len(b.items), 1, "Bucket length is incorrect")
+
+	entry, ok = e.bh.Get(id)
+	// Check bh
+	require.True(ok, "BH does not have ID")
+	require.Equal(entry.Val, tx1.t, "BH incorrectly updated.")
 }
 
 func TestEmapAddIDBucketExists(t *testing.T) {
 	require := require.New(t)
 
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
+
 	var timestamp int64 = 1
 
 	id1 := ids.GenerateTestID()
@@ -321,18 +148,19 @@ func TestEmapAddIDBucketExists(t *testing.T) {
 	// get bucket
 	b, okBucket := e.times[timestamp]
 	require.True(okBucket, "Could not find time bucket")
+	require.Equal(1, e.bh.Len(), "Number of buckets is incorrect.")
 	require.Equal(len(b.items), 2, "Bucket length is incorrect")
+
+	entry, ok := e.bh.Get(id1)
+	// Check bh
+	require.True(ok, "BH does not have ID")
+	require.Equal([]ids.ID{id1, id2}, entry.Item.items, "BH incorrectly updated.")
 }
 
 func TestEmapAny(t *testing.T) {
 	require := require.New(t)
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
+
 	var timestamp int64 = 1
 	id := ids.GenerateTestID()
 	tx := &TestTx{
@@ -348,25 +176,23 @@ func TestSetMin(t *testing.T) {
 	// Sets min to timestamp 3. Requires all buckets
 	// to be removed with timestamp t less than min
 	require := require.New(t)
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
+
 	pushedIds := []ids.ID{}
 	startT := int64(1)
 	minT := int64(3)
 	endT := int64(6)
-
 	for n := startT; n < endT; n++ {
 		id := ids.GenerateTestID()
 		e.add(id, n)
 		_, okSeen := e.seen[id]
 		pushedIds = append(pushedIds, id)
 		require.True(okSeen, "Id not set in seen list")
+		require.True(e.bh.Has(id), "ID not added to bh")
 	}
+	// Check bh
+	require.Equal(len(pushedIds), e.bh.Len(), "BH not added to properly")
+
 	// Set min to 3
 	removedIds := e.SetMin(minT)
 	// Check removed_ids = min_ids
@@ -385,20 +211,17 @@ func TestSetMin(t *testing.T) {
 	for t := startT; t < minT; t++ {
 		_, ok := e.times[t]
 		require.False(ok, "Bucket not removed from bh")
+		require.False(e.bh.Has(pushedIds[t-1]), "Bucket not removed from bh")
 	}
+	require.Equal(int(endT-minT), e.bh.Len(), "Items not removed from bh")
 }
 
 func TestSetMinPopsAll(t *testing.T) {
 	// Sets min to be higher than all timestamps. Should remove all
 	// buckets and ids in EMap.
 	require := require.New(t)
-	e := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	e := NewEMap[*TestTx]()
+
 	pushedIds := []ids.ID{}
 	startT := int64(1)
 	endT := int64(6)
@@ -415,12 +238,7 @@ func TestSetMinPopsAll(t *testing.T) {
 	// Check removed_ids = min_ids
 	require.Equal(pushedIds, removedIds, "Not all ids were returned")
 	// Check EMap is empty
-	emptyEmap := &EMap[*TestTx]{
-		seen:  set.Set[ids.ID]{},
-		times: make(map[int64]*bucket),
-		bh: &bucketHeap{
-			buckets: []*bucket{},
-		},
-	}
+	emptyEmap := NewEMap[*TestTx]()
+
 	require.Equal(emptyEmap, e, "EMap not empty")
 }
