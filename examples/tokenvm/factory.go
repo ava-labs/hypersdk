9263a5cb5025d1ef3ae859ad9d1cf3f636cde5ab
Patrick O'Grady <prohb125@gmail.com>
2023-03-01 17:29:24 -0800

    update factory

--- examples/tokenvm/factory.go
+++ examples/tokenvm/factory.go
@@ -4,7 +4,7 @@
 package vm
 
 import (
-	"github.com/ava-labs/avalanchego/snow"
+	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms"
 
 	"github.com/ava-labs/hypersdk/examples/tokenvm/controller"
@@ -14,6 +14,6 @@ var _ vms.Factory = &Factory{}
 
 type Factory struct{}
 
-func (*Factory) New(*snow.Context) (interface{}, error) {
+func (*Factory) New(logging.Logger) (interface{}, error) {
 	return controller.New(), nil
 }
