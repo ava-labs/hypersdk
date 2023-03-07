761129e98d6641a2c12c272146d05270b7c1cc1d
Sam Liokumovich <samliok@stanford.edu>
2023-03-07 00:17:43 -0800

    packer tests boilerplate

---
+++ codec/packer_test.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package codec
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNNewReader(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+func TestNNewWriter(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+func TestPackerPublicKey(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+
+func TestPackerSignature(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+
+func TestPackerID(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+func TestPackerWindow(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+func TestPackerUnpackBytes(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
+func TestPackerRequiredUnpack(t *testing.T) {
+	require := require.New(t)
+	require.True(true, "not true")
+}
