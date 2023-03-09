4f4fe688aca8bd83de4fea59aa75ce028d18781b
Patrick O'Grady <prohb125@gmail.com>
2023-03-08 16:42:22 -0800

    remove old options

--- examples/tokenvm/cmd/token-cli/cmd/root.go
+++ examples/tokenvm/cmd/token-cli/cmd/root.go
@@ -6,22 +6,22 @@ package cmd
 
 import (
 	"os"
+	"path/filepath"
 	"time"
 
+	"github.com/ava-labs/avalanchego/database"
+	"github.com/ava-labs/hypersdk/pebble"
 	"github.com/spf13/cobra"
 )
 
 const (
 	requestTimeout = 30 * time.Second
 	fsModeWrite    = 0o600
+	databaseFolder = ".token-cli"
 )
 
 var (
-	privateKeyFile string
-	uri            string
-	verbose        bool
-	workDir        string
-
+	db      database.Database
 	rootCmd = &cobra.Command{
 		Use:        "token-cli",
 		Short:      "TokenVM CLI",
@@ -34,7 +34,11 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	workDir = p
+	dbPath := filepath.Join(p, databaseFolder)
+	db, err = pebble.New(dbPath, pebble.NewDefaultConfig())
+	if err != nil {
+		panic(err)
+	}
 
 	cobra.EnablePrefixMatching = true
 	rootCmd.AddCommand(
@@ -67,31 +71,8 @@ func init() {
 		closeOrderCmd,
 
 		// spamCmd,
-	)
 
-	rootCmd.PersistentFlags().StringVar(
-		&privateKeyFile,
-		"private-key-file",
-		// We use the default demo key to make it easier to run the demo locally.
-		// If you want to use your own key, we recommend storing it at
-		// ".token-cli.pk".
-		"demo.pk",
-		"private key file path",
-	)
-	rootCmd.PersistentFlags().StringVar(
-		&uri,
-		"endpoint",
-		// We use the default local endpoint so we don't need to supply it in the
-		// demo. If you change any of the contents of the genesis file, this will
-		// change.
-		"http://localhost:9650/ext/bc/EG9kgVnmBkoeJRD2mXnMrGoNLe9Z3HpFtUMJnCVkpxnDMxALV",
-		"RPC endpoint for VM",
-	)
-	rootCmd.PersistentFlags().BoolVar(
-		&verbose,
-		"verbose",
-		false,
-		"Print verbose information about operations",
+		// resetCmd, (deletes DB)
 	)
 }
 
