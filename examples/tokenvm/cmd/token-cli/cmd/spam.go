343cd782c6f6868852ea259dab4b6d0a9cfd697f
Patrick O'Grady <prohb125@gmail.com>
2023-03-20 17:03:57 -0700

    get basic distribution

--- examples/tokenvm/cmd/token-cli/cmd/spam.go
+++ examples/tokenvm/cmd/token-cli/cmd/spam.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/hypersdk/crypto"
+	"github.com/ava-labs/hypersdk/examples/tokenvm/actions"
+	"github.com/ava-labs/hypersdk/examples/tokenvm/auth"
 	"github.com/ava-labs/hypersdk/examples/tokenvm/client"
 	"github.com/ava-labs/hypersdk/examples/tokenvm/utils"
 	hutils "github.com/ava-labs/hypersdk/utils"
@@ -20,6 +23,8 @@ var spamCmd = &cobra.Command{
 var runSpamCmd = &cobra.Command{
 	Use: "run",
 	RunE: func(*cobra.Command, []string) error {
+		ctx := context.Background()
+
 		// Select chain
 		_, uris, err := promptChain("select chainID", nil)
 		if err != nil {
@@ -36,12 +41,14 @@ var runSpamCmd = &cobra.Command{
 		}
 		cli := client.New(uris[0])
 		hutils.Outf("{{cyan}}stored keys:{{/}} %d\n", len(keys))
+		balances := make([]uint64, len(keys))
 		for i := 0; i < len(keys); i++ {
 			address := utils.Address(keys[i].PublicKey())
-			balance, err := cli.Balance(context.TODO(), address, ids.Empty)
+			balance, err := cli.Balance(ctx, address, ids.Empty)
 			if err != nil {
 				return err
 			}
+			balances[i] = balance
 			hutils.Outf(
 				"%d) {{cyan}}address:{{/}} %s {{cyan}}balance:{{/}} %s TKN\n",
 				i,
@@ -54,11 +61,45 @@ var runSpamCmd = &cobra.Command{
 			return err
 		}
 		key := keys[keyIndex]
+		balance := balances[keyIndex]
+		factory := auth.NewED25519Factory(key)
 
 		// Distribute funds
-		accounts, err := promptInt("number of accounts")
+		numAccounts, err := promptInt("number of accounts")
 		if err != nil {
 			return err
+		}
+		distAmount := balance / uint64(numAccounts+1)
+		hutils.Outf("{{yellow}}distributing funds to each account:{{/}} %s %s\n", valueString(ids.Empty, distAmount), assetString(ids.Empty))
+		accounts := make([]crypto.PrivateKey, numAccounts)
+		for i := 0; i < numAccounts; i++ {
+			// Create account
+			pk, err := crypto.GeneratePrivateKey()
+			if err != nil {
+				return err
+			}
+			accounts[i] = pk
+
+			// Send funds
+			submit, tx, _, err := cli.GenerateTransaction(ctx, nil, &actions.Transfer{
+				To:    pk.PublicKey(),
+				Asset: ids.Empty,
+				Value: distAmount,
+			}, factory)
+			if err != nil {
+				return err
+			}
+			if err := submit(ctx); err != nil {
+				return err
+			}
+			success, err := cli.WaitForTransaction(ctx, tx.ID())
+			if err != nil {
+				return err
+			}
+			if !success {
+				// Should never happen
+				return ErrTxFailed
+			}
 		}
 
 		// Kickoff txs
