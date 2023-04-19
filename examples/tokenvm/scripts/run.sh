52ecdb48db5b458fae60f17f2cb8ed1a61c40f3b
Patrick O'Grady <prohb125@gmail.com>
2023-04-18 22:08:26 -0700

    use scripts/stop.sh in comment

--- examples/tokenvm/scripts/run.sh
+++ examples/tokenvm/scripts/run.sh
@@ -205,13 +205,13 @@ function cleanup() {
     echo ""
     echo "use the following command to terminate:"
     echo ""
-    echo "killall avalanche-network-runner"
+    echo "./scripts/stop.sh"
     echo ""
     exit
   fi
 
   echo "avalanche-network-runner shutting down..."
-  killall avalanche-network-runner
+  ./scripts/stop.sh
 }
 trap cleanup EXIT
 
