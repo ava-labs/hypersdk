6e771f881ae4441ebea989ad8435e380b2458f7e
felipemadero <felipe.madero@gmail.com>
2023-04-19 02:18:30 -0300

    Update to ANR@1.4.1 (#149)
    * use anr 1.4.0
    
    * check e2e fix
    
    * nit
    
    * try e2e fix
    
    * empty
    
    * empty
    
    * empty
    
    * fix add node tracked subnets
    
    * lint
    
    * depend on anr main
    
    * fix version in e2e
    
    * update to v1.4.1
    
    ---------
    
    Co-authored-by: Patrick O'Grady <prohb125@gmail.com>

--- examples/tokenvm/tests/e2e/e2e_test.go
+++ examples/tokenvm/tests/e2e/e2e_test.go
@@ -12,6 +12,7 @@ import (
 
 	runner_sdk "github.com/ava-labs/avalanche-network-runner/client"
 	"github.com/ava-labs/avalanche-network-runner/rpcpb"
+	"github.com/ava-labs/avalanchego/config"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
@@ -61,6 +62,8 @@ var (
 
 	blockchainIDA string
 	blockchainIDB string
+
+	trackSubnetsOpt runner_sdk.OpOption
 )
 
 func init() {
@@ -216,59 +219,55 @@ var _ = ginkgo.BeforeSuite(func() {
 
 	// Create 2 subnets
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Minute)
-	sresp, err := anrCli.CreateSpecificBlockchains(
+	sresp, err := anrCli.CreateBlockchains(
 		ctx,
-		execPath,
 		[]*rpcpb.BlockchainSpec{
 			{
-				VmName:       consts.Name,
-				Genesis:      vmGenesisPath,
-				ChainConfig:  vmConfigPath,
-				SubnetConfig: subnetConfigPath,
-				Participants: subnetA,
+				VmName:      consts.Name,
+				Genesis:     vmGenesisPath,
+				ChainConfig: vmConfigPath,
+				SubnetSpec: &rpcpb.SubnetSpec{
+					SubnetConfig: subnetConfigPath,
+					Participants: subnetA,
+				},
 			},
 			{
-				VmName:       consts.Name,
-				Genesis:      vmGenesisPath,
-				ChainConfig:  vmConfigPath,
-				SubnetConfig: subnetConfigPath,
-				Participants: subnetB,
+				VmName:      consts.Name,
+				Genesis:     vmGenesisPath,
+				ChainConfig: vmConfigPath,
+				SubnetSpec: &rpcpb.SubnetSpec{
+					SubnetConfig: subnetConfigPath,
+					Participants: subnetB,
+				},
 			},
 		},
 	)
 	cancel()
 	gomega.Expect(err).Should(gomega.BeNil())
 
-	blockchainIDA = sresp.Chains[0]
+	blockchainIDA = sresp.ChainIds[0]
+	subnetIDA := sresp.ClusterInfo.CustomChains[blockchainIDA].SubnetId
 	hutils.Outf(
 		"{{green}}successfully added chain:{{/}} %s {{green}}subnet:{{/}} %s {{green}}participants:{{/}} %+v\n",
 		blockchainIDA,
-		sresp.ClusterInfo.CustomChains[blockchainIDA].SubnetId,
+		subnetIDA,
 		subnetA,
 	)
-	blockchainIDB = sresp.Chains[1]
+
+	blockchainIDB = sresp.ChainIds[1]
+	subnetIDB := sresp.ClusterInfo.CustomChains[blockchainIDB].SubnetId
 	hutils.Outf(
 		"{{green}}successfully added chain:{{/}} %s {{green}}subnet:{{/}} %s {{green}}participants:{{/}} %+v\n",
 		blockchainIDB,
-		sresp.ClusterInfo.CustomChains[blockchainIDB].SubnetId,
+		subnetIDB,
 		subnetB,
 	)
 
-	// TODO: network runner health should imply custom VM healthiness
-	// or provide a separate API for custom VM healthiness
-	// "start" is async, so wait some time for cluster health
-	hutils.Outf("\n{{magenta}}waiting for all vms to report healthy...{{/}}: %s\n", consts.ID)
-	for {
-		v, err := anrCli.Health(context.Background())
-		hutils.Outf("\n{{yellow}}health result{{/}}: %+v %s\n", v, err)
-		if err != nil {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		// TODO: clean this up
-		gomega.Expect(err).Should(gomega.BeNil())
-		break
-	}
+	trackSubnetsOpt = runner_sdk.WithGlobalNodeConfig(fmt.Sprintf("{\"%s\":\"%s,%s\"}",
+		config.TrackSubnetsKey,
+		subnetIDA,
+		subnetIDB,
+	))
 
 	gomega.Expect(blockchainIDA).Should(gomega.Not(gomega.BeEmpty()))
 	gomega.Expect(blockchainIDB).Should(gomega.Not(gomega.BeEmpty()))
@@ -1262,6 +1261,7 @@ var _ = ginkgo.Describe("[Test]", func() {
 			context.Background(),
 			"bootstrap",
 			execPath,
+			trackSubnetsOpt,
 		)
 		gomega.Expect(err).To(gomega.BeNil())
 		awaitHealthy(anrCli)
@@ -1357,6 +1357,7 @@ var _ = ginkgo.Describe("[Test]", func() {
 			context.Background(),
 			"sync",
 			execPath,
+			trackSubnetsOpt,
 		)
 		gomega.Expect(err).To(gomega.BeNil())
 
@@ -1472,6 +1473,7 @@ var _ = ginkgo.Describe("[Test]", func() {
 			context.Background(),
 			"sync_concurrent",
 			execPath,
+			trackSubnetsOpt,
 		)
 		gomega.Expect(err).To(gomega.BeNil())
 		awaitHealthy(anrCli)
