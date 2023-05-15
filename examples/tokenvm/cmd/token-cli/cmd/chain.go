9a1c7f86e9f3c9b5c126ea097f9ef24665b43fff
Patrick O'Grady <prohb125@gmail.com>
2023-05-14 17:51:17 -0700

    build passing

--- examples/tokenvm/cmd/token-cli/cmd/chain.go
+++ examples/tokenvm/cmd/token-cli/cmd/chain.go
@@ -259,9 +259,13 @@ var watchChainCmd = &cobra.Command{
 			tpsWindow         = window.Window{}
 		)
 		for ctx.Err() == nil {
-			blk, results, err := scli.ListenBlock(ctx, parser)
+			blk, txBlks, results, err := scli.ListenBlock(ctx, parser)
 			if err != nil {
 				return err
+			}
+			totalTxs := 0
+			for _, txBlk := range txBlks {
+				totalTxs += len(txBlk.Txs)
 			}
 			now := time.Now()
 			if start.IsZero() {
@@ -274,13 +278,14 @@ var watchChainCmd = &cobra.Command{
 					return err
 				}
 				tpsWindow = newWindow
-				window.Update(&tpsWindow, window.WindowSliceSize-hconsts.Uint64Len, uint64(len(blk.Txs)))
+				window.Update(&tpsWindow, window.WindowSliceSize-hconsts.Uint64Len, uint64(totalTxs))
 				runningDuration := time.Since(start)
 				tpsDivisor := math.Min(window.WindowSize, runningDuration.Seconds())
 				utils.Outf(
-					"{{green}}height:{{/}}%d {{green}}txs:{{/}}%d {{green}}units:{{/}}%d {{green}}root:{{/}}%s {{green}}TPS:{{/}}%.2f {{green}}split:{{/}}%dms\n", //nolint:lll
+					"{{green}}height:{{/}}%d {{green}}tx blks:{{/}}%d {{green}}txs:{{/}}%d {{green}}units:{{/}}%d {{green}}root:{{/}}%s {{green}}TPS:{{/}}%.2f {{green}}split:{{/}}%dms\n", //nolint:lll
 					blk.Hght,
-					len(blk.Txs),
+					len(txBlks),
+					totalTxs,
 					blk.UnitsConsumed,
 					blk.StateRoot,
 					float64(window.Sum(tpsWindow))/tpsDivisor,
@@ -288,9 +293,10 @@ var watchChainCmd = &cobra.Command{
 				)
 			} else {
 				utils.Outf(
-					"{{green}}height:{{/}}%d {{green}}txs:{{/}}%d {{green}}units:{{/}}%d {{green}}root:{{/}}%s\n", //nolint:lll
+					"{{green}}height:{{/}}%d {{green}}tx blks:{{/}}%d {{green}}txs:{{/}}%d {{green}}units:{{/}}%d {{green}}root:{{/}}%s\n", //nolint:lll
 					blk.Hght,
-					len(blk.Txs),
+					len(txBlks),
+					totalTxs,
 					blk.UnitsConsumed,
 					blk.StateRoot,
 				)
@@ -301,127 +307,129 @@ var watchChainCmd = &cobra.Command{
 			if hideTxs {
 				continue
 			}
-			for i, tx := range blk.Txs {
-				result := results[i]
-				summaryStr := string(result.Output)
-				actor := auth.GetActor(tx.Auth)
-				status := "⚠️"
-				if result.Success {
-					status = "✅"
-					switch action := tx.Action.(type) {
-					case *actions.CreateAsset:
-						summaryStr = fmt.Sprintf("assetID: %s metadata:%s", tx.ID(), string(action.Metadata))
-					case *actions.MintAsset:
-						amountStr := strconv.FormatUint(action.Value, 10)
-						assetStr := action.Asset.String()
-						if action.Asset == ids.Empty {
-							amountStr = utils.FormatBalance(action.Value)
-							assetStr = consts.Symbol
-						}
-						summaryStr = fmt.Sprintf("%s %s -> %s", amountStr, assetStr, tutils.Address(action.To))
-					case *actions.BurnAsset:
-						summaryStr = fmt.Sprintf("%d %s -> 🔥", action.Value, action.Asset)
-					case *actions.ModifyAsset:
-						summaryStr = fmt.Sprintf(
-							"assetID: %s metadata:%s owner:%s",
-							action.Asset, string(action.Metadata), tutils.Address(action.Owner),
-						)
+			for _, txBlk := range txBlks {
+				for i, tx := range txBlk.Txs {
+					result := results[i]
+					summaryStr := string(result.Output)
+					actor := auth.GetActor(tx.Auth)
+					status := "⚠️"
+					if result.Success {
+						status = "✅"
+						switch action := tx.Action.(type) {
+						case *actions.CreateAsset:
+							summaryStr = fmt.Sprintf("assetID: %s metadata:%s", tx.ID(), string(action.Metadata))
+						case *actions.MintAsset:
+							amountStr := strconv.FormatUint(action.Value, 10)
+							assetStr := action.Asset.String()
+							if action.Asset == ids.Empty {
+								amountStr = utils.FormatBalance(action.Value)
+								assetStr = consts.Symbol
+							}
+							summaryStr = fmt.Sprintf("%s %s -> %s", amountStr, assetStr, tutils.Address(action.To))
+						case *actions.BurnAsset:
+							summaryStr = fmt.Sprintf("%d %s -> 🔥", action.Value, action.Asset)
+						case *actions.ModifyAsset:
+							summaryStr = fmt.Sprintf(
+								"assetID: %s metadata:%s owner:%s",
+								action.Asset, string(action.Metadata), tutils.Address(action.Owner),
+							)
 
-					case *actions.Transfer:
-						amountStr := strconv.FormatUint(action.Value, 10)
-						assetStr := action.Asset.String()
-						if action.Asset == ids.Empty {
-							amountStr = utils.FormatBalance(action.Value)
-							assetStr = consts.Symbol
-						}
-						summaryStr = fmt.Sprintf("%s %s -> %s", amountStr, assetStr, tutils.Address(action.To))
+						case *actions.Transfer:
+							amountStr := strconv.FormatUint(action.Value, 10)
+							assetStr := action.Asset.String()
+							if action.Asset == ids.Empty {
+								amountStr = utils.FormatBalance(action.Value)
+								assetStr = consts.Symbol
+							}
+							summaryStr = fmt.Sprintf("%s %s -> %s", amountStr, assetStr, tutils.Address(action.To))
 
-					case *actions.CreateOrder:
-						inTickStr := strconv.FormatUint(action.InTick, 10)
-						inStr := action.In.String()
-						if action.In == ids.Empty {
-							inTickStr = utils.FormatBalance(action.InTick)
-							inStr = consts.Symbol
-						}
-						outTickStr := strconv.FormatUint(action.OutTick, 10)
-						supplyStr := strconv.FormatUint(action.Supply, 10)
-						outStr := action.Out.String()
-						if action.Out == ids.Empty {
-							outTickStr = utils.FormatBalance(action.OutTick)
-							supplyStr = utils.FormatBalance(action.Supply)
-							outStr = consts.Symbol
-						}
-						summaryStr = fmt.Sprintf("%s %s -> %s %s (supply: %s %s)", inTickStr, inStr, outTickStr, outStr, supplyStr, outStr)
-					case *actions.FillOrder:
-						or, _ := actions.UnmarshalOrderResult(result.Output)
-						inAmtStr := strconv.FormatUint(or.In, 10)
-						inStr := action.In.String()
-						if action.In == ids.Empty {
-							inAmtStr = utils.FormatBalance(or.In)
-							inStr = consts.Symbol
-						}
-						outAmtStr := strconv.FormatUint(or.Out, 10)
-						remainingStr := strconv.FormatUint(or.Remaining, 10)
-						outStr := action.Out.String()
-						if action.Out == ids.Empty {
-							outAmtStr = utils.FormatBalance(or.Out)
-							remainingStr = utils.FormatBalance(or.Remaining)
-							outStr = consts.Symbol
-						}
-						summaryStr = fmt.Sprintf(
-							"%s %s -> %s %s (remaining: %s %s)",
-							inAmtStr, inStr, outAmtStr, outStr, remainingStr, outStr,
-						)
-					case *actions.CloseOrder:
-						summaryStr = fmt.Sprintf("orderID: %s", action.Order)
+						case *actions.CreateOrder:
+							inTickStr := strconv.FormatUint(action.InTick, 10)
+							inStr := action.In.String()
+							if action.In == ids.Empty {
+								inTickStr = utils.FormatBalance(action.InTick)
+								inStr = consts.Symbol
+							}
+							outTickStr := strconv.FormatUint(action.OutTick, 10)
+							supplyStr := strconv.FormatUint(action.Supply, 10)
+							outStr := action.Out.String()
+							if action.Out == ids.Empty {
+								outTickStr = utils.FormatBalance(action.OutTick)
+								supplyStr = utils.FormatBalance(action.Supply)
+								outStr = consts.Symbol
+							}
+							summaryStr = fmt.Sprintf("%s %s -> %s %s (supply: %s %s)", inTickStr, inStr, outTickStr, outStr, supplyStr, outStr)
+						case *actions.FillOrder:
+							or, _ := actions.UnmarshalOrderResult(result.Output)
+							inAmtStr := strconv.FormatUint(or.In, 10)
+							inStr := action.In.String()
+							if action.In == ids.Empty {
+								inAmtStr = utils.FormatBalance(or.In)
+								inStr = consts.Symbol
+							}
+							outAmtStr := strconv.FormatUint(or.Out, 10)
+							remainingStr := strconv.FormatUint(or.Remaining, 10)
+							outStr := action.Out.String()
+							if action.Out == ids.Empty {
+								outAmtStr = utils.FormatBalance(or.Out)
+								remainingStr = utils.FormatBalance(or.Remaining)
+								outStr = consts.Symbol
+							}
+							summaryStr = fmt.Sprintf(
+								"%s %s -> %s %s (remaining: %s %s)",
+								inAmtStr, inStr, outAmtStr, outStr, remainingStr, outStr,
+							)
+						case *actions.CloseOrder:
+							summaryStr = fmt.Sprintf("orderID: %s", action.Order)
 
-					case *actions.ImportAsset:
-						wm := tx.WarpMessage
-						signers, _ := wm.Signature.NumSigners()
-						wt, _ := actions.UnmarshalWarpTransfer(wm.Payload)
-						summaryStr = fmt.Sprintf("source: %s signers: %d | ", wm.SourceChainID, signers)
-						var outputAssetID ids.ID
-						if wt.Return {
-							outputAssetID = wt.Asset
-							summaryStr += fmt.Sprintf("%s %s -> %s (return: %t)", valueString(wt.Asset, wt.Value), assetString(wt.Asset), tutils.Address(wt.To), wt.Return)
-						} else {
-							outputAssetID = actions.ImportedAssetID(wt.Asset, wm.SourceChainID)
-							summaryStr += fmt.Sprintf("%s %s (original: %s) -> %s (return: %t)", valueString(outputAssetID, wt.Value), outputAssetID, wt.Asset, tutils.Address(wt.To), wt.Return)
-						}
-						if wt.Reward > 0 {
-							summaryStr += fmt.Sprintf(" | reward: %s", valueString(outputAssetID, wt.Reward))
-						}
-						if wt.SwapIn > 0 {
-							summaryStr += fmt.Sprintf(" | swap in: %s %s swap out: %s %s expiry: %d fill: %t", valueString(outputAssetID, wt.SwapIn), assetString(outputAssetID), valueString(wt.AssetOut, wt.SwapOut), assetString(wt.AssetOut), wt.SwapExpiry, action.Fill)
-						}
-					case *actions.ExportAsset:
-						wt, _ := actions.UnmarshalWarpTransfer(result.WarpMessage.Payload)
-						summaryStr = fmt.Sprintf("destination: %s | ", action.Destination)
-						var outputAssetID ids.ID
-						if !action.Return {
-							outputAssetID = actions.ImportedAssetID(action.Asset, result.WarpMessage.SourceChainID)
-							summaryStr += fmt.Sprintf("%s %s -> %s (return: %t)", valueString(action.Asset, action.Value), assetString(action.Asset), tutils.Address(action.To), action.Return)
-						} else {
-							outputAssetID = wt.Asset
-							summaryStr += fmt.Sprintf("%s %s (original: %s) -> %s (return: %t)", valueString(action.Asset, action.Value), action.Asset, assetString(wt.Asset), tutils.Address(action.To), action.Return)
-						}
-						if wt.Reward > 0 {
-							summaryStr += fmt.Sprintf(" | reward: %s", valueString(outputAssetID, wt.Reward))
-						}
-						if wt.SwapIn > 0 {
-							summaryStr += fmt.Sprintf(" | swap in: %s %s swap out: %s %s expiry: %d", valueString(outputAssetID, wt.SwapIn), assetString(outputAssetID), valueString(wt.AssetOut, wt.SwapOut), assetString(wt.AssetOut), wt.SwapExpiry)
+						case *actions.ImportAsset:
+							wm := tx.WarpMessage
+							signers, _ := wm.Signature.NumSigners()
+							wt, _ := actions.UnmarshalWarpTransfer(wm.Payload)
+							summaryStr = fmt.Sprintf("source: %s signers: %d | ", wm.SourceChainID, signers)
+							var outputAssetID ids.ID
+							if wt.Return {
+								outputAssetID = wt.Asset
+								summaryStr += fmt.Sprintf("%s %s -> %s (return: %t)", valueString(wt.Asset, wt.Value), assetString(wt.Asset), tutils.Address(wt.To), wt.Return)
+							} else {
+								outputAssetID = actions.ImportedAssetID(wt.Asset, wm.SourceChainID)
+								summaryStr += fmt.Sprintf("%s %s (original: %s) -> %s (return: %t)", valueString(outputAssetID, wt.Value), outputAssetID, wt.Asset, tutils.Address(wt.To), wt.Return)
+							}
+							if wt.Reward > 0 {
+								summaryStr += fmt.Sprintf(" | reward: %s", valueString(outputAssetID, wt.Reward))
+							}
+							if wt.SwapIn > 0 {
+								summaryStr += fmt.Sprintf(" | swap in: %s %s swap out: %s %s expiry: %d fill: %t", valueString(outputAssetID, wt.SwapIn), assetString(outputAssetID), valueString(wt.AssetOut, wt.SwapOut), assetString(wt.AssetOut), wt.SwapExpiry, action.Fill)
+							}
+						case *actions.ExportAsset:
+							wt, _ := actions.UnmarshalWarpTransfer(result.WarpMessage.Payload)
+							summaryStr = fmt.Sprintf("destination: %s | ", action.Destination)
+							var outputAssetID ids.ID
+							if !action.Return {
+								outputAssetID = actions.ImportedAssetID(action.Asset, result.WarpMessage.SourceChainID)
+								summaryStr += fmt.Sprintf("%s %s -> %s (return: %t)", valueString(action.Asset, action.Value), assetString(action.Asset), tutils.Address(action.To), action.Return)
+							} else {
+								outputAssetID = wt.Asset
+								summaryStr += fmt.Sprintf("%s %s (original: %s) -> %s (return: %t)", valueString(action.Asset, action.Value), action.Asset, assetString(wt.Asset), tutils.Address(action.To), action.Return)
+							}
+							if wt.Reward > 0 {
+								summaryStr += fmt.Sprintf(" | reward: %s", valueString(outputAssetID, wt.Reward))
+							}
+							if wt.SwapIn > 0 {
+								summaryStr += fmt.Sprintf(" | swap in: %s %s swap out: %s %s expiry: %d", valueString(outputAssetID, wt.SwapIn), assetString(outputAssetID), valueString(wt.AssetOut, wt.SwapOut), assetString(wt.AssetOut), wt.SwapExpiry)
+							}
 						}
 					}
+					utils.Outf(
+						"%s {{yellow}}%s{{/}} {{yellow}}actor:{{/}} %s {{yellow}}units:{{/}} %d {{yellow}}summary (%s):{{/}} [%s]\n",
+						status,
+						tx.ID(),
+						tutils.Address(actor),
+						result.Units,
+						reflect.TypeOf(tx.Action),
+						summaryStr,
+					)
 				}
-				utils.Outf(
-					"%s {{yellow}}%s{{/}} {{yellow}}actor:{{/}} %s {{yellow}}units:{{/}} %d {{yellow}}summary (%s):{{/}} [%s]\n",
-					status,
-					tx.ID(),
-					tutils.Address(actor),
-					result.Units,
-					reflect.TypeOf(tx.Action),
-					summaryStr,
-				)
 			}
 		}
 		return nil
