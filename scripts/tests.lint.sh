8756c41816fb46303fe445539e3bccaef7e8da15
Josh Hesketh <josh.hesketh@gmail.com>
2023-07-17 12:11:58 +0100

    license fixes

--- scripts/tests.lint.sh
+++ scripts/tests.lint.sh
@@ -42,16 +42,16 @@ function find_go_files {
 # automatically checks license headers
 # to modify the file headers (if missing), remove "--check" flag
 # TESTS='license_header' ADDLICENSE_FLAGS="-v" ./scripts/lint.sh
-_addlicense_flags=${ADDLICENSE_FLAGS:-"--check -v"}
+_addlicense_flags=${ADDLICENSE_FLAGS:-"--verify --debug"}
 function test_license_header {
-  go install -v github.com/google/addlicense@latest
+  go install -v github.com/palantir/go-license@latest
   local target="${1}"
   local files=()
   while IFS= read -r line; do files+=("$line"); done < <(find_go_files "${target}")
 
   # ignore 3rd party code
-  addlicense \
-  -f ./LICENSE.header \
+  go-license \
+  --config ./license.yml \
   ${_addlicense_flags} \
   "${files[@]}"
 }
