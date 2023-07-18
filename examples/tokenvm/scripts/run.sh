acaad2ce5009b578433fa96114a3ef53a5d6e95b
Patrick O'Grady <prohb125@gmail.com>
2023-07-17 23:09:09 -0700

    avalanche-network-runner@v1.7.0

--- examples/tokenvm/scripts/run.sh
+++ examples/tokenvm/scripts/run.sh
@@ -172,7 +172,7 @@ ACK_GINKGO_RC=true ginkgo build ./tests/e2e
 # download avalanche-network-runner
 # https://github.com/ava-labs/avalanche-network-runner
 ANR_REPO_PATH=github.com/ava-labs/avalanche-network-runner
-ANR_VERSION=v1.6.0
+ANR_VERSION=v1.7.0
 # version set
 go install -v ${ANR_REPO_PATH}@${ANR_VERSION}
 
