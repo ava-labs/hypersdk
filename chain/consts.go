cb478d0aae1980c7677c9c99fd92195a0b1a0ceb
Patrick O'Grady <prohb125@gmail.com>
2023-07-18 17:05:19 -0700

    change future bound

--- chain/consts.go
+++ chain/consts.go
@@ -10,7 +10,13 @@ import (
 )
 
 const (
-	FutureBound        = 10 * time.Second
+	// FutureBound is used to reject blocks that are produced....
+	//
+	// This value should be (much) less than the value of [ProposerWindow], otherwise honest
+	// nodes may not build during their allocated window (to avoid producing ahead of current time).
+	//
+	// TODO: finish comments
+	FutureBound        = 1 * time.Second
 	MaxWarpMessageSize = 256 * units.KiB
 	// MaxWarpMessages is the maximum number of warp messages allows in a single
 	// block.
