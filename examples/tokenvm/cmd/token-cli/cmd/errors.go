32ab6c9fa0d33bbe1acb1f637b8628dc5a6765ee
Patrick O'Grady <prohb125@gmail.com>
2023-07-21 16:34:53 -0700

    remove unused errors

--- examples/tokenvm/cmd/token-cli/cmd/errors.go
+++ examples/tokenvm/cmd/token-cli/cmd/errors.go
@@ -6,17 +6,9 @@ package cmd
 import "errors"
 
 var (
-	ErrInputEmpty          = errors.New("input is empty")
-	ErrInvalidArgs         = errors.New("invalid args")
-	ErrMissingSubcommand   = errors.New("must specify a subcommand")
-	ErrIndexOutOfRange     = errors.New("index out-of-range")
-	ErrInsufficientBalance = errors.New("insufficient balance")
-	ErrInvalidChoice       = errors.New("invalid choice")
-	ErrNotMultiple         = errors.New("must be a multiple")
-	ErrInsufficientSupply  = errors.New("insufficient supply")
-	ErrMustFill            = errors.New("must fill")
-	ErrDuplicate           = errors.New("duplicate")
-	ErrNoKeys              = errors.New("no available keys")
-	ErrNoChains            = errors.New("no available chains")
-	ErrTxFailed            = errors.New("tx failed")
+	ErrInvalidArgs        = errors.New("invalid args")
+	ErrMissingSubcommand  = errors.New("must specify a subcommand")
+	ErrNotMultiple        = errors.New("must be a multiple")
+	ErrInsufficientSupply = errors.New("insufficient supply")
+	ErrMustFill           = errors.New("must fill")
 )
