34d7b2168635834f4e028c5792747b915e58fa0b
Patrick O'Grady <prohb125@gmail.com>
2023-07-22 09:08:01 -0700

    fix license spacing

--- gossiper/consts.go
+++ gossiper/consts.go
@@ -1,5 +1,6 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
+
 package gossiper
 
 // initialCapacity is the initial size of a txs array we allocate when
