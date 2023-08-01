368d8df824097c7fe53e5a739f676a4da948c124
Patrick O'Grady <prohb125@gmail.com>
2023-08-01 09:40:37 -0700

    use tip of dev

--- examples/morpheusvm/scripts/run.sh
+++ examples/morpheusvm/scripts/run.sh
@@ -17,7 +17,7 @@ if ! [[ "$0" =~ scripts/run.sh ]]; then
   exit 255
 fi
 
-VERSION=v1.10.5
+VERSION=6e97e33e06422ef6e03a765729aaa90eb26d01a5
 MODE=${MODE:-run}
 LOGLEVEL=${LOGLEVEL:-info}
 AVALANCHE_LOG_LEVEL=${AVALANCHE_LOG_LEVEL:-INFO}
