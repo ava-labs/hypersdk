ca70b25c6aa306bb993cf7dc91fe01d5a26d88ec
Patrick O'Grady <prohb125@gmail.com>
2023-08-09 22:50:05 -0700

    making progress on sketch

--- chain/batch.go
+++ chain/batch.go
@@ -3,22 +3,49 @@ package chain
 import (
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
 	"github.com/ava-labs/hypersdk/codec"
+	"github.com/ava-labs/hypersdk/workers"
 )
 
-type Batch struct{}
+type BatchVerifier interface {
+	Add([]byte, Auth)
+	Done() []func() error
+}
 
-func NewBatch(authRegistry *codec.TypeParser[Auth, *warp.Message, bool], authTypes map[uint8]int) *Batch {
+type Batch struct {
+	job *workers.Job
+	bvs map[uint8]BatchVerifier
+}
+
+func NewBatch(job *workers.Job, authRegistry *codec.TypeParser[Auth, *warp.Message, bool], authTypes map[uint8]int) *Batch {
+	bvs := map[uint8]BatchVerifier{}
 	for t, count := range authTypes {
 		// Already verified that all authTypes are valid
 		a, _, _ := authRegistry.LookupIndex(t)
-		a.
+		bv, ok := a.GetBatchVerifier(job.Workers(), count)
+		if !ok {
+			continue
+		}
+		bvs[t] = bv
 	}
+	return &Batch{job, bvs}
 }
 
 func (b *Batch) Add(digest []byte, auth Auth) {
+	// if batch doesn't exist for auth, just add write to job and start
+	bv, ok := bvs[auth.GetTypeID()]
+	if !ok {
+		b.job.Go(auth.AsyncVerify(digest))
+		return
+	}
+	bv.Add(digest, auth)
 }
 
-func (b *Batch) Verify() {
+func (b *Batch) Done() {
 	// Should interleave everything on a single sigjob, but should allocate sub-batches based on cores
 	// This would mean batch would need to put on sigverify? unless just put directly if no batch?
+	for _, bv := range b.bvs {
+		for _, item := range bv.Done() {
+			b.Job.Go(item)
+		}
+	}
 }
