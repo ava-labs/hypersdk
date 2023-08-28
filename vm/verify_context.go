77a07d3b54a2e77197ac29fcbff112de3873a945
Patrick O'Grady <prohb125@gmail.com>
2023-08-28 11:30:08 -0700

    update verification context handling

--- vm/verify_context.go
+++ vm/verify_context.go
@@ -14,17 +14,34 @@ var _ chain.VerifyContext = (*AcceptedVerifyContext)(nil)
 var _ chain.VerifyContext = (*PendingVerifyContext)(nil)
 
 func (vm *VM) GetVerifyContext(ctx context.Context, blockHeight uint64, parent ids.ID) (chain.VerifyContext, error) {
+	// If [blockHeight] is 0, we throw an error because there is no pre-genesis verification context.
 	if blockHeight == 0 {
 		return nil, errors.New("cannot get context of genesis block")
 	}
-	// If last accepted block is not processed, we need to process it. This will happen when we call [View] on the parent.
-	if (!vm.lastAccepted.Processed() && parent == vm.lastAccepted.ID()) || blockHeight-1 > vm.lastAccepted.Hght {
+
+	// If the last accepted block is not yet processed, we can't use the accepted state for the
+	// verification context. This could happen if state sync finishes with no processing blocks (we
+	// sync to the post-execution state of the parent of the last accepted block, not the post-execution
+	// state of the last accepted block).
+	//
+	// Invariant: When [View] is called on [vm.lastAccepted], the block will be verified and the accepted
+	// state will be updated.
+	if !vm.lastAccepted.Processed() && parent == vm.lastAccepted.ID() {
+		return &PendingVerifyContext{vm.lastAccepted}, nil
+	}
+
+	// If the parent block is not yet accepted, we should return the block's verified but not accepted
+	// parent.
+	if blockHeight-1 > vm.lastAccepted.Hght {
 		blk, err := vm.GetStatelessBlock(ctx, parent)
 		if err != nil {
 			return nil, err
 		}
 		return &PendingVerifyContext{blk}, nil
 	}
+
+	// If the parent block is accepted and processed, we should
+	// just use the accepted state as the verification context.
 	return &AcceptedVerifyContext{vm}, nil
 }
 
@@ -44,9 +61,26 @@ type AcceptedVerifyContext struct {
 	vm *VM
 }
 
-// The caller of this should check for equality with the root during verification.
-func (a *AcceptedVerifyContext) View(ctx context.Context, _ ids.ID) (state.View, error) {
-	return a.vm.State()
+func (a *AcceptedVerifyContext) View(ctx context.Context, blockRoot ids.ID) (state.View, error) {
+	state, err := a.vm.State()
+	if err != nil {
+		return nil, err
+	}
+	// This does not make deferred root generation less
+	// efficient because the root must have already
+	// been calculated before the latest state was written
+	// to disk.
+	root, err := state.GetMerkleRoot(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if root != blockRoot {
+		// This should never happen but we check
+		// this to check subtle state handling bugs
+		// in the [chain] package.
+		return nil, ErrUnexpectedStateRoot
+	}
+	return state, nil
 }
 
 func (a *AcceptedVerifyContext) IsRepeat(ctx context.Context, oldestAllowed int64, txs []*chain.Transaction, marker set.Bits, stop bool) (set.Bits, error) {
