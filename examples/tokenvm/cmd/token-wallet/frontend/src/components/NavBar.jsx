f3064bf30880882073a491c42175a7b11cdd3817
Patrick O'Grady <prohb125@gmail.com>
2023-09-10 21:58:03 -0700

    add basic mine page

--- examples/tokenvm/cmd/token-wallet/frontend/src/components/NavBar.jsx
+++ examples/tokenvm/cmd/token-wallet/frontend/src/components/NavBar.jsx
@@ -13,8 +13,8 @@ const items = [
     icon: <DashboardOutlined />,
   },
   {
-    label: <RLink to={"faucet"}>Faucet</RLink>,
-    key: "faucet",
+    label: <RLink to={"mine"}>Mine</RLink>,
+    key: "mine",
     icon: <ThunderboltOutlined />,
   },
   {
--- examples/tokenvm/cmd/token-wallet/frontend/src/routes.jsx
+++ examples/tokenvm/cmd/token-wallet/frontend/src/routes.jsx
@@ -1,6 +1,7 @@
 import App from "./App";
 
 import Explorer from "./components/Explorer";
+import Mine from "./components/Mine";
 import Mint from "./components/Mint";
 import Transfer from "./components/Transfer";
 
@@ -13,6 +14,10 @@ const routes = [
       {
         path: "explorer",
         element: <Explorer />,
+      },
+      {
+        path: "mine",
+        element: <Mine />,
       },
       {
         path: "mint",
