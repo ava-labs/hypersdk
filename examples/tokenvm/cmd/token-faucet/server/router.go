727f1166055afd27330e69b121adaabae307ea4b
Patrick O'Grady <prohb125@gmail.com>
2023-09-12 16:13:34 -0700

    remove aliases

--- examples/tokenvm/cmd/token-faucet/server/router.go
+++ examples/tokenvm/cmd/token-faucet/server/router.go
@@ -7,32 +7,25 @@ import (
 	"sync"
 
 	"github.com/gorilla/mux"
-
-	"github.com/ava-labs/avalanchego/utils/set"
 )
 
 var (
 	errUnknownBaseURL  = errors.New("unknown base url")
 	errUnknownEndpoint = errors.New("unknown endpoint")
-	errAlreadyReserved = errors.New("route is either already aliased or already maps to a handle")
 )
 
 type router struct {
 	lock   sync.RWMutex
 	router *mux.Router
 
-	routeLock      sync.Mutex
-	reservedRoutes set.Set[string]                    // Reserves routes so that there can't be alias that conflict
-	aliases        map[string][]string                // Maps a route to a set of reserved routes
-	routes         map[string]map[string]http.Handler // Maps routes to a handler
+	routeLock sync.Mutex
+	routes    map[string]map[string]http.Handler // Maps routes to a handler
 }
 
 func newRouter() *router {
 	return &router{
-		router:         mux.NewRouter(),
-		reservedRoutes: set.Set[string]{},
-		aliases:        make(map[string][]string),
-		routes:         make(map[string]map[string]http.Handler),
+		router: mux.NewRouter(),
+		routes: make(map[string]map[string]http.Handler),
 	}
 }
 
@@ -68,14 +61,6 @@ func (r *router) AddRouter(base, endpoint string, handler http.Handler) error {
 }
 
 func (r *router) addRouter(base, endpoint string, handler http.Handler) error {
-	if r.reservedRoutes.Contains(base) {
-		return fmt.Errorf("%w: %s", errAlreadyReserved, base)
-	}
-
-	return r.forceAddRouter(base, endpoint, handler)
-}
-
-func (r *router) forceAddRouter(base, endpoint string, handler http.Handler) error {
 	endpoints := r.routes[base]
 	if endpoints == nil {
 		endpoints = make(map[string]http.Handler)
@@ -94,45 +79,5 @@ func (r *router) forceAddRouter(base, endpoint string, handler http.Handler) err
 		return fmt.Errorf("failed to create new route for %s", url)
 	}
 	route.Name(url)
-
-	var err error
-	if aliases, exists := r.aliases[base]; exists {
-		for _, alias := range aliases {
-			if innerErr := r.forceAddRouter(alias, endpoint, handler); err == nil {
-				err = innerErr
-			}
-		}
-	}
-	return err
-}
-
-func (r *router) AddAlias(base string, aliases ...string) error {
-	r.lock.Lock()
-	defer r.lock.Unlock()
-	r.routeLock.Lock()
-	defer r.routeLock.Unlock()
-
-	for _, alias := range aliases {
-		if r.reservedRoutes.Contains(alias) {
-			return fmt.Errorf("%w: %s", errAlreadyReserved, alias)
-		}
-	}
-
-	for _, alias := range aliases {
-		r.reservedRoutes.Add(alias)
-	}
-
-	r.aliases[base] = append(r.aliases[base], aliases...)
-
-	var err error
-	if endpoints, exists := r.routes[base]; exists {
-		for endpoint, handler := range endpoints {
-			for _, alias := range aliases {
-				if innerErr := r.forceAddRouter(alias, endpoint, handler); err == nil {
-					err = innerErr
-				}
-			}
-		}
-	}
-	return err
+	return nil
 }
--- examples/tokenvm/cmd/token-faucet/server/server.go
+++ examples/tokenvm/cmd/token-faucet/server/server.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"net/url"
-	"path"
 	"sync"
 	"time"
 
@@ -17,43 +15,24 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
-	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/logging"
 )
-
-const baseURL = "/ext"
 
 var (
 	errUnknownLockOption = errors.New("invalid lock options")
 
-	_ PathAdder = readPathAdder{}
-	_ Server    = (*server)(nil)
+	_ Server = (*server)(nil)
 )
 
 type PathAdder interface {
 	// AddRoute registers a route to a handler.
 	AddRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string) error
-
-	// AddAliases registers aliases to the server
-	AddAliases(endpoint string, aliases ...string) error
-}
-
-type PathAdderWithReadLock interface {
-	// AddRouteWithReadLock registers a route to a handler assuming the http
-	// read lock is currently held.
-	AddRouteWithReadLock(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string) error
-
-	// AddAliasesWithReadLock registers aliases to the server assuming the http read
-	// lock is currently held.
-	AddAliasesWithReadLock(endpoint string, aliases ...string) error
 }
 
 // Server maintains the HTTP router
 type Server interface {
 	PathAdder
-	PathAdderWithReadLock
 	// Dispatch starts the API server
 	Dispatch() error
 	// Shutdown this server
@@ -68,6 +47,8 @@ type HTTPConfig struct {
 }
 
 type server struct {
+	baseURL string
+
 	// log this server writes to
 	log logging.Logger
 
@@ -84,6 +65,7 @@ type server struct {
 
 // New returns an instance of a Server.
 func New(
+	baseURL string,
 	log logging.Logger,
 	listener net.Listener,
 	allowedOrigins []string,
@@ -114,6 +96,7 @@ func New(
 	)
 
 	return &server{
+		baseURL:         baseURL,
 		log:             log,
 		shutdownTimeout: shutdownTimeout,
 		router:          router,
@@ -132,67 +115,6 @@ func (s *server) Dispatch() error {
 	return s.srv.Serve(s.listener)
 }
 
-func (s *server) RegisterChain(chainName string, ctx *snow.ConsensusContext, vm common.VM) {
-	var (
-		handlers map[string]*common.HTTPHandler
-		err      error
-	)
-
-	ctx.Lock.Lock()
-	handlers, err = vm.CreateHandlers(context.TODO())
-	ctx.Lock.Unlock()
-	if err != nil {
-		s.log.Error("failed to create handlers",
-			zap.String("chainName", chainName),
-			zap.Error(err),
-		)
-		return
-	}
-
-	s.log.Verbo("about to add API endpoints",
-		zap.Stringer("chainID", ctx.ChainID),
-	)
-	// all subroutes to a chain begin with "bc/<the chain's ID>"
-	defaultEndpoint := path.Join(constants.ChainAliasPrefix, ctx.ChainID.String())
-
-	// Register each endpoint
-	for extension, handler := range handlers {
-		// Validate that the route being added is valid
-		// e.g. "/foo" and "" are ok but "\n" is not
-		_, err := url.ParseRequestURI(extension)
-		if extension != "" && err != nil {
-			s.log.Error("could not add route to chain's API handler",
-				zap.String("reason", "route is malformed"),
-				zap.Error(err),
-			)
-			continue
-		}
-		if err := s.addChainRoute(chainName, handler, ctx, defaultEndpoint, extension); err != nil {
-			s.log.Error("error adding route",
-				zap.Error(err),
-			)
-		}
-	}
-}
-
-func (s *server) addChainRoute(chainName string, handler *common.HTTPHandler, ctx *snow.ConsensusContext, base, endpoint string) error {
-	url := fmt.Sprintf("%s/%s", baseURL, base)
-	s.log.Info("adding route",
-		zap.String("url", url),
-		zap.String("endpoint", endpoint),
-	)
-	// Apply middleware to grab/release chain's lock before/after calling API method
-	h, err := lockMiddleware(
-		handler.Handler,
-		handler.LockOptions,
-		&ctx.Lock,
-	)
-	if err != nil {
-		return err
-	}
-	return s.router.AddRouter(url, endpoint, h)
-}
-
 func (s *server) AddRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string) error {
 	return s.addRoute(handler, lock, base, endpoint)
 }
@@ -204,7 +126,7 @@ func (s *server) AddRouteWithReadLock(handler *common.HTTPHandler, lock *sync.RW
 }
 
 func (s *server) addRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string) error {
-	url := fmt.Sprintf("%s/%s", baseURL, base)
+	url := fmt.Sprintf("%s/%s", s.baseURL, base)
 	s.log.Info("adding route",
 		zap.String("url", url),
 		zap.String("endpoint", endpoint),
@@ -248,25 +170,6 @@ func lockMiddleware(
 	}
 }
 
-func (s *server) AddAliases(endpoint string, aliases ...string) error {
-	url := fmt.Sprintf("%s/%s", baseURL, endpoint)
-	endpoints := make([]string, len(aliases))
-	for i, alias := range aliases {
-		endpoints[i] = fmt.Sprintf("%s/%s", baseURL, alias)
-	}
-	return s.router.AddAlias(url, endpoints...)
-}
-
-func (s *server) AddAliasesWithReadLock(endpoint string, aliases ...string) error {
-	// This is safe, as the read lock doesn't actually need to be held once the
-	// http handler is called. However, it is unlocked later, so this function
-	// must end with the lock held.
-	s.router.lock.RUnlock()
-	defer s.router.lock.RLock()
-
-	return s.AddAliases(endpoint, aliases...)
-}
-
 func (s *server) Shutdown() error {
 	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
 	err := s.srv.Shutdown(ctx)
@@ -275,22 +178,4 @@ func (s *server) Shutdown() error {
 	// If shutdown times out, make sure the server is still shutdown.
 	_ = s.srv.Close()
 	return err
-}
-
-type readPathAdder struct {
-	pather PathAdderWithReadLock
-}
-
-func PathWriterFromWithReadLock(pather PathAdderWithReadLock) PathAdder {
-	return readPathAdder{
-		pather: pather,
-	}
-}
-
-func (a readPathAdder) AddRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string) error {
-	return a.pather.AddRouteWithReadLock(handler, lock, base, endpoint)
-}
-
-func (a readPathAdder) AddAliases(endpoint string, aliases ...string) error {
-	return a.pather.AddAliasesWithReadLock(endpoint, aliases...)
 }
