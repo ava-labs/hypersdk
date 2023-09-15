4a77d074ec7576defcfa726127d6b588f98fb4c9
Patrick O'Grady <prohb125@gmail.com>
2023-09-15 12:36:27 -0700

    update build script

--- examples/tokenvm/cmd/token-wallet/scripts/build.sh
+++ examples/tokenvm/cmd/token-wallet/scripts/build.sh
@@ -6,5 +6,23 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Remove any previous build artifacts
+rm -rf token-wallet.zip
+
 # Build file for local arch
-wails build -clean
+#
+# Don't use upx: https://github.com/upx/upx/issues/446
+wails build -clean -platform darwin/universal
+codesign -s ${APP_SIGNING_KEY_ID} --deep  --timestamp -o runtime -v build/bin/Token\ Wallet.app
+ditto -c -k --keepParent build/bin/Token\ Wallet.app token-wallet.zip
+
+# Need to sign to allow for app to be opened on other computers
+xcrun altool --notarize-app --primary-bundle-id com.ava-labs.token-wallet --username ${APPLE_NOTARIZATION_USERNAME} --password "@keychain:altool" --file token-wallet.zip
+
+# Log until exit
+read -p "Input APPLE_NOTARIZATION_REQUEST_ID: " APPLE_NOTARIZATION_REQUEST_ID
+while true
+do
+  xcrun altool --notarization-info ${APPLE_NOTARIZATION_REQUEST_ID} -u ${APPLE_NOTARIZATION_USERNAME} -p "@keychain:altool"
+  sleep 15
+done
