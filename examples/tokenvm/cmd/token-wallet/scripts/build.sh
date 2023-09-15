7d36d51b7c894fddc969f7d32f64356bff4964ae
Patrick O'Grady <prohb125@gmail.com>
2023-09-15 09:59:50 -0700

    debugging binary build

---
+++ examples/tokenvm/cmd/token-wallet/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+# See the file LICENSE for licensing terms.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Build file for local arch
+wails build -clean
