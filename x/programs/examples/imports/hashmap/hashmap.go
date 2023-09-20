8e01086e0a8ac87bee24257e36f4c4af939f2aa7
Sam Batschelet <sam.batschelet@avalabs.org>
2023-09-19 22:04:17 -0400

    Add guest import examples
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

---
+++ x/programs/examples/imports/hashmap/hashmap.go
@@ -0,0 +1,153 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package hashmap
+
+import (
+	"github.com/bytecodealliance/wasmtime-go/v12"
+
+	"go.uber.org/zap"
+
+	"github.com/ava-labs/avalanchego/utils/logging"
+
+	"github.com/ava-labs/hypersdk/state"
+	"github.com/ava-labs/hypersdk/x/programs/runtime"
+)
+
+// GlobalStorage is a global variable that holds program state.
+var GlobalStorage = storage{
+	state: make(map[int64]maps),
+}
+
+type maps map[string][]byte
+
+// Key value store for program data
+type storage struct {
+	state map[int64]maps
+}
+
+const Name = "map"
+
+func New(log logging.Logger, mu state.Mutable) *Import {
+	return &Import{mu: mu, log: log}
+}
+
+func AddProgramID(id int64) {
+	GlobalStorage.state[id] = make(maps)
+}
+
+type Import struct {
+	mu    state.Mutable
+	log   logging.Logger
+	meter runtime.Meter
+}
+
+func (i *Import) Name() string {
+	return Name
+}
+
+func (i *Import) Register(link runtime.Link, meter runtime.Meter) error {
+	if err := link.FuncWrap(Name, "store_bytes", i.storeBytesFn); err != nil {
+		return err
+	}
+	if err := link.FuncWrap(Name, "get_bytes_len", i.getBytesLenFn); err != nil {
+		return err
+	}
+	if err := link.FuncWrap(Name, "get_bytes", i.getBytesFn); err != nil {
+		return err
+	}
+
+	i.meter = meter
+
+	return nil
+}
+
+func (i *Import) storeBytesFn(caller *wasmtime.Caller, id int64, keyPtr int32, keyLength int32, valuePtr int32, valueLength int32) int32 {
+	memory := runtime.NewMemory(runtime.NewExportClient(caller))
+
+	_, ok := GlobalStorage.state[int64(id)]
+	if !ok {
+		i.log.Error("failed to find program id in storage")
+		return -1
+	}
+
+	keyBytes, err := memory.Range(uint32(keyPtr), uint32(keyLength))
+	if err != nil {
+		i.log.Error("failed to read key from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	valueBytes, err := memory.Range(uint32(valuePtr), uint32(valueLength))
+	if err != nil {
+		i.log.Error("failed to read value from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// Need to copy the value because the GC can collect the value after this function returns
+	// is this still true?
+	copiedValue := make([]byte, len(valueBytes))
+	copy(copiedValue, valueBytes)
+	GlobalStorage.state[id][string(keyBytes)] = copiedValue
+
+	return 0
+}
+
+func (i *Import) getBytesLenFn(caller *wasmtime.Caller, id int64, keyPtr int32, keyLength int32) int32 {
+	memory := runtime.NewMemory(runtime.NewExportClient(caller))
+
+	_, ok := GlobalStorage.state[int64(id)]
+	if !ok {
+		i.log.Error("failed to find program id in storage")
+		return -1
+	}
+
+	keyBytes, err := memory.Range(uint32(keyPtr), uint32(keyLength))
+	if err != nil {
+		i.log.Error("failed to read key from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+	val, ok := GlobalStorage.state[id][string(keyBytes)]
+	if !ok {
+		return -1
+	}
+
+	return int32(len(val))
+}
+
+func (i *Import) getBytesFn(caller *wasmtime.Caller, id int64, keyPtr int32, keyLength int32, valLength int32) int32 {
+	memory := runtime.NewMemory(runtime.NewExportClient(caller))
+
+	_, ok := GlobalStorage.state[int64(id)]
+	if !ok {
+		i.log.Error("failed to find program id in storage")
+		return -1
+	}
+
+	keyBytes, err := memory.Range(uint32(keyPtr), uint32(keyLength))
+	if err != nil {
+		i.log.Error("failed to read key from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+	val, ok := GlobalStorage.state[id][string(keyBytes)]
+	if !ok {
+		return -1
+	}
+
+	ptr, err := runtime.WriteBytes(memory, val)
+	if err != nil {
+		i.log.Error("failed to write to memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	return int32(ptr)
+}
---
+++ x/programs/examples/imports/program/program.go
@@ -0,0 +1,183 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package imports
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/bytecodealliance/wasmtime-go/v12"
+
+	"github.com/ava-labs/hypersdk/codec"
+	"github.com/ava-labs/hypersdk/state"
+	"github.com/ava-labs/hypersdk/x/programs/examples/storage"
+	"github.com/ava-labs/hypersdk/x/programs/runtime"
+)
+
+const Name = "program"
+
+type Import struct {
+	db         state.Immutable
+	log        logging.Logger
+	imports    runtime.Imports
+	meter      runtime.Meter
+	registered bool
+}
+
+// New returns a new program invoke host module which can perform program to program calls.
+func New(log logging.Logger, db state.Immutable) *Import {
+	return &Import{
+		db:  db,
+		log: log,
+	}
+}
+
+func (i *Import) Register(linker runtime.Link, meter runtime.Meter, imports runtime.Imports) error {
+	if i.registered {
+		return fmt.Errorf("import module already registered")
+	}
+	if err := linker.FuncWrap(Name, "invoke_program", i.invokeProgramFn); err != nil {
+		return err
+	}
+	i.registered = true
+	i.imports = imports
+	i.meter = meter
+
+	return nil
+}
+
+// invokeProgramFn makes a call to an entry function of a program in the context of another program's ID.
+func (i *Import) invokeProgramFn(
+	caller *wasmtime.Caller,
+	programIDPtr,
+	functionPtr,
+	functionLen,
+	argsPtr,
+	argsLen uint32,
+	maxUnits uint64,
+) int64 {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	memory := runtime.NewMemory(runtime.NewExportClient(caller))
+
+	programIDBytes, err := memory.Range(uint32(programIDPtr), uint32(ids.IDLen))
+	if err != nil {
+		i.log.Error("failed to read function name from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	programID, err := ids.ToID(programIDBytes)
+	if err != nil {
+		i.log.Error("failed to create programID",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// get the entry function for invoke to call.
+	functionBytes, err := memory.Range(uint32(functionPtr), uint32(functionLen))
+	if err != nil {
+		i.log.Error("failed to read function name from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	programWasmBytes, exists, err := storage.GetProgram(i.db, programID)
+	if !exists {
+		i.log.Error("program does not exist")
+	}
+	if err != nil {
+		i.log.Error("failed to get program from storage",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// spend the maximum number of units allowed for this call
+	i.meter.Spend(maxUnits)
+
+	// create a new runtime for the program to be invoked
+	rt := runtime.New(i.log, runtime.NewConfigBuilder(maxUnits).Build(), i.imports)
+	err = rt.Initialize(context.Background(), programWasmBytes)
+	if err != nil {
+		i.log.Error("failed to initialize runtime",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	argsBytes, err := memory.Range(uint32(argsPtr), uint32(argsLen))
+	if err != nil {
+		i.log.Error("failed to read program args name from memory",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// sync args to new runtime and return arguments to the invoke call
+	params, err := getCallArgs(ctx, rt, argsBytes, programIDPtr)
+	if err != nil {
+		i.log.Error("failed to unmarshal call arguments",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	function := string(functionBytes)
+	res, err := rt.Call(ctx, function, params...)
+	if err != nil {
+		i.log.Error("failed to call entry function",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// spend the remaining balance
+	balance := rt.Meter().GetBalance()
+	_, err = rt.Meter().Spend(balance)
+	if err != nil {
+		i.log.Error("failed to spend balance",
+			zap.Error(err),
+		)
+		return -1
+	}
+
+	// return balance to parent runtime
+	i.meter.AddUnits(balance)
+
+	return int64(res[0])
+}
+
+func getCallArgs(ctx context.Context, rt runtime.Runtime, buffer []byte, invokeProgramID uint32) ([]interface{}, error) {
+	// first arg contains id of program to call
+	args := []interface{}{invokeProgramID}
+	p := codec.NewReader(buffer, len(buffer))
+	for !p.Empty() {
+		size := p.UnpackInt64(true)
+		isInt := p.UnpackBool()
+		if isInt {
+			valueInt := p.UnpackUint64(true)
+			args = append(args, valueInt)
+		} else {
+			valueBytes := make([]byte, size)
+			p.UnpackFixedBytes(int(size), &valueBytes)
+			ptr, err := runtime.WriteBytes(rt.Memory(), valueBytes)
+			if err != nil {
+				return nil, err
+			}
+			args = append(args, ptr)
+		}
+	}
+	if p.Err() != nil {
+		return nil, fmt.Errorf("failed to unpack arguments: %w", p.Err())
+	}
+	return args, nil
+}
