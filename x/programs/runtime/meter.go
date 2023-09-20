7cf7f758aa9aa93e24adb8096e706e7a9cfd98fd
Sam Batschelet <sam.batschelet@avalabs.org>
2023-09-19 22:11:56 -0400

    Implement meter
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- x/programs/runtime/meter.go
+++ x/programs/runtime/meter.go
@@ -1,75 +1,39 @@
-// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
 package runtime
 
-import (
-	"context"
-	"errors"
-	"sync"
+import "github.com/bytecodealliance/wasmtime-go/v12"
 
-	"go.uber.org/zap"
+var _ Meter = (*meter)(nil)
 
-	"github.com/tetratelabs/wazero/api"
-
-	"github.com/ava-labs/avalanchego/utils/logging"
-)
-
-var (
-	ErrMeterInsufficientBalance       = errors.New("operation failed insufficient balance")
-	_                           Meter = (*meter)(nil)
-)
-
-type Meter interface {
-	api.Meter
-	GetBalance(context.Context) uint64
-}
-
-// NewMeter returns a meter capable of tracking the cost of operations.
-func NewMeter(log logging.Logger, maxFee uint64, costMap map[string]uint64) *meter {
+// NewMeter returns a new meter.
+func NewMeter(store *wasmtime.Store, maxUnits uint64) Meter {
+	store.AddFuel(maxUnits)
 	return &meter{
-		log:     log,
-		costMap: costMap,
-		balance: maxFee,
+		maxUnits: maxUnits,
+		store:    store,
 	}
 }
 
 type meter struct {
-	lock         sync.RWMutex
-	costMap      map[string]uint64
-	balance      uint64
-	initialError error
-
-	log logging.Logger
+	maxUnits uint64
+	store    *wasmtime.Store
 }
 
-func (m *meter) AddCost(_ context.Context, op string) error {
-	if err := m.closed(); err != nil {
-		return err
+func (m *meter) GetBalance() uint64 {
+	consumed, ok := m.store.FuelConsumed()
+	if !ok {
+		return 0
 	}
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	fee := m.costMap[op]
-	if fee > m.balance {
-		m.log.Debug("insufficient balance", zap.Uint64("fee", fee), zap.Uint64("balance", m.balance))
-		m.initialError = ErrMeterInsufficientBalance
-		return m.initialError
+	if m.maxUnits < consumed {
+		panic("meter balance should never be negative")
 	}
 
-	m.balance -= fee
-
-	return nil
+	return m.maxUnits - consumed
 }
 
-func (m *meter) GetBalance(_ context.Context) uint64 {
-	m.lock.RLock()
-	defer m.lock.RUnlock()
-	return m.balance
+func (m *meter) Spend(units uint64) (uint64, error) {
+	return m.store.ConsumeFuel(units)
 }
 
-func (m *meter) closed() error {
-	m.lock.RLock()
-	defer m.lock.RUnlock()
-	return m.initialError
+func (m *meter) AddUnits(units uint64) error {
+	return m.store.AddFuel(units)
 }
