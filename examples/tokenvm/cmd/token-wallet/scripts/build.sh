7430195be78cc671050c3dd6c267fd62761536da
Patrick O'Grady <prohb125@gmail.com>
2023-09-20 16:02:33 -0700

    provide correct cgo flags

--- examples/tokenvm/cmd/token-wallet/scripts/build.sh
+++ examples/tokenvm/cmd/token-wallet/scripts/build.sh
@@ -6,6 +6,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+
 PUBLISH=${PUBLISH:-true}
 
 # Install wails
@@ -15,6 +21,11 @@ go install -v github.com/wailsapp/wails/v2/cmd/wails@v2.5.1
 #
 # Don't use upx: https://github.com/upx/upx/issues/446
 wails build -clean -platform darwin/universal
+
+OUTPUT=build/bin/Token\ Wallet.app
+if [ ! -d "$OUTPUT" ]; then
+  exit 1
+fi
 
 # Exit early if not publishing
 if [ ${PUBLISH} == false ]; then
