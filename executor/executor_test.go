5930598c6132d3c0ca1ddfeebc0bc3e73a87af50
Patrick O'Grady <prohb125@gmail.com>
2023-10-15 10:55:31 -0700

    multi-conflcit test passing

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -95,3 +95,41 @@ func TestExecutorSimpleConflict(t *testing.T) {
 	e.Wait()
 	require.Equal([]int{0, 10, 20, 30, 40, 50, 60, 70, 80, 90}, completed[90:])
 }
+
+func TestExecutorMultiConflict(t *testing.T) {
+	var (
+		require      = require.New(t)
+		conflictKey  = ids.GenerateTestID().String()
+		conflictKey2 = ids.GenerateTestID().String()
+		l            sync.Mutex
+		completed    = make([]int, 0, 100)
+		e            = New(100, 4)
+	)
+	for i := 0; i < 100; i++ {
+		s := set.NewSet[string](i + 1)
+		for k := 0; k < i+1; k++ {
+			s.Add(ids.GenerateTestID().String())
+		}
+		if i%10 == 0 {
+			s.Add(conflictKey)
+		}
+		if i == 15 || i == 20 {
+			s.Add(conflictKey2)
+		}
+		ti := i
+		e.Run(s, func() {
+			if ti == 0 {
+				time.Sleep(3 * time.Second)
+			}
+			if ti == 15 {
+				time.Sleep(5 * time.Second)
+			}
+
+			l.Lock()
+			completed = append(completed, ti)
+			l.Unlock()
+		})
+	}
+	e.Wait()
+	require.Equal([]int{0, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90}, completed[89:])
+}
