ae70fe9118f252896b854c93de8deed4a7131b11
Patrick O'Grady <prohb125@gmail.com>
2023-10-15 19:40:20 -0700

    add missing licenses

--- executor/errors.go
+++ executor/errors.go
@@ -1,7 +1,8 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package executor
 
 import "errors"
 
-var (
-	ErrStopped = errors.New("stopped")
-)
+var ErrStopped = errors.New("stopped")
