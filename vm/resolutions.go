bb8fc63b5860b5abee17d5583985cadc0993cf97
Patrick O'Grady <prohb125@gmail.com>
2023-10-15 17:29:17 -0700

    update vm resolutions

--- vm/resolutions.go
+++ vm/resolutions.go
@@ -479,3 +479,7 @@ func (vm *VM) UnitPrices(context.Context) (chain.Dimensions, error) {
 	}
 	return chain.NewFeeManager(v).UnitPrices(), nil
 }
+
+func (vm *VM) GetTransactionExecutionCores() int {
+	return vm.config.GetTransactionExecutionCores()
+}
