c1ac1bae8e3007428687ca62a7a470509536baf9
Patrick O'Grady <prohb125@gmail.com>
2023-10-16 23:11:06 -0700

    add test for upper half sig

--- crypto/secp256r1/secp256r1_test.go
+++ crypto/secp256r1/secp256r1_test.go
@@ -3,6 +3,7 @@ package secp256r1
 import (
 	"crypto/elliptic"
 	"encoding/hex"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -38,5 +39,33 @@ func TestASN1Parsing(t *testing.T) {
 
 	// Verify signature
 	msg := []byte("aaa")
+	require.True(IsNormalized(new(big.Int).SetBytes(s)))
 	require.True(Verify(msg, PublicKey(cpk), Signature(append(r, s...))))
+}
+
+func TestASN1ParsingUpperOrder(t *testing.T) {
+	// PublicKey/Signature source: https://kjur.github.io/jsrsasign/sample/sample-ecdsa.html
+	ssig := "3046022100c8e58be6d0f1a14543441f13e091befe44d6f50ae3674e52ea4a83878f52a0a60221009b4d1aeb55aec89ae2caa868c0326df2e35546d163c1ddf0e9afd65d8b3c8388"
+	hpk := "04306b5d823340e69712cd1feff3b31ae48f60e6f8d62d9e4248d630969b1e7c85c7425fed4efd200c102ac1d93e5bbe37b8c027fa63bd58be298734d33bda53c3"
+
+	// Parse uncompressed public key
+	require := require.New(t)
+	rpk, err := hex.DecodeString(hpk)
+	require.NoError(err)
+	x, y := elliptic.Unmarshal(elliptic.P256(), rpk)
+	cpk := elliptic.MarshalCompressed(elliptic.P256(), x, y)
+	require.Len(cpk, PublicKeyLen)
+
+	// Parse signature
+	sig, err := hex.DecodeString(ssig)
+	require.NoError(err)
+	r, s, err := ParseASN1Signature(sig)
+	require.NoError(err)
+	require.Len(r, rsLen)
+	require.Len(s, rsLen)
+
+	// Verify signature
+	msg := []byte("aaa")
+	require.False(IsNormalized(new(big.Int).SetBytes(s)))
+	require.False(Verify(msg, PublicKey(cpk), Signature(append(r, s...))))
 }
