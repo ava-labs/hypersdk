b2590967f94fb1cf161d53d189ca8f6f6e80cd97
Patrick O'Grady <prohb125@gmail.com>
2023-10-17 01:04:35 -0700

    hardcode some tests

--- crypto/secp256r1/secp256r1_test.go
+++ crypto/secp256r1/secp256r1_test.go
@@ -63,6 +63,32 @@ func TestNormalization(t *testing.T) {
 	}
 }
 
+func TestInvalidSignature(t *testing.T) {
+	msg := []byte("hello")
+	publicKey := "03831f7d72b912e589ec5dec0321342f21d036a1983601926654e4d1c2278ab278"
+	badSignature := "96f96b7eb69615acd55c4b9fede157ce054c1520789cbc519c67c8b43893337c37202734c76c8beb8b200103860dd5edede4cc98f4b47ba81e92e151e516572f"
+
+	require := require.New(t)
+	pk, err := hex.DecodeString(publicKey)
+	require.NoError(err)
+	sig, err := hex.DecodeString(badSignature)
+	require.NoError(err)
+	require.False(Verify(msg, PublicKey(pk), Signature(sig)))
+}
+
+func TestSignatureLargeS(t *testing.T) {
+	msg := []byte("hello")
+	publicKey := "03ca7eedb087dd30ba97c468f1c3ac06e0571dd055d0cdaf4147d11df0337c11f9"
+	badSignature := "cd0fb02caaac3896f9a1257f077344b3710626c59913ae0100fcc2f8005b10c19d0c237bc8929f3a07993d2c42037bcee6adf33feb3523c17c9ee29b651c7ef7"
+
+	require := require.New(t)
+	pk, err := hex.DecodeString(publicKey)
+	require.NoError(err)
+	sig, err := hex.DecodeString(badSignature)
+	require.NoError(err)
+	require.False(Verify(msg, PublicKey(pk), Signature(sig)))
+}
+
 func TestASN1Parsing(t *testing.T) {
 	// PublicKey/Signature source: https://kjur.github.io/jsrsasign/sample/sample-ecdsa.html
 	ssig := "304502210099086f10c8fc0b32e83f6f0280997950b4f6fe376479334fab2ab12f652b767a02203c53f459a2c35ee274cff20e27461f919dd891475dcd59bab7e2e5db3bad05be"
