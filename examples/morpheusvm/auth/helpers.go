3352f34d96cf670d5672888cce5d08d7690f413e
Patrick O'Grady <prohb125@gmail.com>
2023-10-17 23:46:17 -0700

    cleanup auth/helpers

--- examples/morpheusvm/auth/helpers.go
+++ examples/morpheusvm/auth/helpers.go
@@ -11,16 +11,12 @@ import (
 // GetSigner extracts the signer that authorized
 // the transaction. If the auth type is not supported,
 // it will return nil.
-//
-// TODO: consider prefixing signers with a type byte
-// to ensure we never try to verify signatures across
-// schemes.
 func GetSigner(auth chain.Auth) codec.ShortBytes {
 	switch a := auth.(type) {
 	case *ED25519:
-		return a.Signer.ShortBytes(a.GetTypeID())
+		return a.signerAddress()
 	case *SECP256R1:
-		return a.Signer.ShortBytes(a.GetTypeID())
+		return a.signerAddress()
 	default:
 		// We should never reach this point during block
 		// execution because types should be asserted during parse.
