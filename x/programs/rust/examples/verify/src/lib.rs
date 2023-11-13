20443ffc42381340f8ef690a27c676a73e3621a7
samliok <samliok@stanford.edu>
2023-11-12 22:35:29 -0800

    rust style/lint

--- x/programs/rust/examples/verify/src/lib.rs
+++ x/programs/rust/examples/verify/src/lib.rs
@@ -1,36 +1,49 @@
-use wasmlanche_sdk::{program::Program, public, types::Bytes32};
-use wasmlanche_sdk::host::verify_ed25519;
 use ed25519_dalek::{Signature, Verifier};
+use wasmlanche_sdk::host::verify_ed25519;
+use wasmlanche_sdk::{program::Program, public, types::Bytes32};
 // import custom types from types.rs
 mod types;
-use types::{EDSignatureBytes, BatchPubKeys};
+use types::{BatchPubKeys, EDSignatureBytes};
 
-/// Runs multiple ED25519 signature verifications in Wasm. 
+/// Runs multiple ED25519 signature verifications in Wasm.
 /// Returns true if every public key in [pub_keys] verifies [signature_bytes] against [message].
 /// Returns false if otherwise.
 #[public]
-pub fn verify_ed_in_wasm(_: Program, pub_keys: BatchPubKeys, signature_bytes: EDSignatureBytes, message: Bytes32) -> bool {
+pub fn verify_ed_in_wasm(
+    _: Program,
+    pub_keys: BatchPubKeys,
+    signature_bytes: EDSignatureBytes,
+    message: Bytes32,
+) -> bool {
     let mut result = true;
     let signature: Signature = Signature::from_bytes(signature_bytes.as_bytes());
     // iterate through batch pub keys
     for pub_key in pub_keys.0.iter() {
         match pub_key.as_key().verify(message.as_bytes(), &signature) {
-            Ok(_) => {},
-            Err(_) => {result = false},
+            Ok(_) => {}
+            Err(_) => result = false,
         }
     }
     result
 }
 
+/// Runs multiple ED25519 signature verifications in the host.
+/// Returns true if every public key in [pub_keys] verifies [signature_bytes] against [message].
+/// Returns false if otherwise.
 #[public]
-pub fn verify_ed_multiple_host_func(program: Program, pub_keys: BatchPubKeys, signature_bytes: EDSignatureBytes, message: Bytes32) -> bool {
+pub fn verify_ed_multiple_host_func(
+    program: Program,
+    pub_keys: BatchPubKeys,
+    signature_bytes: EDSignatureBytes,
+    message: Bytes32,
+) -> bool {
     let mut result = true;
     let signature: Signature = Signature::from_bytes(signature_bytes.as_bytes());
     // iterate through batch pub keys
     for pub_key in pub_keys.0.iter() {
         match verify_ed25519(&program, message, &signature, pub_key.as_key()) {
-            1 => {},
-            _ => {result = false},
+            1 => {}
+            _ => result = false,
         }
     }
     result
--- x/programs/rust/examples/verify/src/types.rs
+++ x/programs/rust/examples/verify/src/types.rs
@@ -1,23 +1,37 @@
-use ed25519_dalek::{Signature, SigningKey, Signer, VerifyingKey};
-use wasmlanche_sdk::{program::Program, public, types::Bytes32};
+use ed25519_dalek::VerifyingKey;
+use wasmlanche_sdk::types::Bytes32;
 
-// Custom Args
-// verifying key = ed25519 pub key
+// We create these custom wrapper types to allow us to pass them
+// across the WASM boundary.
+
+// EDVerifyingKey is a wrapper around ed25519_dalek::VerifyingKey.
+// this represents an ED25519 public key
 pub struct EDVerifyingKey(ed25519_dalek::VerifyingKey);
-pub struct EDSignatureBytes([u8; 64]);
+
+// EDSignatureBytes represents the bytes of an ed25519 signature.
+pub struct EDSignatureBytes([u8; EDSignatureBytes::LEN]);
+
+// BatchPubKeys represents a batch of EDVerifyingKeys.
+// Currently, we need to hardcode the length of the batch(due to limitations of the SDK).
+// Once we have dynamic args, we can marshal the length in go and unmarshal in the macro.
 pub struct BatchPubKeys(pub Vec<EDVerifyingKey>);
 
-impl From<i64> for EDVerifyingKey {
-    fn from(value: i64) -> Self {
-        let bytes = Bytes32::from(value);
-        let mut verifying_key_bytes: [u8; 32] = [0; 32];
-        verifying_key_bytes.copy_from_slice(bytes.as_bytes());
-        // TODO: is unwrap ok here
-        Self(VerifyingKey::from_bytes(&verifying_key_bytes).unwrap())
+impl EDVerifyingKey {
+    const LEN: usize = ed25519_dalek::PUBLIC_KEY_LENGTH;
+    pub fn as_key(&self) -> &ed25519_dalek::VerifyingKey {
+        &self.0
+    }
+}
+
+impl EDSignatureBytes {
+    const LEN: usize = ed25519_dalek::SIGNATURE_LENGTH;
+    pub fn as_bytes(&self) -> &[u8; Self::LEN] {
+        &self.0
     }
 }
 
 impl BatchPubKeys {
+    // hardcoded length of batch
     const TEMP_LEN: usize = 5;
     pub fn push(&mut self, pub_key: EDVerifyingKey) {
         self.0.push(pub_key);
@@ -27,35 +41,32 @@ impl BatchPubKeys {
     }
 }
 
+impl From<i64> for EDVerifyingKey {
+    fn from(value: i64) -> Self {
+        let bytes = Bytes32::from(value);
+        Self(
+            VerifyingKey::from_bytes(
+            bytes.as_bytes()
+                .try_into()
+                .expect("invalid bytes")
+            ).expect("invalid bytes"),
+        )
+    }
+}
+
 impl From<i64> for BatchPubKeys {
     fn from(value: i64) -> Self {
-        let mut pub_keys : BatchPubKeys = BatchPubKeys(Vec::new());
+        let mut pub_keys: BatchPubKeys = BatchPubKeys(Vec::new());
         // in the meantime we need to hardcode this
         // once we have dynamic args we can marshal the length in go
         // and unmarshal in the macro
         for i in 0..BatchPubKeys::TEMP_LEN {
-            let ptr : i64 = value + (i as i64) * 32;
+            // 32 bytes per key, update ptr & grab next key
+            let ptr: i64 = value + (i * EDVerifyingKey::LEN) as i64;
             pub_keys.push(EDVerifyingKey::from(ptr));
         }
 
-        if pub_keys.len() != 5 {
-            panic!("Expected 5 initialized values");
-        }
-
         pub_keys
-    }
-}
-
-impl EDVerifyingKey {
-    pub fn as_key(&self) -> &ed25519_dalek::VerifyingKey {
-        &self.0
-    }
-}
-
-impl EDSignatureBytes {
-    const LEN: usize = 64;
-    pub fn as_bytes(&self) -> &[u8; 64] {
-        &self.0
     }
 }
 
