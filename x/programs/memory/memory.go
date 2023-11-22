5d1b7f7b477fa1bdb23528afc5d4b26322829260
Sam Batschelet <sam.batschelet@avalabs.org>
2023-11-22 06:42:52 -0500

    add memory
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

---
+++ x/programs/memory/memory.go
@@ -0,0 +1,175 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package memory
+
+import (
+	"fmt"
+	"runtime"
+
+	rt "github.com/ava-labs/hypersdk/x/programs/runtime"
+
+	"github.com/bytecodealliance/wasmtime-go/v14"
+)
+
+var (
+	ErrOverflow = fmt.Errorf("memory pointer overflow")
+	ErrUnderflow = fmt.Errorf("memory pointer underflow")
+	ErrInvalidType	= fmt.Errorf("invalid type")
+)
+
+const (
+	AllocFnName   = "alloc"
+	DeallocFnName = "dealloc"
+	MemoryFnName  = "memory"
+)
+
+type Memory struct {
+	inner *wasmtime.Memory
+	caller *wasmtime.Caller
+}
+
+func New(inner *wasmtime.Memory, caller *wasmtime.Caller) *Memory {
+	return &Memory{
+		inner: inner,
+		caller: caller,
+	}
+}
+
+func (m *Memory) Range(offset uint64, length uint64) ([]byte, error) {
+	err := m.ensureValidOffset(offset, length)
+	if err != nil {
+		return nil, err
+	}
+
+	// ensure memory is not GCed during the life of this method
+	mem := m.inner
+	runtime.KeepAlive(mem)
+
+	data := mem.UnsafeData(m.caller)
+	buf := make([]byte, length)
+
+	// copy data from memory to buf to ensure it is not GCed.
+	copy(buf, data[offset:offset+length])
+
+	return buf, nil
+}
+
+func (m *Memory) Write(offset uint64, buf []byte) error {
+	err := m.ensureValidOffset(offset, uint64(len(buf)))
+	if err != nil {
+		return err
+	}
+
+	data := m.inner.UnsafeData(m.caller)
+	copy(data[offset:], buf)
+
+	return nil
+}
+
+func (m *Memory) ensureValidOffset(offset uint64, length uint64) error {
+	memLen, err := m.Len()
+	if err != nil {
+		return err
+	}
+
+	// verify available memory is large enough
+	if offset+length > memLen {
+		return ErrOverflow
+	}
+	
+	return nil
+}
+
+func (m *Memory) Alloc(length uint64) (uint64, error) {
+	err := m.ensureValidOffset(0, length)
+	if err != nil {
+		return 0, err
+	}
+
+	ext := m.caller.GetExport(AllocFnName)
+	if ext == nil {
+		return 0, fmt.Errorf("export not found: %s", AllocFnName)
+	}
+
+	fn := ext.Func()
+	if fn == nil {
+		return 0, fmt.Errorf("%w: function not found: %s", ErrInvalidType, AllocFnName)
+	}
+
+	result, err := fn.Call(m.caller, int32(length))
+	if err != nil {
+		return 0, rt.HandleTrapError(err)
+	}
+
+	addr, ok := result.(int32)
+	if !ok {
+		return 0, fmt.Errorf("%w: invalid result type: %T", ErrInvalidType, result)
+	}
+	if addr < 0 {
+		return 0, ErrUnderflow
+	}
+
+	return uint64(addr), nil
+}
+
+func (m *Memory) Grow(delta uint64) (uint64, error) {
+	mem := m.inner
+	runtime.KeepAlive(mem)
+
+	return mem.Grow(m.caller, delta)
+}
+
+func (m *Memory) Len() (uint64, error) {
+	mem := m.inner
+	runtime.KeepAlive(mem)
+
+	return uint64(mem.DataSize(m.caller)), nil
+}
+
+// WriteBytes is a helper function that allocates memory and writes the given
+// bytes to the memory returning the offset.
+func WriteBytes(m Memory, buf []byte) (uint64, error) {
+	offset, err := m.Alloc(uint64(len(buf)))
+	if err != nil {
+		return 0, err
+	}
+	err = m.Write(offset, buf)
+	if err != nil {
+		return 0, err
+	}
+
+	return offset, nil
+}
+
+// CallParam defines a value to be passed to a guest function.
+type CallParam struct {
+	Value interface{} `json,yaml:"value"`
+}
+
+// WriteParams is a helper function that writes the given params to memory if non integer.
+// Supported types include int, uint64 and string.
+func WriteParams(m Memory, p []CallParam) ([]uint64, error) {
+	params := []uint64{}
+	for _, param := range p {
+		switch v := param.Value.(type) {
+		case string:
+			ptr, err := WriteBytes(m, []byte(v))
+			if err != nil {
+				return nil, err
+			}
+			params = append(params, ptr)
+		case int:
+			if v < 0 {
+				return nil, fmt.Errorf("failed to write param: %w", ErrUnderflow)
+			}
+			params = append(params, uint64(v))
+		case uint64:
+			params = append(params, v)
+		default:
+			return nil, fmt.Errorf("%w: support types int, uint64 and string", runtime.ErrInvalidParamType)
+		}
+	}
+
+	return params, nil
+}
---
+++ x/programs/memory/memory_test.go
@@ -0,0 +1,146 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package memory
+
+import (
+	"context"
+	_ "embed"
+	"os"
+	"testing"
+
+	"github.com/ava-labs/avalanchego/utils"
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/bytecodealliance/wasmtime-go/v14"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/hypersdk/x/programs/runtime"
+)
+
+var log = logging.NewLogger(
+	"",
+	logging.NewWrappedCore(
+		logging.Info,
+		os.Stderr,
+		logging.Plain.ConsoleEncoder(),
+	))
+
+func TestLimitMaxMemory(t *testing.T) {
+	require := require.New(t)
+
+	// memory has a single page
+	wasm, err := wasmtime.Wat2Wasm(`
+	(module
+
+	  (memory 2) ;; 2 pages
+	  (export "memory" (memory 0))
+	)
+	`)
+	require.NoError(err)
+
+	// wasm defines 2 pages of memory but runtime set max 1 page
+	maxUnits := uint64(1)
+	cfg, err := runtime.NewConfigBuilder().
+		WithLimitMaxMemory(1 * runtime.MemoryPageSize). // 1 page
+		Build()
+	require.NoError(err)
+	rt := runtime.New(log, cfg, nil)
+	err = rt.Initialize(context.Background(), wasm, maxUnits)
+	require.ErrorContains(err, "memory minimum size of 2 pages exceeds memory limits")
+}
+
+func TestLimitMaxMemoryGrow(t *testing.T) {
+	require := require.New(t)
+
+	wasm, err := wasmtime.Wat2Wasm(`
+	(module
+	
+	  (memory 1) ;; 1 pages
+	  (export "memory" (memory 0))
+	)
+	`)
+	require.NoError(err)
+
+	maxUnits := uint64(1)
+	cfg, err := runtime.NewConfigBuilder().
+		WithLimitMaxMemory(1 * runtime.MemoryPageSize). // 1 page
+		Build()
+	require.NoError(err)
+	rt := runtime.New(logging.NoLog{}, cfg, nil)
+	err = rt.Initialize(context.Background(), wasm, maxUnits)
+	require.NoError(err)
+
+	length, err := rt.Memory().Len()
+	require.NoError(err)
+	require.Equal(uint64(0x10000), length)
+
+	// attempt to grow memory to 2 pages which exceeds the limit
+	_, err = rt.Memory().Grow(1)
+	require.ErrorContains(err, "failed to grow memory by `1`")
+}
+
+func TestWriteExceedsLimitMaxMemory(t *testing.T) {
+	require := require.New(t)
+
+	wasm, err := wasmtime.Wat2Wasm(`
+	(module
+	
+	  (memory 1) ;; 1 pages
+	  (export "memory" (memory 0))
+	)
+	`)
+	require.NoError(err)
+
+	maxUnits := uint64(1)
+	cfg, err := runtime.NewConfigBuilder().
+		WithLimitMaxMemory(1 * runtime.MemoryPageSize). // 1 page
+		Build()
+	require.NoError(err)
+	rt := runtime.New(logging.NoLog{}, cfg, nil)
+	err = rt.Initialize(context.Background(), wasm, maxUnits)
+	require.NoError(err)
+	maxMemory, err := rt.Memory().Len()
+	require.NoError(err)
+
+	bytes := utils.RandomBytes(int(maxMemory) + 1)
+	err = rt.Memory().Write(0, bytes)
+	require.Error(err, "write memory failed: invalid memory size")
+}
+
+func TestWithMaxWasmStack(t *testing.T) {
+	require := require.New(t)
+	wasm, err := wasmtime.Wat2Wasm(`
+	(module $test
+	(type (;0;) (func (result i32)))
+	(export "get_guest" (func 0))
+	(func (;0;) (type 0) (result i32)
+		(local i32)
+		i32.const 1
+	  )
+	) 
+	`)
+	require.NoError(err)
+
+	maxUnits := uint64(4)
+	cfg, err := runtime.NewConfigBuilder().
+		WithMaxWasmStack(720).
+		Build()
+	require.NoError(err)
+	rt := runtime.New(logging.NoLog{}, cfg, nil)
+	err = rt.Initialize(context.Background(), wasm, maxUnits)
+	require.NoError(err)
+	_, err = rt.Call(context.Background(), "get")
+	require.NoError(err)
+
+	// stack is ok for 1 call.
+	cfg, err = runtime.NewConfigBuilder().
+		WithMaxWasmStack(500).
+		Build()
+	require.NoError(err)
+	rt = runtime.New(logging.NoLog{}, cfg, nil)
+	err = rt.Initialize(context.Background(), wasm, maxUnits)
+	require.NoError(err)
+	// exceed the stack limit
+	_, err = rt.Call(context.Background(), "get")
+	require.ErrorIs(err, runtime.ErrTrapStackOverflow)
+}
