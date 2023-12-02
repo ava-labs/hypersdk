236e963b3b0ebb80c41f24b0ba4f2124e5de995c
samliok <samliok@stanford.edu>
2023-12-02 12:52:37 -0800

    represent pointers as *const u8

--- x/programs/rust/wasmlanche_sdk/src/host/state.rs
+++ x/programs/rust/wasmlanche_sdk/src/host/state.rs
@@ -10,10 +10,10 @@ use borsh::{to_vec, BorshSerialize};
 #[link(wasm_import_module = "state")]
 extern "C" {
     #[link_name = "put"]
-    fn _put(caller_id: i64, key: i64, value: i64) -> i32;
+    fn _put(caller_id: i64, key_ptr: *const u8, value: *const u8) -> i32;
 
     #[link_name = "get"]
-    fn _get(caller_id: i64, key_ptr: i64) -> i64;
+    fn _get(caller_id: i64, key_ptr: *const u8) -> i64;
 }
 
 /// Persists the bytes at `value_ptr` to the bytes at key ptr on the host storage.
@@ -32,7 +32,7 @@ where
     let value_bytes = prepend_length(&value_bytes);
     let key_bytes = prepend_length(key.as_bytes());
 
-    match unsafe { _put(caller.id(), key_bytes.as_ptr() as i64, value_bytes.as_ptr() as i64) } {
+    match unsafe { _put(caller.id(), key_bytes.as_ptr(), value_bytes.as_ptr()) } {
         0 => Ok(()),
         _ => Err(StateError::Write),
     }
@@ -55,5 +55,5 @@ where
 pub(crate) unsafe fn get_bytes(caller: &Program, key: &Key) -> i64 {
     // prepend length to key
     let key = prepend_length(key.as_bytes());
-    unsafe { _get(caller.id(), key.as_ptr() as i64) }
+    unsafe { _get(caller.id(), key.as_ptr()) }
 }
