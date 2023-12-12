d4e95eb185b4face6a60724239b75e4e4f52a892
Sam Batschelet <sam.batschelet@avalabs.org>
2023-12-12 14:49:25 -0500

    wip
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

---
+++ x/programs/program/utils.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package program
+
+import "math"
+
+func EnsureInt64ToInt32(v int64) bool {
+	return v >= math.MinInt32 && v <= math.MaxInt32
+}
+
+func EnsureUint32ToInt32(v uint32) bool {
+	return v <= math.MaxInt32
+}
