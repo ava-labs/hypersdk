f97070bca16d152494c8bad7df0ce5a545cda94b
William Law <williamlaw.wtl@gmail.com>
2024-01-16 13:24:26 -0500

    [crypto] add aggregation for BLS public keys (#679)
    * [crypto] remove agg function for BLS signatures
    
    * [crypto] fix lint
    
    * [crypto] support aggregation for public and sig
    
    * [crypto] add aggregation pk and sig test

--- crypto/bls/public_test.go
+++ crypto/bls/public_test.go
@@ -36,3 +36,51 @@ func TestPublicKeyBytes(t *testing.T) {
 	require.Equal(pk, pk2)
 	require.Equal(pkBytes, pk2Bytes)
 }
+
+func TestAggregatePublicKeysNoop(t *testing.T) {
+	require := require.New(t)
+
+	sk, err := GeneratePrivateKey()
+	require.NoError(err)
+
+	pk := PublicFromPrivateKey(sk)
+	pkBytes := PublicKeyToBytes(pk)
+
+	aggPK, err := AggregatePublicKeys([]*PublicKey{pk})
+	require.NoError(err)
+
+	aggPKBytes := PublicKeyToBytes(aggPK)
+	require.NoError(err)
+
+	require.Equal(pk, aggPK)
+	require.Equal(pkBytes, aggPKBytes)
+}
+
+func TestAggregatePublicKeys(t *testing.T) {
+	require := require.New(t)
+
+	msg := utils.RandomBytes(1234)
+
+	// Public Key 1
+	sk1, err := GeneratePrivateKey()
+	require.NoError(err)
+	pk1 := PublicFromPrivateKey(sk1)
+
+	// Public Key 2
+	sk2, err := GeneratePrivateKey()
+	require.NoError(err)
+	pk2 := PublicFromPrivateKey(sk2)
+
+	// Aggregates 2 public keys
+	aggPks, err := AggregatePublicKeys([]*PublicKey{pk1, pk2})
+	require.NoError(err)
+
+	// Aggregates 2 signatures
+	sig1 := Sign(msg, sk1)
+	sig2 := Sign(msg, sk2)
+	aggSigs, err := AggregateSignatures([]*Signature{sig1, sig2})
+	require.NoError(err)
+
+	// Verifies aggregate signature with aggregate public key
+	require.True(Verify(msg, aggPks, aggSigs))
+}
