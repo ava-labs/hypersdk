c7d06e149842fe92a7012e882423a02927ac0a2b
dboehm-avalabs <david.boehm@avalabs.org>
2024-02-01 23:09:19 -0500

    gas

--- x/programs/rust/wasmlanche_sdk/src/context.rs
+++ x/programs/rust/wasmlanche_sdk/src/context.rs
@@ -1,9 +1,12 @@
 use crate::{
     program::Program,
+    types::Address,
 };
 use borsh::{BorshDeserialize, BorshSerialize};
 
 #[derive(Clone, Copy, BorshDeserialize, BorshSerialize)]
 pub struct Context {
    pub program: Program,
+   pub gas_remaining: u64,
+  // pub actor: Address,
 }
