3c14898fb470e42bce48dca00192e2f67ef15735
Patrick O'Grady <prohb125@gmail.com>
2024-02-03 15:58:38 -0800

    add signature to chunk

--- chain/chunk.go
+++ chain/chunk.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/crypto/bls"
 	"github.com/ava-labs/hypersdk/codec"
 	"github.com/ava-labs/hypersdk/consts"
 	"github.com/ava-labs/hypersdk/utils"
 )
 
 type Chunk struct {
-	Producer ids.ShortID `json:"producer"`
-	Expiry   int64       `json:"expiry"`
+	Producer  ids.ShortID    `json:"producer"`
+	Signer    *bls.PublicKey `json:"signer"`
+	Signature *bls.Signature `json:"signature"`
 
-	Txs []*Transaction `json:"txs"`
+	Expiry int64          `json:"expiry"`
+	Txs    []*Transaction `json:"txs"`
 
 	size int
 	id   ids.ID
@@ -37,10 +40,15 @@ func (c *Chunk) Size() int {
 }
 
 func (c *Chunk) Marshal() ([]byte, error) {
-	size := consts.ShortIDLen + consts.Uint64Len + consts.IntLen + codec.CummSize(c.Txs)
+	size := consts.ShortIDLen + consts.Uint64Len + consts.IntLen + codec.CummSize(c.Txs) + bls.PublicKeyLen + bls.SignatureLen
 	p := codec.NewWriter(size, consts.NetworkSizeLimit)
 
+	// Marshal header
 	p.PackShortID(c.Producer)
+	p.PackFixedBytes(bls.PublicKeyToBytes(c.Signer))
+	p.PackFixedBytes(bls.SignatureToBytes(c.Signature))
+
+	// Marsha transactions
 	p.PackInt64(c.Expiry)
 	p.PackInt(len(c.Txs))
 	for _, tx := range c.Txs {
@@ -64,10 +72,26 @@ func UnmarshalChunk(raw []byte, parser Parser) (*Chunk, error) {
 	)
 	c.id = utils.ToID(raw)
 	c.size = len(raw)
+
+	// Parse header
 	p.UnpackShortID(true, &c.Producer)
-	c.Expiry = p.UnpackInt64(false)
+	pk := make([]byte, bls.PublicKeyLen)
+	p.UnpackFixedBytes(bls.PublicKeyLen, &pk)
+	signer, err := bls.PublicKeyFromBytes(pk)
+	if err != nil {
+		return nil, err
+	}
+	c.Signer = signer
+	sig := make([]byte, bls.SignatureLen)
+	p.UnpackFixedBytes(bls.SignatureLen, &sig)
+	signature, err := bls.SignatureFromBytes(sig)
+	if err != nil {
+		return nil, err
+	}
+	c.Signature = signature
 
 	// Parse transactions
+	c.Expiry = p.UnpackInt64(false)
 	txCount := p.UnpackInt(true) // can't produce empty blocks
 	c.Txs = []*Transaction{}     // don't preallocate all to avoid DoS
 	for i := 0; i < txCount; i++ {
