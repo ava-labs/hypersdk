65077e8fbd589f34594511dac7e264f537310475
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-02-08 10:25:13 -0800

    nit

--- scripts/tests.lint.sh
+++ scripts/tests.lint.sh
@@ -28,7 +28,7 @@ TESTS=${TESTS:-"golangci_lint license_header"}
 
 # https://github.com/golangci/golangci-lint/releases
 function test_golangci_lint {
-  go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+  go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.56.0
   golangci-lint run --config .golangci.yml
 }
 
