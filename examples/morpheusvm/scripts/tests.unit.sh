94426b6a790a99cd66c4066d6a5a11936a09d1c8
vikinatora <vikinatora@gmail.com>
2024-02-20 09:48:12 +0200

    chore: rebase on top of master

--- examples/morpheusvm/scripts/tests.unit.sh
+++ examples/morpheusvm/scripts/tests.unit.sh
@@ -4,17 +4,9 @@
 
 set -e
 
-# Set the CGO flags to use the portable version of BLST
-#
-# We use "export" here instead of just setting a bash variable because we need
-# to pass this flag to all child processes spawned by the shell.
-export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
-
 if ! [[ "$0" =~ scripts/tests.unit.sh ]]; then
-  echo "must be run from repository root"
+  echo "must be run from morpheusvm root"
   exit 255
 fi
 
-# Provision of the list of tests requires word splitting, so disable the shellcheck
-# shellcheck disable=SC2046
-go test -race -timeout="10m" -coverprofile="coverage.out" -covermode="atomic" $(find . -name "*.go" | grep -v "./cmd" | grep -v "./tests" | xargs -n1 dirname | sort -u | xargs)
+../../scripts/tests.unit.sh
