b4a82b5e4dca3bf1a2ac9f1fe5897115798d7781
vikinatora <vikinatora@gmail.com>
2024-02-20 09:48:14 +0200

    fix: update mockgen version to pass ci checks

--- scripts/mock.gen.sh
+++ scripts/mock.gen.sh
@@ -21,7 +21,7 @@ if ! command -v mockgen &> /dev/null
 then
   echo "mockgen not found, installing..."
   # https://github.com/uber-go/mock
-  go install -v go.uber.org/mock/mockgen@v0.2.0
+  go install -v go.uber.org/mock/mockgen@v0.4.0
 fi
 
 # alert the user if they do not have $GOPATH properly configured
