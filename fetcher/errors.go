93f91201a878ae9511a40b9c37f570b619f2fbe3
William Law <williamlaw.wtl@gmail.com>
2024-02-22 20:59:40 -0500

    [chain/processor]: fetcher wip

---
+++ fetcher/errors.go
@@ -0,0 +1,8 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package fetcher
+
+import "errors"
+
+var ErrInvalidKeyValue        = errors.New("invalid key or value")
