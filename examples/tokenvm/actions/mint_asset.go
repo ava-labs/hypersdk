f847bd6f77bf48868c9f4d68f55ba622686929e0
William Law <williamlaw.wtl@gmail.com>
2024-03-05 15:47:19 -0500

    [tstate] tune key access in examples

--- examples/tokenvm/actions/mint_asset.go
+++ examples/tokenvm/actions/mint_asset.go
@@ -37,7 +37,7 @@ func (*MintAsset) GetTypeID() uint8 {
 
 func (m *MintAsset) StateKeys(codec.Address, ids.ID) state.Keys {
 	return state.Keys{
-		string(storage.AssetKey(m.Asset)):         state.All,
+		string(storage.AssetKey(m.Asset)):         state.Read | state.Write,
 		string(storage.BalanceKey(m.To, m.Asset)): state.All,
 	}
 }
