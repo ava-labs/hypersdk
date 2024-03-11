809ebbfedc3b8fdfec2c1ef53f22364ab995ae36
Patrick O'Grady <prohb125@gmail.com>
2024-03-11 11:50:04 -0700

    add more benchmarks

--- opool/opool_test.go
+++ opool/opool_test.go
@@ -12,8 +12,9 @@ import (
 )
 
 var (
-	cases   = []int{1, 16, 64, 128, 512, 2048, 4096, 16384}
-	workers = []int{1, 2, 4, 8, 16, 24, 32}
+	signatures = []int{128, 2048, 4096, 16384, 32768}
+	workers    = []int{2, 4, 8, 16, 24, 32}
+	batches    = []int{1, 2, 4, 8, 16, 32, 64, 128}
 )
 
 func generateSignature(b *testing.B) ([]byte, ed25519.PublicKey, []byte) {
@@ -31,7 +32,7 @@ func generateSignature(b *testing.B) ([]byte, ed25519.PublicKey, []byte) {
 }
 
 func BenchmarkBaseline(b *testing.B) {
-	for _, sigs := range cases {
+	for _, sigs := range signatures {
 		b.Run(fmt.Sprintf("sigs=%d", sigs), func(b *testing.B) {
 			b.StopTimer()
 			msg, pub, sig := generateSignature(b)
@@ -47,70 +48,107 @@ func BenchmarkBaseline(b *testing.B) {
 }
 
 func BenchmarkNewGoroutines(b *testing.B) {
-	for _, sigs := range cases {
-		b.Run(fmt.Sprintf("sigs=%d", sigs), func(b *testing.B) {
-			b.StopTimer()
-			msg, pub, sig := generateSignature(b)
-			b.StartTimer()
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				wg := sync.WaitGroup{}
-				for j := 0; j < sigs; j++ {
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						ed25519.Verify(pub, msg, sig)
-					}()
-				}
-				wg.Wait()
+	for _, sigs := range signatures {
+		for _, batch := range batches {
+			if batch > sigs {
+				continue
 			}
-		})
-	}
-}
-
-func BenchmarkErrgroup(b *testing.B) {
-	for _, w := range workers {
-		for _, sigs := range cases {
-			b.Run(fmt.Sprintf("sigs=%d w=%d", sigs, w), func(b *testing.B) {
+			b.Run(fmt.Sprintf("sigs=%d batch=%d", sigs, batch), func(b *testing.B) {
 				b.StopTimer()
 				msg, pub, sig := generateSignature(b)
 				b.StartTimer()
 				b.ReportAllocs()
 				for i := 0; i < b.N; i++ {
-					g, _ := errgroup.WithContext(context.TODO())
-					g.SetLimit(w)
-					for j := 0; j < sigs; j++ {
-						g.Go(func() error {
-							ed25519.Verify(pub, msg, sig)
-							return nil
-						})
+					wg := sync.WaitGroup{}
+					started := 0
+					for started < sigs {
+						run := min(batch, sigs-started)
+						started += run
+						wg.Add(1)
+						go func() {
+							defer wg.Done()
+
+							for j := 0; j < run; j++ {
+								ed25519.Verify(pub, msg, sig)
+							}
+						}()
 					}
-					g.Wait()
+					wg.Wait()
 				}
 			})
 		}
 	}
 }
 
-func BenchmarkOPool(b *testing.B) {
-	for _, w := range workers {
-		for _, sigs := range cases {
-			b.Run(fmt.Sprintf("sigs=%d w=%d", sigs, w), func(b *testing.B) {
-				b.StopTimer()
-				msg, pub, sig := generateSignature(b)
-				b.StartTimer()
-				b.ReportAllocs()
-				for i := 0; i < b.N; i++ {
-					op := New(w, sigs)
-					for j := 0; j < sigs; j++ {
-						op.Go(func() (func(), error) {
-							ed25519.Verify(pub, msg, sig)
-							return nil, nil
-						})
-					}
-					op.Wait()
+func BenchmarkErrgroup(b *testing.B) {
+	for _, sigs := range signatures {
+		for _, w := range workers {
+			for _, batch := range batches {
+				if batch > sigs {
+					continue
 				}
-			})
+				if w > sigs {
+					continue
+				}
+				b.Run(fmt.Sprintf("sigs=%d w=%d batch=%d", sigs, w, batch), func(b *testing.B) {
+					b.StopTimer()
+					msg, pub, sig := generateSignature(b)
+					b.StartTimer()
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						g, _ := errgroup.WithContext(context.TODO())
+						g.SetLimit(w)
+						started := 0
+						for started < sigs {
+							run := min(batch, sigs-started)
+							started += run
+							g.Go(func() error {
+								for j := 0; j < run; j++ {
+									ed25519.Verify(pub, msg, sig)
+								}
+								return nil
+							})
+						}
+						g.Wait()
+					}
+				})
+			}
+		}
+	}
+}
+
+func BenchmarkOPool(b *testing.B) {
+	for _, sigs := range signatures {
+		for _, w := range workers {
+			for _, batch := range batches {
+				if batch > sigs {
+					continue
+				}
+				if w > sigs {
+					continue
+				}
+				b.Run(fmt.Sprintf("sigs=%d w=%d batch=%d", sigs, w, batch), func(b *testing.B) {
+					b.StopTimer()
+					msg, pub, sig := generateSignature(b)
+					b.StartTimer()
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						op := New(w, sigs)
+						started := 0
+						for started < sigs {
+							run := min(batch, sigs-started)
+							started += run
+							op.Go(func() (func(), error) {
+								for j := 0; j < run; j++ {
+									ed25519.Verify(pub, msg, sig)
+								}
+								return nil, nil
+							})
+						}
+						op.Wait()
+					}
+				})
+			}
 		}
 	}
 }
