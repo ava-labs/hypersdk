92f6d40d7a2a501a7e474454796318d9159e3671
Patrick O'Grady <prohb125@gmail.com>
2024-03-11 16:03:58 -0700

    handle deadlock case on Get

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -37,8 +37,9 @@ type Fetcher struct {
 }
 
 type tx struct {
-	wg   *sync.WaitGroup
-	keys state.Keys
+	blockers int
+	waiter   chan struct{}
+	keys     state.Keys
 }
 
 type task struct {
@@ -108,6 +109,21 @@ func (f *Fetcher) runWorker() {
 	}
 }
 
+func (f *Fetcher) set(k string, v []byte, exists bool, chunks uint16) {
+	f.l.Lock()
+	defer f.l.Unlock()
+
+	// Puts a key that was fetched from data into cache
+	f.keys[k].cache = &data{v, exists, chunks}
+	for _, id := range f.keys[k].blocked {
+		f.txs[id].blockers--
+		if f.txs[id].blockers == 0 {
+			close(f.txs[id].waiter)
+		}
+	}
+	f.keys[k].blocked = nil
+}
+
 func (f *Fetcher) handleErr(err error) {
 	f.stopOnce.Do(func() {
 		f.l.Lock()
@@ -117,31 +133,6 @@ func (f *Fetcher) handleErr(err error) {
 		f.l.Unlock()
 		close(f.stop)
 	})
-}
-
-// handleDone ensures that [err] cannot change after [Wait]
-// returns.
-//
-// This gracefully handles the case where [Stop] is called while waiting
-// for results but results are returned at the same time.
-func (f *Fetcher) handleDone() error {
-	f.l.Lock()
-	f.done = true
-	fErr := f.err
-	f.l.Unlock()
-	return fErr
-}
-
-func (f *Fetcher) set(k string, v []byte, exists bool, chunks uint16) {
-	f.l.Lock()
-	defer f.l.Unlock()
-
-	// Puts a key that was fetched from data into cache
-	f.keys[k].cache = &data{v, exists, chunks}
-	for _, id := range f.keys[k].blocked {
-		f.txs[id].wg.Done() // Notify all other txs
-	}
-	f.keys[k].blocked = nil
 }
 
 // Fetch enqueues a set of [stateKeys] to be fetched from disk. Duplicate keys
@@ -177,14 +168,16 @@ func (f *Fetcher) Fetch(ctx context.Context, txID ids.ID, stateKeys state.Keys) 
 		// Register to get notified when the key is fetched
 		d.blocked = append(d.blocked, txID)
 	}
-	wg := &sync.WaitGroup{}
-	wg.Add(len(tasks))
-	f.txs[txID] = &tx{wg, stateKeys}
+	f.txs[txID] = &tx{blockers: len(tasks), waiter: make(chan struct{}), keys: stateKeys}
 	f.l.Unlock()
 
-	// Send fetch tasks to the workers
+	// Send fetch tasks to the workers or exit
 	for _, t := range tasks {
-		f.tasks <- t
+		select {
+		case f.tasks <- t:
+		case <-f.stop:
+			return f.err
+		}
 	}
 	return nil
 }
@@ -197,15 +190,17 @@ func (f *Fetcher) Get(txID ids.ID) (map[string]uint16, map[string][]byte, error)
 	// Block until all keys for the tx are fetched or if the fetcher errored
 	f.l.RLock()
 	tx, ok := f.txs[txID]
-	fErr := f.err // allows us to avoid deadlock when [tx.wg] will never be cleared
 	f.l.RUnlock()
 	if !ok {
 		return nil, nil, fmt.Errorf("%w: unable to get keys for transaction %s", ErrMissingTx, txID)
 	}
-	if fErr != nil {
-		return nil, nil, fErr
+	select {
+	case <-tx.waiter:
+	case <-f.stop:
+		// While waiting, the fetcher may error. Handling this case
+		// prevents a deadlock.
+		return nil, nil, f.err
 	}
-	tx.wg.Wait()
 
 	// Fetch keys from cache
 	f.l.RLock()
@@ -240,5 +235,8 @@ func (f *Fetcher) Wait() error {
 		close(f.tasks)
 	})
 	f.wg.Wait()
-	return f.handleDone()
+	f.l.Lock()
+	f.done = true
+	f.l.Unlock()
+	return f.err
 }
