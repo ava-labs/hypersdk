a0312506e130ae437286f2b0a6f1ba1ed166bb50
Patrick O'Grady <prohb125@gmail.com>
2024-03-16 13:05:17 -0700

    add note about Loki

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -175,4 +175,5 @@ echo -e "${YELLOW}We use a shorter EPOCH_DURATION to speed up devnet startup. In
 sleep $SLEEP_DUR
 
 # Start load test on dedicated machine
+# TODO: only start using again once test is run async and logs are collected using Loki (stream isn't reliable)
 $TMPDIR/avalanche node loadtest ${CLUSTER} ${VMID} --loadTestRepoURL="https://github.com/ava-labs/hypersdk/commit/${VM_COMMIT}" --loadTestBuildCmd="cd /home/ubuntu/hypersdk/examples/morpheusvm; CGO_CFLAGS=\"-O -D__BLST_PORTABLE__\" go build -o ~/simulator ./cmd/morpheus-cli" --loadTestCmd="/home/ubuntu/simulator spam run ed25519 --num-accounts=10000 --txs-per-second=100000 --num-clients=5 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
