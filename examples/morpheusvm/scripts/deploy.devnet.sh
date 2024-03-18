ec60ab9f86eb670dc67402b431c2003a090f7e5d
Patrick O'Grady <prohb125@gmail.com>
2024-03-18 07:54:47 -0700

    print more load commands

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -142,7 +142,7 @@ function cleanup {
   echo -e "${RED}To destroy the devnet, run:${NC} \"${TMPDIR}/avalanche node destroy ${CLUSTER}\""
 }
 trap cleanup EXIT
-# TODO: compare performance to c7gn
+# TODO: compare performance to c7gn (https://docs.aws.amazon.com/ec2/latest/instancetypes/ec2-instance-regions.html)
 $TMPDIR/avalanche node devnet wiz ${CLUSTER} ${VMID} --aws --node-type c7g.8xlarge --num-apis 1,1,1,1,1 --num-validators 2,2,2,2,2 --region us-east-1,eu-west-1,us-west-1,ap-northeast-2,ca-central-1 --use-static-ip=false --enable-monitoring=true --default-validator-params --custom-vm-repo-url="https://www.github.com/ava-labs/hypersdk" --custom-vm-branch $VM_COMMIT --custom-vm-build-script="examples/morpheusvm/scripts/build.sh" --custom-subnet=true --subnet-genesis="${TMPDIR}/morpheusvm.genesis" --subnet-config="${TMPDIR}/morpheusvm.genesis" --chain-config="${TMPDIR}/morpheusvm.config" --node-config="${TMPDIR}/node.config" --remote-cli-version $CLI_COMMIT
 
 echo "Cluster info: (~/.avalanche-cli/nodes/inventories/${CLUSTER}/clusterInfo.yaml)"
@@ -187,4 +187,9 @@ sleep $SLEEP_DUR
 # TODO: only start using again once test is run async and logs are collected using Loki (stream isn't reliable)
 $TMPDIR/avalanche node loadtest ${CLUSTER} ${VMID} --loadTestRepoURL="https://github.com/ava-labs/hypersdk/commit/${VM_COMMIT}" --loadTestBuildCmd="cd /home/ubuntu/hypersdk/examples/morpheusvm; CGO_CFLAGS=\"-O -D__BLST_PORTABLE__\" go build -o ~/simulator ./cmd/morpheus-cli" --loadTestCmd="exit"
 
-echo -e "${YELLOW}To run load test, ssh into monitoring instance run this command:${NC} /home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
+echo -e "${YELLOW}To run load test, ssh into monitoring instance run these commands:${NC}"
+echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
+echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=ee11a050c75f0f47390f8ed98ab29fbce8c1f820b0245af56e1cb484a80c8022d77899baf0059747b8b685cfe62296f85f67083dc0bf8d2fab24c5ee3a7563b9"
+echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=34214e27f4c7d17315694968e37d999b848bb7b0bc95d679eb8163cf516c15dd9e77d9ebe639f9bece4260f4cce91ccf365dbce726da4299ff5a1b1ed31b339e"
+echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=ba09c65939a182f46879fcda172eabe9844d1f0a835a00c905dd2fa11b61a50ff38c9fdaef41e74730a732208284f2199fcd2f31779942662139884ca3f97a77"
+echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=3e5ab8a792187c8fa0a87e2171058d9a0c16ca07bc35c2cfb5e2132078fe18c0a70d00475d1e86ef32bb22397e47722c420dd4caf157400b83d9262af6bf0af5"
