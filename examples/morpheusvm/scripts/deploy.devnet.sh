d4a8f91080d56d51ec05b53d5bac1035a8324a3c
Patrick O'Grady <prohb125@gmail.com>
2024-03-19 09:11:03 -0700

    remove fast start

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -53,8 +53,8 @@ go build -v -o "${TMPDIR}"/morpheus-cli ./cmd/morpheus-cli
 # morpheus1qp52zjc3ul85309xn9stldfpwkseuth5ytdluyl7c5mvsv7a4fc76g6c4w4: 34214e27f4c7d17315694968e37d999b848bb7b0bc95d679eb8163cf516c15dd9e77d9ebe639f9bece4260f4cce91ccf365dbce726da4299ff5a1b1ed31b339e
 # morpheus1qzqjp943t0tudpw06jnvakdc0y8w790tzk7suc92aehjw0epvj93s0uzasn: ba09c65939a182f46879fcda172eabe9844d1f0a835a00c905dd2fa11b61a50ff38c9fdaef41e74730a732208284f2199fcd2f31779942662139884ca3f97a77
 # morpheus1qz97wx3vl3upjuquvkulp56nk20l3jumm3y4yva7v6nlz5rf8ukty8fh27r: 3e5ab8a792187c8fa0a87e2171058d9a0c16ca07bc35c2cfb5e2132078fe18c0a70d00475d1e86ef32bb22397e47722c420dd4caf157400b83d9262af6bf0af5
-EPOCH_DURATION=180000
-VALIDITY_WINDOW=60000
+EPOCH_DURATION=60000
+VALIDITY_WINDOW=59000
 MIN_BLOCK_GAP=1000
 MIN_UNIT_PRICE="1,1,1,1,1"
 MAX_CHUNK_UNITS="1800000,15000,15000,15000,15000"
@@ -180,8 +180,19 @@ do
 done
 
 # Start load test on dedicated machine
-# TODO: only start using again once test is run async and logs are collected using Loki (stream isn't reliable)
+START_TIME=$(date +%s)
 $TMPDIR/avalanche node loadtest ${CLUSTER} ${VMID} --loadTestRepoURL="https://github.com/ava-labs/hypersdk/commit/${VM_COMMIT}" --loadTestBuildCmd="cd /home/ubuntu/hypersdk/examples/morpheusvm; CGO_CFLAGS=\"-O -D__BLST_PORTABLE__\" go build -o ~/simulator ./cmd/morpheus-cli" --loadTestCmd="exit"
+END_TIME=$(date +%s)
+TIME_TAKEN=$((END_TIME - START_TIME))
+
+# Wait for epoch initialization
+SLEEP_DUR=$(($EPOCH_DURATION / 1000 * 2))
+if [ $TIME_TAKEN -lt $SLEEP_DUR ]; then
+  SLEEP_DUR=$(($SLEEP_DUR - $TIME_TAKEN))
+  echo "Waiting for epoch initialization ($SLEEP_DUR seconds)..."
+  echo -e "${YELLOW}We use a shorter EPOCH_DURATION to speed up devnet startup. In a production environment, this should be set to a longer value.${NC}"
+  sleep $SLEEP_DUR
+fi
 
 echo -e "${YELLOW}To run load test, ssh into monitoring instance run these commands:${NC}"
 echo "/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
