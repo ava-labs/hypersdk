806c5e7762c2f7a331ff3e2e74f26ce05c13b84b
Patrick O'Grady <prohb125@gmail.com>
2024-03-26 07:48:31 -0700

    state initializer done

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -1,34 +1,50 @@
 package merkle
 
 import (
+	"context"
+
+	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/utils/maybe"
 	"github.com/ava-labs/avalanchego/x/merkledb"
 	"github.com/ava-labs/hypersdk/smap"
 )
 
-type Merkle struct {
-	state *smap.SMap[[]byte]
+const (
+	stateInitialSize   = 10_000_000
+	pendingInitialSize = 1_000_000
+)
 
+type Merkle struct {
+	state   *smap.SMap[[]byte]
 	mdb     merkledb.MerkleDB
 	pending map[string]maybe.Maybe[[]byte]
 }
 
-func NewMerkle() (*Merkle, error) {
-	vm.stateDB, err = merkledb.New(ctx, vm.rawStateDB, merkledb.Config{
-		BranchFactor: vm.genesis.GetStateBranchFactor(),
+func New(db database.Database, vm VM, cfg Config) (*Merkle, error) {
+	mdb, err := merkledb.New(context.TODO(), db, merkledb.Config{
+		BranchFactor: cfg.GetStateBranchFactor(),
 		// RootGenConcurrency limits the number of goroutines
 		// that will be used across all concurrent root generations.
-		RootGenConcurrency:          uint(vm.config.GetRootGenerationCores()),
-		HistoryLength:               uint(vm.config.GetStateHistoryLength()),
-		ValueNodeCacheSize:          uint(vm.config.GetValueNodeCacheSize()),
-		IntermediateNodeCacheSize:   uint(vm.config.GetIntermediateNodeCacheSize()),
-		IntermediateWriteBufferSize: uint(vm.config.GetStateIntermediateWriteBufferSize()),
-		IntermediateWriteBatchSize:  uint(vm.config.GetStateIntermediateWriteBatchSize()),
-		Reg:                         merkleRegistry,
+		RootGenConcurrency:          cfg.GetRootGenerationCores(),
+		HistoryLength:               cfg.GetStateHistoryLength(),
+		ValueNodeCacheSize:          cfg.GetValueNodeCacheSize(),
+		IntermediateNodeCacheSize:   cfg.GetIntermediateNodeCacheSize(),
+		IntermediateWriteBufferSize: cfg.GetStateIntermediateWriteBufferSize(),
+		IntermediateWriteBatchSize:  cfg.GetStateIntermediateWriteBatchSize(),
+		Reg:                         vm.GetMerkleRegistry(),
 		TraceLevel:                  merkledb.InfoTrace,
-		Tracer:                      vm.tracer,
+		Tracer:                      vm.GetTracer(),
 	})
+	if err != nil {
+		return nil, err
+	}
+	return &Merkle{
+		state:   smap.New[[]byte](stateInitialSize),
+		mdb:     mdb,
+		pending: make(map[string]maybe.Maybe[[]byte], pendingInitialSize),
+	}, nil
 }
 
 func (m *Merkle) Close() error {
+	return m.mdb.Close()
 }
