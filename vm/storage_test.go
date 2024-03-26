525fbd28df8e09cf49b2c5dae584f89de57f91d3
Patrick O'Grady <prohb125@gmail.com>
2024-03-25 21:47:55 -0700

    add sync to test

--- vm/storage_test.go
+++ vm/storage_test.go
@@ -26,94 +26,98 @@ func randBytes() []byte {
 }
 
 func BenchmarkMerkleDB(b *testing.B) {
-	// Setup DB
-	tdir := b.TempDir()
-	pdb, _, err := pebble.New(tdir, pebble.NewDefaultConfig())
-	if err != nil {
-		b.Fatal(err)
-	}
-	db, err := merkledb.New(context.TODO(), pdb, merkledb.Config{
-		BranchFactor:                merkledb.BranchFactor16,
-		RootGenConcurrency:          uint(runtime.NumCPU()),
-		HistoryLength:               0,
-		ValueNodeCacheSize:          128 * units.MiB,
-		IntermediateWriteBufferSize: 128 * units.MiB,
-		IntermediateWriteBatchSize:  128 * units.MiB,
-		Tracer:                      trace.Noop,
-	})
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	// Run experiments
-	keys := make([]string, 10_000_000)
-	sizeArr := []int{1_000, 10_000, 100_000, 1_000_000, 10_000_000}
-	for s, size := range sizeArr {
-		// Determine new item count
-		last := 0
-		if s > 0 {
-			last = sizeArr[s-1]
-		}
-
-		// Add keys to DB
-		ops := make(map[string]maybe.Maybe[[]byte], size-last)
-		for j := last; j < size; j++ {
-			keys[j] = string(randBytes())
-			ops[keys[j]] = maybe.Some(randBytes())
-		}
-		view, err := db.NewView(context.TODO(), merkledb.ViewChanges{MapOps: ops})
+	for _, sync := range []bool{true, false} {
+		// Setup DB
+		tdir := b.TempDir()
+		cfg := pebble.NewDefaultConfig()
+		cfg.Sync = sync
+		pdb, _, err := pebble.New(tdir, cfg)
 		if err != nil {
 			b.Fatal(err)
 		}
-		if err := view.CommitToDB(context.TODO()); err != nil {
+		db, err := merkledb.New(context.TODO(), pdb, merkledb.Config{
+			BranchFactor:                merkledb.BranchFactor16,
+			RootGenConcurrency:          uint(runtime.NumCPU()),
+			HistoryLength:               0,
+			ValueNodeCacheSize:          128 * units.MiB,
+			IntermediateWriteBufferSize: 128 * units.MiB,
+			IntermediateWriteBatchSize:  128 * units.MiB,
+			Tracer:                      trace.Noop,
+		})
+		if err != nil {
 			b.Fatal(err)
 		}
 
-		// Run through ops
-		for _, keyOps := range []int{1_000, 5_000, 10_000, 25_000, 50_000, 75_000, 100_000, 250_000, 500_000, 1_000_000} {
-			if keyOps > size {
-				continue
+		// Run experiments
+		keys := make([]string, 10_000_000)
+		sizeArr := []int{1_000, 10_000, 100_000, 1_000_000, 10_000_000}
+		for s, size := range sizeArr {
+			// Determine new item count
+			last := 0
+			if s > 0 {
+				last = sizeArr[s-1]
 			}
-			b.Run(fmt.Sprintf("keys=%d_ops=%d", size, keyOps), func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					// Initialize sampler (ensure not just re-setting the same keys)
-					s := sampler.NewUniform()
-					s.Initialize(uint64(keyOps))
 
-					// Generate new MapOps
-					ops = make(map[string]maybe.Maybe[[]byte], keyOps)
-					for j := 0; j < keyOps; j++ {
-						idx, err := s.Next()
+			// Add keys to DB
+			ops := make(map[string]maybe.Maybe[[]byte], size-last)
+			for j := last; j < size; j++ {
+				keys[j] = string(randBytes())
+				ops[keys[j]] = maybe.Some(randBytes())
+			}
+			view, err := db.NewView(context.TODO(), merkledb.ViewChanges{MapOps: ops})
+			if err != nil {
+				b.Fatal(err)
+			}
+			if err := view.CommitToDB(context.TODO()); err != nil {
+				b.Fatal(err)
+			}
+
+			// Run through ops
+			for _, keyOps := range []int{1_000, 5_000, 10_000, 25_000, 50_000, 75_000, 100_000, 250_000, 500_000, 1_000_000} {
+				if keyOps > size {
+					continue
+				}
+				b.Run(fmt.Sprintf("sync=%t_keys=%d_ops=%d", sync, size, keyOps), func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						// Initialize sampler (ensure not just re-setting the same keys)
+						s := sampler.NewUniform()
+						s.Initialize(uint64(keyOps))
+
+						// Generate new MapOps
+						ops = make(map[string]maybe.Maybe[[]byte], keyOps)
+						for j := 0; j < keyOps; j++ {
+							idx, err := s.Next()
+							if err != nil {
+								b.Fatal(err)
+							}
+							ops[keys[idx]] = maybe.Some(randBytes())
+						}
+
+						// Create view, commit, get root
+						view, err = db.NewView(context.TODO(), merkledb.ViewChanges{MapOps: ops})
 						if err != nil {
 							b.Fatal(err)
 						}
-						ops[keys[idx]] = maybe.Some(randBytes())
+						if err := view.CommitToDB(context.TODO()); err != nil {
+							b.Fatal(err)
+						}
+						if _, err := db.GetMerkleRoot(context.TODO()); err != nil {
+							b.Fatal(err)
+						}
 					}
-
-					// Create view, commit, get root
-					view, err = db.NewView(context.TODO(), merkledb.ViewChanges{MapOps: ops})
-					if err != nil {
-						b.Fatal(err)
-					}
-					if err := view.CommitToDB(context.TODO()); err != nil {
-						b.Fatal(err)
-					}
-					if _, err := db.GetMerkleRoot(context.TODO()); err != nil {
-						b.Fatal(err)
-					}
-				}
-			})
+				})
+			}
 		}
-	}
 
-	// Remove created files to ensure we don't run into any error logs
-	if err := db.Close(); err != nil {
-		b.Fatal(err)
-	}
-	if err := pdb.Close(); err != nil {
-		b.Fatal(err)
-	}
-	if err := os.RemoveAll(tdir); err != nil {
-		b.Fatal(err)
+		// Remove created files to ensure we don't run into any error logs
+		if err := db.Close(); err != nil {
+			b.Fatal(err)
+		}
+		if err := pdb.Close(); err != nil {
+			b.Fatal(err)
+		}
+		if err := os.RemoveAll(tdir); err != nil {
+			b.Fatal(err)
+		}
 	}
 }
