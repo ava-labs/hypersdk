1ece1d4c0be7904694440afe6f88b83fba0fbcd8
Patrick O'Grady <prohb125@gmail.com>
2024-03-26 13:21:28 -0700

    add min capacity cmd

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -33,7 +33,7 @@ CLI_COMMIT=v1.4.3-rc.2
 cd $TMPDIR
 git clone https://github.com/ava-labs/avalanche-cli
 cd avalanche-cli
-git checkout ea273893e6d33933257a26fa76349c30695efd76
+git checkout 1e7f5b37e4dbcb4471d28b45d8b43f7b73f75313
 ./scripts/build.sh
 mv ./bin/avalanche "${TMPDIR}/avalanche"
 cd $pw
@@ -199,6 +199,6 @@ fi
 
 # Start load test on dedicated machine
 echo "Starting load test"
-$TMPDIR/avalanche node loadtest start "default" ${CLUSTER} ${VMID} --region eu-west-1 --aws --node-type c7gn.8xlarge --load-test-repo="https://github.com/ava-labs/hypersdk" --load-test-branch=$VM_COMMIT --load-test-build-cmd="cd /home/ubuntu/hypersdk/examples/morpheusvm; CGO_CFLAGS=\"-O -D__BLST_PORTABLE__\" go build -o ~/simulator ./cmd/morpheus-cli" --load-test-cmd="/home/ubuntu/simulator spam run ed25519 --accounts=10000 --txs-per-second=100000 --step-size=1000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
+$TMPDIR/avalanche node loadtest start "default" ${CLUSTER} ${VMID} --region eu-west-1 --aws --node-type c7gn.8xlarge --load-test-repo="https://github.com/ava-labs/hypersdk" --load-test-branch=$VM_COMMIT --load-test-build-cmd="cd /home/ubuntu/hypersdk/examples/morpheusvm; CGO_CFLAGS=\"-O -D__BLST_PORTABLE__\" go build -o ~/simulator ./cmd/morpheus-cli" --load-test-cmd="/home/ubuntu/simulator spam run ed25519 --accounts=1000000 --txs-per-second=100000 --min-capacity=5000 --step-size=1000 --conns-per-host=10 --cluster-info=/home/ubuntu/clusterInfo.yaml --private-key=323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7"
 echo -e "${YELLOW}To stop load test, run:${NC} \"${TMPDIR}/avalanche node loadtest stop ${CLUSTER} --load-test=\"default\"\""
 echo -e "${YELLOW}To monitor load test, visit grafana dashboard (URL logged above)${NC}"
