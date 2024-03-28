529b6b2787f7c917ec5d79c929497eb3b2093800
Patrick O'Grady <prohb125@gmail.com>
2024-03-28 11:04:02 -0700

    re-inject BLS

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+
 # Set console colors
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
