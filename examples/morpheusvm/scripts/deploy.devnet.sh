7b6861e6dd26d3768384aa36118ccfb6aeccf32b
Patrick O'Grady <prohb125@gmail.com>
2024-04-03 11:16:20 -0700

    further limit

--- examples/morpheusvm/scripts/deploy.devnet.sh
+++ examples/morpheusvm/scripts/deploy.devnet.sh
@@ -158,7 +158,7 @@ cat <<EOF > "${TMPDIR}"/node.config
   "consensus-on-accept-gossip-validator-size":"10",
   "consensus-on-accept-gossip-peer-size":"10",
   "network-compression-type":"zstd",
-  "consensus-app-concurrency":"512",
+  "consensus-app-concurrency":"256",
   "profile-continuous-enabled":true,
   "profile-continuous-freq":"1m",
   "http-host":"",
