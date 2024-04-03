35c7936cbcc9d92a61fea39cfc1feafa4e5988fa
William Law <williamlaw.wtl@gmail.com>
2024-04-03 10:45:44 -0400

    switch statements

--- executor/executor.go
+++ executor/executor.go
@@ -6,6 +6,7 @@ package executor
 import (
 	"sync"
 	"sync/atomic"
+	"fmt"
 
 	// "github.com/ava-labs/avalanchego/utils/set"
 
@@ -31,7 +32,12 @@ type Executor struct {
 	done      bool
 	completed int
 	tasks     map[int]*task
-	nodes     map[string]int
+	nodes     map[string]*node
+}
+
+type node struct {
+	id int 
+	isAllocateWrite bool
 }
 
 // New creates a new [Executor].
@@ -40,7 +46,7 @@ func New(items, concurrency int, metrics Metrics) *Executor {
 		metrics:    metrics,
 		stop:       make(chan struct{}),
 		tasks:      make(map[int]*task, items),
-		nodes:      make(map[string]int, items*2), // TODO: tune this
+		nodes:      make(map[string]*node, items*2), // TODO: tune this
 		executable: make(chan *task, items),       // ensure we don't block while holding lock
 	}
 	e.wg.Add(concurrency)
@@ -69,6 +75,7 @@ func (e *Executor) runWorker() {
 			if !ok {
 				return
 			}
+			fmt.Printf("a tid %v\n", t.id)
 			if err := t.f(); err != nil {
 				e.stopOnce.Do(func() {
 					e.err = err
@@ -77,22 +84,30 @@ func (e *Executor) runWorker() {
 				return
 			}
 
+			fmt.Printf("b tid %v\n", t.id)
+			//fmt.Printf("tid %v | t.blocking %v\n", t.id, len(t.blocking))
 			e.l.Lock()
 			for _, bt := range t.blocking {
-				if bt.dependencies.Add(-1) > 0 {
+				//fmt.Printf("tid %v | id %v | executed %v | dep %v | b %v\n", t.id, bt.id, bt.executed, bt.dependencies.Load(), bt.blocking)
+				if bt.dependencies.Load() > 0 && bt.dependencies.Add(-1) > 0 {
 					continue
 				}
-				e.executable <- bt
+				if !bt.executed {
+					//fmt.Printf("len %v\n", len(e.executable))
+					e.executable <- bt	
+				}
 			}
 			t.blocking = nil // free memory
 			t.executed = true
 			e.completed++
 			if e.done && e.completed == len(e.tasks) {
+				fmt.Printf("this sholdn't print\n")
 				// We will close here if there are unexecuted tasks
 				// when we call [Wait].
 				close(e.executable)
 			}
 			e.l.Unlock()
+			fmt.Printf("c tid %v\n", t.id)
 		case <-e.stop:
 			return
 		}
@@ -115,19 +130,29 @@ func (e *Executor) Run(keys state.Keys, f func() error) {
 	e.tasks[id] = t
 
 	// Record dependencies
-	//previousDependencies := set.NewSet[int](len(keys))
 	for k, v := range keys {
-		latest, ok := e.nodes[k]
+		n, ok := e.nodes[k]
 		if ok {
-			lt := e.tasks[latest]
+			lt := e.tasks[n.id]
 			if !lt.executed {
-				if v.Has(state.Allocate) || v.Has(state.Write) {
+				switch {
+				case v == state.Read && !n.isAllocateWrite:
+					lt.blocking[id] = t
+					continue
+				/*case v == state.Read && n.isAllocateWrite:
 					t.dependencies.Add(int64(1))
+					lt.blocking[id] = t
+				case (v.Has(state.Allocate) || v.Has(state.Write)) && !n.isAllocateWrite:
+					// blocked by all reads
+					t.dependencies.Add(int64(len(lt.blocking)))
+					lt.blocking[id] = t
+				case (v.Has(state.Allocate) || v.Has(state.Write)) && n.isAllocateWrite:
+					t.dependencies.Add(int64(1))
+					lt.blocking[id] = t*/			
 				}
-				lt.blocking[id] = t
 			}
 		}
-		e.nodes[k] = id
+		e.nodes[k] = &node{id: id, isAllocateWrite: v.Has(state.Allocate) || v.Has(state.Write)}
 	}
 
 	if t.dependencies.Load() > 0 {
