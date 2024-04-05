242da7e51842a199544b50b4ef82dbb9f2d22a8e
Patrick O'Grady <prohb125@gmail.com>
2024-04-04 17:51:56 -0700

    test corruption check

--- appenddb/appenddb_test.go
+++ appenddb/appenddb_test.go
@@ -2,8 +2,10 @@ package appenddb
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/stretchr/testify/require"
@@ -53,4 +55,22 @@ func TestAppendDB(t *testing.T) {
 	v, err = db.Get("hello")
 	require.NoError(err)
 	require.Equal([]byte("world"), v)
+
+	// Corrupt file
+	require.NoError(db.Close())
+	f, err := os.OpenFile(filepath.Join(baseDir, "0"), os.O_RDWR, os.ModeAppend)
+	require.NoError(err)
+	_, err = f.WriteString("corrupt")
+	require.NoError(err)
+	require.NoError(f.Close())
+
+	// Restart
+	db, last, err = New(logger, baseDir, 100)
+	require.NoError(err)
+	require.Equal(ids.Empty, last)
+
+	// Get
+	v, err = db.Get("hello")
+	require.ErrorIs(err, database.ErrNotFound)
+	require.NoError(db.Close())
 }
