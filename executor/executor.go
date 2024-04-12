e761ccd369f7342d529084adf3e462f522b98490
Patrick O'Grady <prohb125@gmail.com>
2024-04-12 13:03:15 +0100

    need to set higher base dependencies

--- executor/executor.go
+++ executor/executor.go
@@ -14,6 +14,8 @@ import (
 	uatomic "go.uber.org/atomic"
 )
 
+const baseDependencies = 1_000_000
+
 // Executor sequences the concurrent execution of
 // tasks with arbitrary conflicts on-the-fly.
 //
@@ -60,7 +62,7 @@ func (e *Executor) work() {
 	}
 }
 
-type blockingRequest struct {
+type requester struct {
 	t     *task
 	perms state.Permissions
 }
@@ -68,12 +70,12 @@ type blockingRequest struct {
 type task struct {
 	id      int
 	f       func() error
-	readers []*task
+	reading []*task
 
-	l        sync.Mutex
-	executed bool
-	blocking map[int]*blockingRequest // next to be executed
-	reading  map[int]*task            // using after execution
+	l          sync.Mutex
+	executed   bool
+	requesters map[int]*requester
+	readers    map[int]*task
 
 	dependencies atomic.Int64
 }
@@ -87,23 +89,26 @@ func (e *Executor) runTask(t *task) {
 		return
 	}
 
+	// Execute the task
 	if err := t.f(); err != nil {
 		e.err.CompareAndSwap(nil, err)
 		return
 	}
 
-	// Clear readers
-	for _, rt := range t.readers {
+	// Notify other tasks that we are done reading
+	// them (if we only require non-exclusive access)
+	for _, rt := range t.reading {
 		rt.l.Lock()
-		delete(rt.reading, t.id)
+		delete(rt.readers, t.id)
 		rt.l.Unlock()
 	}
+	t.reading = nil
 
-	// Nodify blocking and mark as reading if needed
+	// Nodify requesters that they can execute
 	t.l.Lock()
-	for _, bt := range t.blocking {
-		// Mark this task as reading, even if it isn't
-		// executable yet.
+	for _, bt := range t.requesters {
+		// As soon as we clear a requester, make sure we mark
+		// that they are now reading us.
 		if bt.perms == state.Read {
 			t.reading[bt.t.id] = bt.t
 		}
@@ -114,7 +119,7 @@ func (e *Executor) runTask(t *task) {
 		}
 		e.executable <- bt.t
 	}
-	t.blocking = nil // free memory
+	t.requesters = nil // free memory
 	t.executed = true
 	t.l.Unlock()
 }
@@ -132,19 +137,19 @@ func (e *Executor) Run(keys state.Keys, f func() error) {
 	t := &task{
 		id:      id,
 		f:       f,
-		readers: []*task{},
+		reading: []*task{},
 
-		blocking: make(map[int]*blockingRequest),
-		reading:  make(map[int]*task),
+		requesters: make(map[int]*requester),
+		readers:    make(map[int]*task),
 	}
 
 	// Add fake dependencies to ensure we don't execute the task
 	// before we are finished enqueuing all dependencies.
 	//
-	// We can't have more than 1 dependency per key, so this effectively
-	// ensures we don't get to 0 before the iteration is finished.
-	dependenciesRemaining := int64(len(keys) + 1)
-	t.dependencies.Add(dependenciesRemaining)
+	// We can have more than 1 dependency per key (in the case that there
+	// are many readers for a single key), so we set this higher than we ever
+	// expect to see.
+	t.dependencies.Add(baseDependencies)
 
 	// Record dependencies
 	dependencies := set.NewSet[int](len(keys))
@@ -152,47 +157,54 @@ func (e *Executor) Run(keys state.Keys, f func() error) {
 		exclusive := v.Has(state.Allocate) || v.Has(state.Write)
 		lt, ok := e.nodes[k]
 		if ok {
-			lt.l.Lock()
+			// If we don't need exclusive access to a key, make sure
+			// to mark that we are reading it.
+			if !exclusive {
+				t.reading = append(t.reading, lt)
+			}
 
 			// Handle the case where the dependency hasn't been executed yet
+			lt.l.Lock()
 			if !lt.executed {
-				// Provide the blocking task with enough
-				// information to update its [reading] map
-				// after it is executed.
-				lt.blocking[id] = &blockingRequest{t, v}
+				lt.requesters[id] = &requester{t, v}
 				lt.l.Unlock()
-
-				// Update the latest node if we neeed exclusive access
-				// to the key.
 				dependencies.Add(lt.id)
+
+				// If we need exclusive access to the node, we update
+				// [nodes] to ensure anyone else that needs access to the key
+				// after us can only do so after we are done.
 				if exclusive {
 					e.nodes[k] = t
-				} else {
-					t.readers = append(t.readers, lt)
 				}
 				continue
 			}
 
-			// Handle the case where the dependency has already been executed
-			if exclusive {
-				for _, rt := range lt.reading {
-					// TODO: May cause a deadlock here
-					rt.l.Lock()
-					rt.blocking[id] = &blockingRequest{t, v}
-					rt.l.Unlock()
-					dependencies.Add(rt.id)
-				}
-				e.nodes[k] = t
-			} else {
-				lt.reading[id] = t
-				t.readers = append(t.readers, lt)
+			// Handle the case where we need to read something that has already been executed
+			if !exclusive {
+				lt.readers[id] = t
+				lt.l.Unlock()
+				continue
 			}
+
+			// Handle the case where the dependency has already been executed and we need exclusive access
+			//
+			// We are guaranteed that any [reader] has not been executed yet, so we can safely put a dependency
+			// on these readers.
+			for _, rt := range lt.readers {
+				rt.l.Lock()
+				rt.requesters[id] = &requester{t, v}
+				rt.l.Unlock()
+				dependencies.Add(rt.id)
+			}
+			e.nodes[k] = t
 			lt.l.Unlock()
+			continue
 		}
+		e.nodes[k] = t
 	}
 
 	// Adjust dependency traker and execute if necessary
-	extraDependencies := dependenciesRemaining - int64(dependencies.Len())
+	extraDependencies := baseDependencies - int64(dependencies.Len())
 	if t.dependencies.Add(-extraDependencies) > 0 {
 		if e.metrics != nil {
 			e.metrics.RecordBlocked()
