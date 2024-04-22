f604320f4321d0c5fc4b8268129e67ef572bd0b5
francois <hello@franfran.dev>
2024-04-22 18:02:05 +0200

    apply review changes: call root on view, nits

--- merkle/merkle_test.go
+++ merkle/merkle_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ava-labs/avalanchego/trace"
 	"github.com/ava-labs/avalanchego/utils/units"
 	"github.com/ava-labs/avalanchego/x/merkledb"
+	"github.com/stretchr/testify/require"
 )
 
 var resRoot ids.ID
@@ -19,43 +20,41 @@ var resErr error
 
 func BenchmarkMerkleTxRoot(b *testing.B) {
 	for _, size := range []int{10, 100, 1000, 10000} {
-    ctx := context.TODO()
-    tracer := trace.Noop
-    merkleItems := make([][]byte, 0, size)
-    for i := 0; i < size; i++ {
-      item := make([]byte, 32)
-      _, err := rand.Read(item)
-      if err != nil {
-        b.Fatal(err)
-      }
-      merkleItems = append(merkleItems, item)
-    }
+		ctx := context.TODO()
+		tracer := trace.Noop
+		merkleItems := make([][]byte, 0, size)
+		for i := 0; i < size; i++ {
+			item := make([]byte, 32)
+			_, err := rand.Read(item)
+			require.NoError(b, err)
+			merkleItems = append(merkleItems, item)
+		}
 
-    var root ids.ID
-    var db merkledb.MerkleDB
-    var err error
+		var root ids.ID
+		var db merkledb.MerkleDB
+		var err error
 
-    defaultConfig := merkledb.Config{
-      BranchFactor:              merkledb.BranchFactor16,
-      HistoryLength:             100,
-      IntermediateNodeCacheSize: units.MiB,
-      ValueNodeCacheSize:        units.MiB,
-      Tracer:                    tracer,
-    }
+		defaultConfig := merkledb.Config{
+			BranchFactor:              merkledb.BranchFactor16,
+			HistoryLength:             100,
+			IntermediateNodeCacheSize: units.MiB,
+			ValueNodeCacheSize:        units.MiB,
+			Tracer:                    tracer,
+		}
 
 		b.Run(strconv.Itoa(size), func(b *testing.B) {
 			for n := 0; n < b.N; n++ {
 				for i := 0; i < size; i++ {
-          root, db, err = GenerateMerkleRoot(ctx, defaultConfig, tracer, merkleItems, false)
+					root, db, err = GenerateMerkleRoot(ctx, defaultConfig, merkleItems, false)
 				}
 			}
 		})
 
-    // avoid compiler optimizations to cancel out the bench
-    resRoot = root
-    resDb = db
-    resErr = err
+		// avoid compiler optimizations to cancel out the bench
+		resRoot = root
+		resDb = db
+		resErr = err
 	}
 
-  b.ReportAllocs()
+	b.ReportAllocs()
 }
