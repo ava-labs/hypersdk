5c84c690baa4c1a0014fae325a172248d77cddb8
William Law <williamlaw.wtl@gmail.com>
2024-04-24 17:01:45 -0400

    fix client rpc error

--- rpc/jsonrpc_client.go
+++ rpc/jsonrpc_client.go
@@ -170,7 +170,7 @@ func (cli *JSONRPCClient) GenerateTransactionManual(
 
 	// Build transaction
 	actionRegistry, authRegistry := parser.Registry()
-	tx := chain.NewTx(base, action)
+	tx := chain.NewTx(base, []chain.Action{action})
 	tx, err := tx.Sign(authFactory, actionRegistry, authRegistry)
 	if err != nil {
 		return nil, nil, fmt.Errorf("%w: failed to sign transaction", err)
