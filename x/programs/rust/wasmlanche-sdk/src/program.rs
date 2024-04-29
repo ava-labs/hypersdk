76f8132d777ca44687a48e34e09416642dc59564
Franfran <51274081+iFrostizz@users.noreply.github.com>
2024-04-29 14:30:13 +0200

    Cached state values in the program state (#840)
    * add cache
    
    * implement cache reads
    
    * actually cache reads
    
    * implement cache delete and handle borsch errors
    
    * use only one cache map
    
    * remove flushed bool
    
    * root of transactions by stateless merkledb
    
    * make root generation a function
    
    * preallocate memory for merkle array and consumebytes flag
    
    * add <*.code-workspace> to .gitignore and remove it from git commit
    
    * move root generation func to merkle package, tx root by items of [txID + result]
    
    * rebase & blk marshal/unmarshal & merkleroot to ids.ID
    
    * write benches for the merkle package
    
    * use crypto/rand, fix var name, report allocs
    
    * put the 10k bench back
    
    * pass config by parameter
    
    * happy clippy
    
    * borrow V
    
    * add TODO
    
    * Revert "pass config by parameter"
    
    This reverts commit 4aec58931b749fd9d4865c779d9ea43fead5694e.
    
    * Revert "put the 10k bench back"
    
    This reverts commit 058d7e70bb9007e407d8bbb578cae19c33f31cd1.
    
    * Revert "use crypto/rand, fix var name, report allocs"
    
    This reverts commit 214005b440230fa9f40ce353cb341d4b065e1f3c.
    
    * Revert "write benches for the merkle package"
    
    This reverts commit 07993bf41a7e206f8041dfce6f08aba05c81f33d.
    
    * Revert "rebase & blk marshal/unmarshal & merkleroot to ids.ID"
    
    This reverts commit 74428360e2bad4a6689688d4d70e9251105b1274.
    
    * Revert "move root generation func to merkle package, tx root by items of [txID + result]"
    
    This reverts commit e5519601c14267b5ea13f49cf7ac81e4d9d77415.
    
    * Revert "add <*.code-workspace> to .gitignore and remove it from git commit"
    
    This reverts commit ce0028973ea65c323238533972a53af4d75facc6.
    
    * Revert "preallocate memory for merkle array and consumebytes flag"
    
    This reverts commit 68e49b643185013da13b2528e7d0dea9d835858d.
    
    * Revert "make root generation a function"
    
    This reverts commit aa44f975003e03a628c5cbe0cdb25d7d33a4cb73.
    
    * Revert "pass config by parameter"
    
    This reverts commit 4aec58931b749fd9d4865c779d9ea43fead5694e.
    
    * Revert "move root generation func to merkle package, tx root by items of [txID + result]"
    
    This reverts commit e5519601c14267b5ea13f49cf7ac81e4d9d77415.
    
    * Revert "preallocate memory for merkle array and consumebytes flag"
    
    This reverts commit 68e49b643185013da13b2528e7d0dea9d835858d.
    
    * Revert "make root generation a function"
    
    This reverts commit aa44f975003e03a628c5cbe0cdb25d7d33a4cb73.
    
    * merge main!
    
    * merge imports
    
    ---------
    
    Co-authored-by: bianyuanop <chen.me.nan@gmail.com>
    Co-authored-by: Richard Pringle <richard.pringle@avalabs.org>

--- x/programs/rust/wasmlanche-sdk/src/program.rs
+++ x/programs/rust/wasmlanche-sdk/src/program.rs
@@ -1,5 +1,8 @@
+use std::hash::Hash;
+
 use borsh::{BorshDeserialize, BorshSerialize};
 
+use crate::state::Key;
 use crate::{memory::to_host_ptr, state::Error as StateError, state::State, Params};
 
 /// Represents the current Program in the context of the caller. Or an external
@@ -25,7 +28,10 @@ impl Program {
     /// Returns a State object that can be used to interact with persistent
     /// storage exposed by the host.
     #[must_use]
-    pub fn state(&self) -> State {
+    pub fn state<K>(&self) -> State<K>
+    where
+        K: Into<Key> + Hash + PartialEq + Eq + Clone,
+    {
         State::new(Program::new(*self.id()))
     }
 
