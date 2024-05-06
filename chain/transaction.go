c95fa4ad24bfe6f19384be4727fd31d1f2ae0d48
William Law <williamlaw.wtl@gmail.com>
2024-05-06 11:36:58 -0400

    do sponsorkeys after action iteration

--- chain/transaction.go
+++ chain/transaction.go
@@ -112,15 +112,19 @@ func (t *Transaction) StateKeys(sm StateManager) (state.Keys, error) {
 	// Verify the formatting of state keys passed by the controller
 	for i, action := range t.Actions {
 		actionKeys := action.StateKeys(t.Auth.Actor(), codec.CreateLID(uint8(i), t.id))
-		sponsorKeys := sm.SponsorStateKeys(t.Auth.Sponsor())
-		for _, m := range []state.Keys{actionKeys, sponsorKeys} {
-			for k, v := range m {
-				if !keys.Valid(k) {
-					return nil, ErrInvalidKeyValue
-				}
-				stateKeys.Add(k, v)
+		for k, v := range actionKeys {
+			if !keys.Valid(k) {
+				return nil, ErrInvalidKeyValue
 			}
+			stateKeys.Add(k, v)
 		}
+	}
+	sponsorKeys := sm.SponsorStateKeys(t.Auth.Sponsor())
+	for k, v := range sponsorKeys {
+		if !keys.Valid(k) {
+			return nil, ErrInvalidKeyValue
+		}
+		stateKeys.Add(k, v)
 	}
 
 	// Cache keys if called again
