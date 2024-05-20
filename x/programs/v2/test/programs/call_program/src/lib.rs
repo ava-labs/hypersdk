fdabdfae4e4922ce144f014f7e0108610dda73a0
David Boehm <david.boehm@avalabs.org>
2024-05-20 09:42:45 -0400

    rust lint

--- x/programs/v2/test/programs/call_program/src/lib.rs
+++ x/programs/v2/test/programs/call_program/src/lib.rs
@@ -8,7 +8,7 @@ pub fn get_value(_: Context) -> i64 {
 
 #[public]
 pub fn get_value_external(_: Context, target: Program, max_units: i64) -> i64 {
-    let v : Vec<Param> = vec![];
+    let v: Vec<Param> = vec![];
     target
         .call_function("get_value", &v.into_iter().collect(), max_units)
         .unwrap()
