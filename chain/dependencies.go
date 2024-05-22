1dae036e8570038d40ef231106b40704392af2aa
Patrick O'Grady <prohb125@gmail.com>
2024-05-21 21:13:32 -0400

    remove MaxComputeUnits

--- chain/dependencies.go
+++ chain/dependencies.go
@@ -220,13 +220,9 @@ type Object interface {
 type Action interface {
 	Object
 
-	// MaxComputeUnits is the maximum amount of compute a given [Action] could use. This is
-	// used to determine whether the [Action] can be included in a given block and to compute
-	// the required fee to execute.
-	//
-	// Developers should make every effort to bound this as tightly to the actual max so that
-	// users don't need to have a large balance to call an [Action] (must prepay fee before execution).
-	MaxComputeUnits(Rules) uint64
+	// ComputeUnits is the amount of compute required to call [Execute]. This is used to determine
+	// whether the [Action] can be included in a given block and to compute the required fee to execute.
+	ComputeUnits(Rules) uint64
 
 	// StateKeysMaxChunks is used to estimate the fee a transaction should pay. It includes the max
 	// chunks each state key could use without requiring the state keys to actually be provided (may
@@ -257,7 +253,7 @@ type Action interface {
 		timestamp int64,
 		actor codec.Address,
 		actionID ids.ID,
-	) (computeUnits uint64, outputs [][]byte, err error)
+	) (outputs [][]byte, err error)
 }
 
 type Auth interface {
