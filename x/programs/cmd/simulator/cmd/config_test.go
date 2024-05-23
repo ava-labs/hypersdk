016858bed6e8c357682219c808e3bc4a4614aa0e
Richard Pringle <richard.pringle@avalabs.org>
2024-05-23 11:53:54 -0400

    Improve config table test in simulator

--- x/programs/cmd/simulator/cmd/config_test.go
+++ x/programs/cmd/simulator/cmd/config_test.go
@@ -14,32 +14,34 @@ import (
 func TestValidateAssertion(t *testing.T) {
 	require := require.New(t)
 	tests := []struct {
+		name      string
 		actual    int64
-		assertion Require
+		assertion *Require
 		expected  bool
 		wantErr   error
 	}{
-		{5, Require{ResultAssertion{Operator: string(NumericGt), Value: "3"}}, true, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericLt), Value: "10"}}, true, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericEq), Value: "5"}}, true, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericNe), Value: "3"}}, true, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericGt), Value: "10"}}, false, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericLt), Value: "2"}}, false, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericGe), Value: "5"}}, true, nil},
-		{5, Require{ResultAssertion{Operator: string(NumericGe), Value: "1"}}, true, nil},
-		{5, Require{}, false, strconv.ErrSyntax},
+		{"IsGreaterThan", 5, &Require{ResultAssertion{Operator: string(NumericGt), Value: "3"}}, true, nil},
+		{"IsNotGreaterThan", 5, &Require{ResultAssertion{Operator: string(NumericGt), Value: "10"}}, false, nil},
+		{"IsLessThan", 5, &Require{ResultAssertion{Operator: string(NumericLt), Value: "10"}}, true, nil},
+		{"IsNotLessThan", 5, &Require{ResultAssertion{Operator: string(NumericLt), Value: "2"}}, false, nil},
+		{"IsEqualTo", 5, &Require{ResultAssertion{Operator: string(NumericEq), Value: "5"}}, true, nil},
+		{"IsNotEqual", 5, &Require{ResultAssertion{Operator: string(NumericNe), Value: "3"}}, true, nil},
+		{"IsGreaterThanOrEqualToSame", 5, &Require{ResultAssertion{Operator: string(NumericGe), Value: "5"}}, true, nil},
+		{"IsGreaterThanOrEqualToSmaller", 5, &Require{ResultAssertion{Operator: string(NumericGe), Value: "1"}}, true, nil},
+		{"ParseNothingFails", 5, &Require{}, false, strconv.ErrSyntax},
 	}
 
 	for _, tt := range tests {
-		actual, err := borsh.Serialize(tt.actual)
-		require.NoError(err)
-		assertion := tt.assertion
-		result, err := validateAssertion(actual, &assertion)
-		if tt.wantErr != nil {
-			require.ErrorIs(err, tt.wantErr)
-		} else {
+		t.Run(tt.name, func(*testing.T) {
+			actual, err := borsh.Serialize(tt.actual)
 			require.NoError(err)
-		}
-		require.Equal(tt.expected, result)
+			result, err := validateAssertion(actual, tt.assertion)
+			if tt.wantErr != nil {
+				require.ErrorIs(err, tt.wantErr)
+			} else {
+				require.NoError(err)
+			}
+			require.Equal(tt.expected, result)
+		})
 	}
 }
