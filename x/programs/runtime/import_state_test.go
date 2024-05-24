ccdb3e54cfe511cfa3c9d07f01c7697dbf0c1191
David Boehm <david.boehm@avalabs.org>
2024-05-24 12:37:59 -0400

    cleanup

--- x/programs/runtime/import_state_test.go
+++ x/programs/runtime/import_state_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/ava-labs/hypersdk/x/programs/test"
 )
 
-func TestImportStatePutGetRemove(t *testing.T) {
+func TestImportStatePutGet(t *testing.T) {
 	require := require.New(t)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -32,19 +32,79 @@ func TestImportStatePutGetRemove(t *testing.T) {
 	valueBytes, err := borsh.Serialize(int64(10))
 	require.NoError(err)
 
-	result, err := runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "write", Params: valueBytes, Fuel: 10000000})
+	result, err := runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "put", Params: valueBytes, Fuel: 10000000})
 	require.NoError(err)
 	require.Nil(result)
 
-	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "read", Params: nil, Fuel: 10000000})
+	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "get", Params: nil, Fuel: 10000000})
 	require.NoError(err)
 	require.Equal(valueBytes, result)
+}
 
-	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "remove", Params: nil, Fuel: 10000000})
+func TestImportStateRemove(t *testing.T) {
+	require := require.New(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	runtime := NewRuntime(
+		NewConfig(),
+		logging.NoLog{},
+		test.Loader{ProgramName: "state_access"})
+
+	state := test.NewTestDB()
+	programID := ids.GenerateTestID()
+
+	valueBytes, err := borsh.Serialize(int64(10))
+	require.NoError(err)
+
+	result, err := runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "put", Params: valueBytes, Fuel: 10000000})
+	require.NoError(err)
+	require.Nil(result)
+
+	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "delete", Params: nil, Fuel: 10000000})
 	require.NoError(err)
 	require.Equal(append([]byte{1}, valueBytes...), result)
 
-	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "read", Params: nil, Fuel: 10000000})
+	result, err = runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "get", Params: nil, Fuel: 10000000})
 	require.NoError(err)
-	require.Equal(valueBytes, result)
+	require.Equal(nil, result)
+}
+
+func TestImportStateDeleteMissingKey(t *testing.T) {
+	require := require.New(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	runtime := NewRuntime(
+		NewConfig(),
+		logging.NoLog{},
+		test.Loader{ProgramName: "state_access"})
+
+	state := test.NewTestDB()
+	programID := ids.GenerateTestID()
+
+	result, err := runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "delete", Params: nil, Fuel: 10000000})
+	require.NoError(err)
+	require.Equal([]byte{0}, result)
+}
+
+func TestImportStateGetMissingKey(t *testing.T) {
+	require := require.New(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	runtime := NewRuntime(
+		NewConfig(),
+		logging.NoLog{},
+		test.Loader{ProgramName: "state_access"})
+
+	state := test.NewTestDB()
+	programID := ids.GenerateTestID()
+
+	result, err := runtime.CallProgram(ctx, &CallInfo{ProgramID: programID, State: state, FunctionName: "get", Params: nil, Fuel: 10000000})
+	require.NoError(err)
+	require.Equal([]byte{0}, result)
 }
--- x/programs/test/programs/state_access/src/lib.rs
+++ x/programs/test/programs/state_access/src/lib.rs
@@ -8,29 +8,29 @@ pub enum StateKeys {
 
 /// Initializes the program with a name, symbol, and total supply.
 #[public]
-pub fn write(context: Context<StateKeys>, value:i64) {
+pub fn put(context: Context<StateKeys>, value:i64) {
     let Context { program, .. } = context;
     program
-    .state()
-    .store(StateKeys::State, &value)
-    .expect("failed to store state");
+        .state()
+        .store(StateKeys::State, &value)
+        .expect("failed to store state");
 }
 
 
 #[public]
-pub fn read(context: Context<StateKeys>) -> i64 {
+pub fn get(context: Context<StateKeys>) -> i64 {
     let Context { program, .. } = context;
     program
-    .state()
-    .get(StateKeys::State)
-    .expect("failed to get state")
+        .state()
+        .get(StateKeys::State)
+        .expect("failed to get state")
 }
 
 #[public]
-pub fn remove(context: Context<StateKeys>) -> Option<i64> {
+pub fn delete(context: Context<StateKeys>) -> Option<i64> {
     let Context { program, .. } = context;
     program
-    .state()
-    .delete(StateKeys::State)
-    .expect("failed to get state")
+        .state()
+        .delete(StateKeys::State)
+        .expect("failed to get state")
 }
