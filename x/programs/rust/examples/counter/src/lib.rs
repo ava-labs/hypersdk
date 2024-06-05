08c955b79a6481e175a2800280cd3cb2dd7a1ad2
David Boehm <91908103+dboehm-avalabs@users.noreply.github.com>
2024-06-05 11:53:01 -0400

    [x/programs] Cleanup Program Calls (#1013)
    * Cleanup Program Calls

--- x/programs/rust/examples/counter/src/lib.rs
+++ x/programs/rust/examples/counter/src/lib.rs
@@ -162,6 +162,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "simulator can't pass other program params currently"]
     fn external_call() {
         let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
 
