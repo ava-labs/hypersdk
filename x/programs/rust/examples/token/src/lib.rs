813c491d58cd6f7cbc06e4729a32c035087ba727
Francois Hardrouyere <hello@franfran.dev>
2024-06-11 17:14:47 +0200

    add allowance

--- x/programs/rust/examples/token/src/lib.rs
+++ x/programs/rust/examples/token/src/lib.rs
@@ -1,9 +1,16 @@
+use std::cmp::Ordering;
+
 use borsh::{BorshDeserialize, BorshSerialize};
 #[cfg(not(feature = "bindings"))]
 use wasmlanche_sdk::Context;
-use wasmlanche_sdk::{public, state_keys, types::Address};
+use wasmlanche_sdk::{public, state_keys, types::Address, Program};
 
 const INITIAL_SUPPLY: u64 = 123456789;
+
+#[derive(BorshSerialize)]
+pub enum TokenError {
+    InsufficientBalance,
+}
 
 /// The program state keys.
 #[state_keys]
@@ -16,6 +23,8 @@ pub enum StateKeys {
     Symbol,
     /// The balance of the token by address. Key prefix 0x3 + address.
     Balance(Address),
+    /// The allowance given from an address to an address. Key prefix 0x4 + address + address.
+    Allowance(Address, Address),
 }
 
 /// Initializes the program with a name, symbol, and total supply.
@@ -83,55 +92,105 @@ fn mint_to_internal(
     context
 }
 
-/// Burn the token from the recipient.
 #[public]
-pub fn burn_from(context: Context<StateKeys>, recipient: Address) -> u64 {
-    let Context { program, .. } = context;
-    program
-        .state()
-        .delete::<u64>(StateKeys::Balance(recipient))
-        .expect("failed to burn recipient tokens")
-        .expect("recipient balance not found")
+pub fn burn(context: Context<StateKeys>, amount: u64) -> Result<(), TokenError> {
+    let Context { program, actor, .. } = context;
+    _burn_from(&program, actor, amount)
+}
+
+#[public]
+pub fn burn_from(
+    context: Context<StateKeys>,
+    from: Address,
+    amount: u64,
+) -> Result<(), TokenError> {
+    let Context { program, actor, .. } = context;
+    let allowance = _allowance(&program, from, actor);
+    if allowance >= amount {
+        _set_allowance(&program, from, actor, allowance - amount);
+        _burn_from(&program, from, amount)?;
+    }
+    Ok(())
+}
+
+fn _burn_from(program: &Program<StateKeys>, from: Address, amount: u64) -> Result<(), TokenError> {
+    let balance = _balance_of(program, from);
+    match balance.cmp(&amount) {
+        Ordering::Less => return Err(TokenError::InsufficientBalance),
+        Ordering::Equal => {
+            program
+                .state()
+                .delete::<u64>(StateKeys::Balance(from))
+                .expect("state corrupt");
+        }
+        Ordering::Greater => program
+            .state()
+            .store(StateKeys::Balance(from), &(balance - amount))
+            .expect("state corrupt"),
+    }
+    Ok(())
+}
+
+#[public]
+pub fn transfer(context: Context<StateKeys>, to: Address, amount: u64) -> Result<(), TokenError> {
+    let Context { program, actor, .. } = context;
+    _transfer_from(&program, actor, to, amount)
 }
 
 /// Transfers balance from the sender to the recipient.
 #[public]
-pub fn transfer(
+pub fn transfer_from(
     context: Context<StateKeys>,
-    sender: Address,
-    recipient: Address,
+    from: Address,
+    to: Address,
     amount: u64,
-) -> bool {
+) -> Result<(), TokenError> {
     let Context { program, .. } = context;
-    assert_ne!(sender, recipient, "sender and recipient must be different");
+    let allowance = _allowance(&program, from, to);
+    if allowance >= amount {
+        _set_allowance(&program, from, to, allowance - amount);
+        _transfer_from(&program, from, to, amount)?;
+    }
+    Ok(())
+}
+
+fn _transfer_from(
+    program: &Program<StateKeys>,
+    from: Address,
+    to: Address,
+    amount: u64,
+) -> Result<(), TokenError> {
+    assert_ne!(from, to, "sender and recipient must be different");
 
     // ensure the sender has adequate balance
     let sender_balance = program
         .state()
-        .get::<u64>(StateKeys::Balance(sender))
+        .get::<u64>(StateKeys::Balance(from))
         .expect("state corrupt")
         .unwrap_or_default();
 
-    assert!(sender_balance >= amount, "invalid input");
+    if sender_balance < amount {
+        return Err(TokenError::InsufficientBalance);
+    }
 
     let recipient_balance = program
         .state()
-        .get::<u64>(StateKeys::Balance(recipient))
+        .get::<u64>(StateKeys::Balance(to))
         .expect("state corrupt")
         .unwrap_or_default();
 
     // update balances
     program
         .state()
-        .store(StateKeys::Balance(sender), &(sender_balance - amount))
+        .store(StateKeys::Balance(from), &(sender_balance - amount))
         .expect("failed to store balance");
 
     program
         .state()
-        .store(StateKeys::Balance(recipient), &(recipient_balance + amount))
+        .store(StateKeys::Balance(to), &(recipient_balance + amount))
         .expect("failed to store balance");
 
-    true
+    Ok(())
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -149,15 +208,45 @@ pub fn mint_to_many(context: Context<StateKeys>, minters: Vec<Minter>) -> bool {
     true
 }
 
-/// Gets the balance of the recipient.
 #[public]
-pub fn get_balance(context: Context<StateKeys>, recipient: Address) -> i64 {
+pub fn balance_of(context: Context<StateKeys>, recipient: Address) -> u64 {
     let Context { program, .. } = context;
+    _balance_of(&program, recipient)
+}
+
+pub fn _balance_of(program: &Program<StateKeys>, recipient: Address) -> u64 {
     program
         .state()
         .get(StateKeys::Balance(recipient))
         .expect("state corrupt")
         .unwrap_or_default()
+}
+
+#[public]
+pub fn approve(context: Context<StateKeys>, to: Address, amount: u64) {
+    let Context { program, actor } = context;
+    _set_allowance(&program, actor, to, amount);
+}
+
+#[public]
+pub fn allowance(context: Context<StateKeys>, from: Address, to: Address) -> u64 {
+    let Context { program, .. } = context;
+    _allowance(&program, from, to)
+}
+
+fn _allowance(program: &Program<StateKeys>, from: Address, to: Address) -> u64 {
+    program
+        .state()
+        .get(StateKeys::Allowance(from, to))
+        .expect("state corrupt")
+        .unwrap_or_default()
+}
+
+fn _set_allowance(program: &Program<StateKeys>, from: Address, to: Address, amount: u64) {
+    program
+        .state()
+        .store(StateKeys::Allowance(from, to), &amount)
+        .expect("state corrupt");
 }
 
 #[cfg(test)]
