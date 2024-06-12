4bb83150f02f4c72a528101223cf2bbae4a5da77
David Boehm <david.boehm@avalabs.org>
2024-06-12 09:41:05 -0400

    Update import_log_debug.go

--- x/programs/runtime/import_log_debug.go
+++ x/programs/runtime/import_log_debug.go
@@ -16,7 +16,7 @@ func NewLogModule() *ImportModule {
 	return &ImportModule{
 		Name: "log",
 		HostFunctions: map[string]HostFunction{
-			"write": {FuelCost: logCost, Function: FunctionNoOutput(func(_ *CallInfo, input []byte) error {
+			"write": {FuelCost: logCost, Function: FunctionNoOutput[[]byte](func(_ *CallInfo, input []byte) error {
 				_, err := fmt.Fprintf(os.Stderr, "%s\n", input)
 				return err
 			})},
