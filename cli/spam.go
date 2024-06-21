ebe274a72c326d932f5983483aad3266ba839f1b
Francois Hardrouyere <hello@franfran.dev>
2024-06-21 21:50:57 +0200

    fix modifier assignment

--- cli/spam.go
+++ cli/spam.go
@@ -471,8 +471,8 @@ type timeModifier struct {
 	Timestamp int64
 }
 
-func (t *timeModifier) Base(timestamp int64, _ ids.ID, _ uint64) {
-	t.Timestamp = timestamp
+func (t *timeModifier) Base(timestamp *int64, _ *ids.ID, _ *uint64) {
+	timestamp = &t.Timestamp
 }
 
 func startIssuer(cctx context.Context, issuer *txIssuer) {
