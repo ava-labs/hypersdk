8cd037ecb4f03098cf5a469eab8c2c193e9e6af1
David Boehm <david.boehm@avalabs.org>
2024-06-26 09:22:56 -0400

    Update lib.rs

--- x/programs/test/programs/deploy_program/src/lib.rs
+++ x/programs/test/programs/deploy_program/src/lib.rs
@@ -2,6 +2,5 @@ use wasmlanche_sdk::{public, types::Address, types::Id, Context};
 
 #[public]
 pub fn deploy(ctx: Context, program_id: Id) -> Address {
-    let Context { program, .. } = ctx;
-    program.deploy(program_id, &[]).unwrap()
+    ctx.program().deploy(program_id, &[]).unwrap()
 }
