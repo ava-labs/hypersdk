80d2535e643dc31da88f0e184a882631fb8f67c4
Francois Hardrouyere <hello@franfran.dev>
2024-07-02 18:41:26 +0200

    remove `call_function` changes

--- x/programs/test/programs/fuel/src/lib.rs
+++ x/programs/test/programs/fuel/src/lib.rs
@@ -7,5 +7,5 @@ pub fn get_fuel(ctx: Context) -> u64 {
 
 #[public]
 pub fn out_of_fuel(_: Context, target: Program) -> ExternalCallError {
-    target.call_function("get_fuel", &[], 0).unwrap_err()
+    target.call_function::<u64>("get_fuel", &[], 0).unwrap_err()
 }
