97f222f8c2bad6ebe4977b73da9722495f8c5253
Franfran <51274081+iFrostizz@users.noreply.github.com>
2024-07-05 14:51:50 +0200

    Add the ability to return raw bytes from `call_function` (#1079)
    * deserialize to an intermediate `DeferDeserialize` to be able to return
    either bytes or a type from `call_function`
    
    * lint
    
    * fmt
    
    * update doc example
    
    * add defer test
    
    * fix test
    
    * backoff unnecessary changes
    
    * update docs
    
    * nits
    
    * write unit tests, write docs, faillible `deserialize`
    
    * update docs

--- x/programs/rust/wasmlanche-sdk/src/program.rs
+++ x/programs/rust/wasmlanche-sdk/src/program.rs
@@ -6,11 +6,33 @@ use crate::{
     Gas,
 };
 use borsh::{BorshDeserialize, BorshSerialize};
-use std::{cell::RefCell, collections::HashMap};
+use std::{cell::RefCell, collections::HashMap, io::Read};
 use thiserror::Error;
 
+/// Defer deserialization from bytes
+/// <div class="warning">It is possible that this type performs multiple allocations during deserialization. It should be used sparingly.</div>
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub struct DeferDeserialize(Vec<u8>);
+
+impl DeferDeserialize {
+    /// # Errors
+    /// Returns a [`std::io::Error`] if there was an issue deserializing the value
+    pub fn deserialize<T: BorshDeserialize>(self) -> Result<T, std::io::Error> {
+        let Self(bytes) = self;
+        borsh::from_slice(&bytes)
+    }
+}
+
+impl BorshDeserialize for DeferDeserialize {
+    fn deserialize_reader<R: Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut inner = Vec::new();
+        reader.read_to_end(&mut inner)?;
+        Ok(Self(inner))
+    }
+}
+
 /// An error that is returned from call to public functions.
-#[derive(Error, Debug, BorshSerialize, BorshDeserialize)]
+#[derive(Error, Debug, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
 #[repr(u8)]
 #[non_exhaustive]
 #[borsh(use_discriminant = true)]
@@ -71,13 +93,13 @@ impl<K> Program<K> {
     /// # use wasmlanche_sdk::{types::Address, Program};
     /// #
     /// # let program_id = [0; Address::LEN];
-    /// # let target: Program<()> = borsh::from_slice(&program_id).expect("the program should deserialize");
+    /// # let target: Program<()> = borsh::from_slice(&program_id).unwrap();
     /// let increment = 10;
-    /// let params = borsh::to_vec(&increment).expect("failed to borsh serialize params");
+    /// let params = borsh::to_vec(&increment).unwrap();
     /// let max_units = 1000000;
-    /// target
-    ///     .call_function("call_with_param", &params, max_units)
-    ///     .unwrap()
+    /// let has_incremented: bool = target.call_function("increment", &params, max_units)?;
+    /// assert!(has_incremented);
+    /// # Ok::<(), wasmlanche_sdk::ExternalCallError>(())
     /// ```
     pub fn call_function<T: BorshDeserialize>(
         &self,
@@ -169,5 +191,22 @@ impl<K> BorshSerialize for CallProgramArgs<'_, K> {
         max_units.serialize(writer)?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DeferDeserialize;
+
+    #[test]
+    fn defer_bytes() {
+        type ExpectedType = u64;
+
+        let expected: ExpectedType = 42;
+        let serialized = borsh::to_vec(&expected).unwrap();
+        let deferred: DeferDeserialize = borsh::from_slice(&serialized).unwrap();
+        assert_eq!(deferred.0, serialized);
+        let actual = deferred.deserialize::<ExpectedType>().unwrap();
+        assert_eq!(actual, expected);
     }
 }
