438da871adb9f2b264f2f813a004699865062c6c
Francois Hardrouyere <hello@franfran.dev>
2024-07-08 17:25:38 +0200

    write timelock tests

--- x/programs/rust/examples/timelock/src/lib.rs
+++ x/programs/rust/examples/timelock/src/lib.rs
@@ -1,10 +1,9 @@
-use borsh::BorshSerialize;
+use borsh::{BorshDeserialize, BorshSerialize};
 #[cfg(not(feature = "bindings"))]
 use wasmlanche_sdk::Context;
-use wasmlanche_sdk::{public, state_keys};
-use wasmlanche_sdk::{ExternalCallError, Program};
+use wasmlanche_sdk::{public, state_keys, DeferDeserialize, ExternalCallError, Program};
 
-#[derive(Debug, BorshSerialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize, PartialEq)]
 pub enum Error {
     AlreadyScheduled,
     NotScheduled,
@@ -66,13 +65,14 @@ pub fn schedule(
 }
 
 #[public]
-pub fn execute(context: Context<StateKeys>, max_units: u64) -> Result<Vec<u8>, Error> {
-    let program = context.program;
+pub fn execute(context: Context<StateKeys>, max_units: u64) -> Result<DeferDeserialize, Error> {
+    let program = context.program();
 
     if program
         .state()
         .get::<bool>(StateKeys::Executed)
         .expect("state corrupt")
+        .is_some_and(|executed| executed)
     {
         return Err(Error::AlreadyExecuted);
     }
@@ -106,6 +106,150 @@ pub fn execute(context: Context<StateKeys>, max_units: u64) -> Result<Vec<u8>, E
         .unwrap();
 
     called_program
-        .call_function_bytes(&function, args.as_slice(), max_units)
+        .call_function(&function, args.as_slice(), max_units)
         .map_err(Error::ExternalCall)
+}
+
+#[public]
+pub fn next_execution(context: Context<StateKeys>) -> Option<u64> {
+    context
+        .program()
+        .state()
+        .get(StateKeys::Time)
+        .expect("state corrupt")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Error, LOCK_TIME};
+    use simulator::{Endpoint, Id, Step, TestContext};
+    use wasmlanche_sdk::DeferDeserialize;
+
+    const PROGRAM_PATH: &str = env!("PROGRAM_PATH");
+
+    #[test]
+    fn cannot_pre_execute() {
+        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
+
+        let program_id = simulator
+            .run_step(&Step::create_program(PROGRAM_PATH))
+            .unwrap()
+            .id;
+
+        let test_context = TestContext::from(program_id);
+
+        let res = simulator
+            .run_step(&Step {
+                endpoint: Endpoint::Execute,
+                method: "execute".to_string(),
+                max_units: u64::MAX,
+                params: vec![test_context.clone().into(), u64::MAX.into()],
+            })
+            .unwrap()
+            .result
+            .response::<Result<(), Error>>()
+            .unwrap();
+
+        assert_eq!(res, Err(Error::NotScheduled));
+    }
+
+    #[test]
+    fn schedule_in_the_future() {
+        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
+
+        let program_id = simulator
+            .run_step(&Step::create_program(PROGRAM_PATH))
+            .unwrap()
+            .id;
+
+        let mut test_context = TestContext::from(program_id);
+        let timestamp = 123456;
+        test_context.timestamp = timestamp;
+
+        simulator
+            .run_step(&Step {
+                endpoint: Endpoint::Execute,
+                method: "schedule".to_string(),
+                max_units: u64::MAX,
+                params: vec![
+                    test_context.clone().into(),
+                    Id::default().into(),
+                    String::new().into(),
+                    Vec::new().into(),
+                ],
+            })
+            .unwrap();
+
+        let next_execution: Option<u64> = simulator
+            .run_step(&Step {
+                endpoint: Endpoint::ReadOnly,
+                method: "next_execution".to_string(),
+                max_units: 0,
+                params: vec![test_context.into()],
+            })
+            .unwrap()
+            .result
+            .response()
+            .unwrap();
+
+        assert_eq!(next_execution, Some(timestamp + LOCK_TIME));
+    }
+
+    #[test]
+    fn delayed_execution() {
+        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
+
+        let program_id = simulator
+            .run_step(&Step::create_program(PROGRAM_PATH))
+            .unwrap()
+            .id;
+
+        let mut test_context = TestContext::from(program_id);
+        let timestamp = 123456;
+        test_context.timestamp = timestamp;
+
+        simulator
+            .run_step(&Step {
+                endpoint: Endpoint::Execute,
+                method: "schedule".to_string(),
+                max_units: u64::MAX,
+                params: vec![
+                    test_context.clone().into(),
+                    program_id.into(),
+                    String::from("next_execution").into(),
+                    Vec::new().into(),
+                ],
+            })
+            .unwrap();
+
+        let res = simulator
+            .run_step(&Step {
+                endpoint: Endpoint::Execute,
+                method: "execute".to_string(),
+                max_units: u64::MAX,
+                params: vec![test_context.clone().into(), 1000000u64.into()],
+            })
+            .unwrap()
+            .result
+            .response::<Result<(), Error>>()
+            .unwrap();
+
+        assert_eq!(res, Err(Error::TooEarly));
+
+        test_context.timestamp = timestamp + LOCK_TIME;
+
+        let res = simulator
+            .run_step(&Step {
+                endpoint: Endpoint::Execute,
+                method: "execute".to_string(),
+                max_units: u64::MAX,
+                params: vec![test_context.clone().into(), 1000000u64.into()],
+            })
+            .unwrap()
+            .result
+            .response::<Result<DeferDeserialize, Error>>()
+            .unwrap();
+
+        assert!(res.is_ok());
+    }
 }
