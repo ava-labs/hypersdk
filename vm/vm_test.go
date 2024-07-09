b2da6c8cbdcdb0eea355dfd659215c6b702c1903
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-07-09 16:38:32 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/ava-labs/hypersdk/cache"
 	"github.com/ava-labs/hypersdk/chain"
-	"github.com/ava-labs/hypersdk/config"
 	"github.com/ava-labs/hypersdk/emap"
 	"github.com/ava-labs/hypersdk/mempool"
 	"github.com/ava-labs/hypersdk/trace"
