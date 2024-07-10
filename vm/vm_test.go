e91d85a918aca5a6d1b18f2e0059a08e5370a646
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-07-09 21:50:06 -0400

    remove mock controller
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/ava-labs/avalanchego/api/metrics"
+	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/database/memdb"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow"
@@ -15,13 +16,17 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
+	"github.com/ava-labs/hypersdk/builder"
 	"github.com/ava-labs/hypersdk/cache"
 	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/config"
 	"github.com/ava-labs/hypersdk/emap"
+	"github.com/ava-labs/hypersdk/gossiper"
 	"github.com/ava-labs/hypersdk/mempool"
 	"github.com/ava-labs/hypersdk/trace"
 )
+
+var _ Controller = (*testController)(nil)
 
 func TestBlockCache(t *testing.T) {
 	require := require.New(t)
@@ -40,7 +45,10 @@ func TestBlockCache(t *testing.T) {
 	tracer, _ := trace.New(&trace.Config{Enabled: false})
 	bByID, _ := cache.NewFIFO[ids.ID, *chain.StatelessBlock](3)
 	bByHeight, _ := cache.NewFIFO[uint64, ids.ID](3)
-	controller := NewMockController(ctrl)
+
+	rules := chain.NewMockRules(ctrl)
+	rules.EXPECT().GetValidityWindow().Return(int64(60))
+
 	vm := VM{
 		snowCtx: &snow.Context{Log: logging.NoLog{}, Metrics: metrics.NewPrefixGatherer()},
 		config:  &config.Config{},
@@ -55,7 +63,9 @@ func TestBlockCache(t *testing.T) {
 		seen:           emap.NewEMap[*chain.Transaction](),
 		mempool:        mempool.New[*chain.Transaction](tracer, 100, 32, nil),
 		acceptedQueue:  make(chan *chain.StatelessBlock, 1024), // don't block on queue
-		c:              controller,
+		c: &testController{
+			rules: rules,
+		},
 	}
 
 	// Init metrics (called in [Accepted])
@@ -67,9 +77,6 @@ func TestBlockCache(t *testing.T) {
 	// put the block into the cache "vm.blocks"
 	// and delete from "vm.verifiedBlocks"
 	ctx := context.TODO()
-	rules := chain.NewMockRules(ctrl)
-	rules.EXPECT().GetValidityWindow().Return(int64(60))
-	controller.EXPECT().Rules(gomock.Any()).Return(rules)
 	vm.Accepted(ctx, blk)
 
 	// we have not set up any persistent db
@@ -77,4 +84,51 @@ func TestBlockCache(t *testing.T) {
 	blk2, err := vm.GetStatelessBlock(ctx, blkID)
 	require.NoError(err)
 	require.Equal(blk, blk2)
+}
+
+type testController struct {
+	rules chain.Rules
+}
+
+func (t *testController) Initialize(
+	*VM,
+	*snow.Context,
+	metrics.MultiGatherer,
+	[]byte,
+	[]byte,
+	[]byte,
+) (
+	config Config,
+	genesis Genesis,
+	builder builder.Builder,
+	gossiper gossiper.Gossiper,
+	vmDB database.Database,
+	stateDB database.Database,
+	handler Handlers,
+	actionRegistry chain.ActionRegistry,
+	authRegistry chain.AuthRegistry,
+	authEngines map[uint8]AuthEngine,
+	err error,
+) {
+	return nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil
+}
+
+func (t *testController) Rules(int64) chain.Rules {
+	return t.rules
+}
+
+func (*testController) StateManager() chain.StateManager {
+	return nil
+}
+
+func (*testController) Accepted(context.Context, *chain.StatelessBlock) error {
+	return nil
+}
+
+func (*testController) Rejected(context.Context, *chain.StatelessBlock) error {
+	return nil
+}
+
+func (*testController) Shutdown(context.Context) error {
+	return nil
 }
