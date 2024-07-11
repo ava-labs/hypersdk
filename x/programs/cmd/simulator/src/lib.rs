c1b58d7c4110d1a1b76478ac2363b1ed676f9d76
Richard Pringle <richard.pringle@avalabs.org>
2024-07-11 16:20:19 +0200

    Don't add unnecessary dependency (#1122)

--- x/programs/cmd/simulator/src/lib.rs
+++ x/programs/cmd/simulator/src/lib.rs
@@ -83,14 +83,44 @@ pub enum Key {
     Secp256r1(String),
 }
 
-#[derive(Clone, Debug, PartialEq, Default, Serialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Clone, Debug, PartialEq, Default)]
 #[non_exhaustive]
 pub struct TestContext {
     program_id: Id,
     pub actor: Address,
     pub height: u64,
     pub timestamp: u64,
+}
+
+impl Serialize for TestContext {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        #[derive(Serialize)]
+        #[serde(rename_all = "camelCase")]
+        struct BorrowedContext<'a> {
+            program_id: Id,
+            actor: &'a [u8],
+            height: u64,
+            timestamp: u64,
+        }
+
+        let Self {
+            program_id,
+            actor,
+            height,
+            timestamp,
+        } = self;
+
+        BorrowedContext {
+            program_id: *program_id,
+            actor: actor.as_ref(),
+            height: *height,
+            timestamp: *timestamp,
+        }
+        .serialize(serializer)
+    }
 }
 
 impl From<Id> for TestContext {
