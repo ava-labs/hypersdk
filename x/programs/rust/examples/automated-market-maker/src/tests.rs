e5a2dfc7ab87aa04aa72ef0f5a814d46783d6a49
samliok <samliok@stanford.edu>
2024-07-12 17:13:39 -0400

    testing boilerplate

--- x/programs/rust/examples/automated-market-maker/src/tests.rs
+++ x/programs/rust/examples/automated-market-maker/src/tests.rs
@@ -1,168 +1,53 @@
 
 #[cfg(test)]
 mod tests {
-    use simulator::{Endpoint, Key, Step, StepResponseError, TestContext};
+    use simulator::{Endpoint, Key, Step, StepResponseError, TestContext, Param};
     use wasmlanche_sdk::ExternalCallError;
 
     const PROGRAM_PATH: &str = env!("PROGRAM_PATH");
 
     #[test]
-    fn amm_integration_test() {
-        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
-
-        let owner = String::from("owner");
+    fn amm_init() {
         
-        let program_id = simulator
-            .run_step(&Step::create_program(PROGRAM_PATH))
-            .unwrap()
-            .id;
-
-        simulator
-            .run_step(&Step::create_key(Key::Ed25519(owner)))
-            .unwrap();
-
-        let test_context = TestContext::from(program_id);
-
-        let resp_err = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "remove_liquidity".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.clone().into(), 100000u64.into()],
-            })
-            .unwrap()
-            .result
-            .response::<(u64, u64)>()
-            .unwrap_err();
-
-        let StepResponseError::ExternalCall(call_err) = resp_err else {
-            panic!("wrong error returned");
-        };
-
-        assert!(matches!(call_err, ExternalCallError::CallPanicked));
-
-        let resp_err = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "swap".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.into(), 100000u64.into(), true.into()],
-            })
-            .unwrap()
-            .result
-            .response::<u64>()
-            .unwrap_err();
-
-        let StepResponseError::ExternalCall(call_err) = resp_err else {
-            panic!("wrong error returned");
-        };
-
-        assert!(matches!(call_err, ExternalCallError::CallPanicked));
     }
 
     #[test]
     fn add_liquidity_same_ratio() {
-        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
+        
+    }
 
-        let owner = String::from("owner");
-
-        let program_id = simulator
-            .run_step(&Step::create_program(PROGRAM_PATH))
-            .unwrap()
-            .id;
-
-        simulator
-            .run_step(&Step::create_key(Key::Ed25519(owner)))
-            .unwrap();
-
-        let test_context = TestContext::from(program_id);
-
-        let resp = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "add_liquidity".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.clone().into(), 1000u64.into(), 1000u64.into()],
-            })
-            .unwrap()
-            .result
-            .response::<u64>()
-            .unwrap();
-
-        assert_eq!(resp, 1000);
-
-        let resp = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "add_liquidity".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.into(), 1000u64.into(), 1001u64.into()],
-            })
-            .unwrap()
-            .result
-            .response::<u64>()
-            .unwrap_err();
-
-        let StepResponseError::ExternalCall(call_err) = resp else {
-            panic!("unexpected error");
-        };
-
-        assert!(matches!(call_err, ExternalCallError::CallPanicked));
+    #[test]
+    fn add_liquidity_different_ratio() {
+        
     }
 
     #[test]
     fn swap_changes_ratio() {
-        let mut simulator = simulator::ClientBuilder::new().try_build().unwrap();
+       
+    }
 
-        let owner = String::from("owner");
+    #[test]
+    fn swap_insufficient_funds() {
+        
+    }
 
-        let program_id = simulator
-            .run_step(&Step::create_program(PROGRAM_PATH))
-            .unwrap()
-            .id;
+    #[test]
+    fn swap_incorrect_ratio() {
+        
+    }
 
-        simulator
-            .run_step(&Step::create_key(Key::Ed25519(owner)))
-            .unwrap();
+    #[test]
+    fn swap_incorrect_token() {
+        
+    }
 
-        let test_context = TestContext::from(program_id);
+    #[test]
+    fn remove_liquidity() {
+        
+    }
 
-        let resp = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "add_liquidity".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.clone().into(), 1000u64.into(), 1000u64.into()],
-            })
-            .unwrap()
-            .result
-            .response::<u64>()
-            .unwrap();
-
-        assert_eq!(resp, 1000);
-
-        simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "swap".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.clone().into(), 10u64.into(), true.into()],
-            })
-            .unwrap();
-
-        let (amount_x, amount_y) = simulator
-            .run_step(&Step {
-                endpoint: Endpoint::Execute,
-                method: "remove_liquidity".to_string(),
-                max_units: u64::MAX,
-                params: vec![test_context.into(), 1000.into()],
-            })
-            .unwrap()
-            .result
-            .response::<(u64, u64)>()
-            .unwrap();
-
-        assert!(amount_x > 1000);
-        assert!(amount_y < 1000);
+    #[test]
+    fn remove_liquidity_insufficient_funds() {
+        
     }
 }
