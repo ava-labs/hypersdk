0611157a7ee3911a39aad89c3815aeaeb2235d0d
David Boehm <david.boehm@avalabs.org>
2024-07-12 08:17:12 -0400

    add send via call test

--- x/programs/runtime/import_balance_test.go
+++ x/programs/runtime/import_balance_test.go
@@ -14,6 +14,38 @@ import (
 	"github.com/ava-labs/hypersdk/x/programs/test"
 )
 
+func TestImportBalanceSendBalanceToAnotherProgram(t *testing.T) {
+	require := require.New(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	program := newTestProgram(ctx, "balance")
+	r := program.Runtime
+	stateManager := r.StateManager.(test.StateManager)
+	stateManager.Balances[program.Address] = 3
+
+	// create a new instance of the balance program
+	newInstanceAddress := codec.CreateAddress(0, ids.GenerateTestID())
+	programID := stateManager.AccountMap[program.Address]
+	r.StateManager.(test.StateManager).AccountMap[newInstanceAddress] = programID
+	stateManager.Balances[newInstanceAddress] = 0
+
+	// program 2 starts with 0 balance
+	result, err := r.CallProgram(newInstanceAddress, "balance")
+	require.NoError(err)
+	require.Equal(uint64(0), into[uint64](result))
+
+	// send 2 from program1 to program2, results in 1 being returned since that is the new balance of program 1
+	result, err = program.Call("send_via_call", newInstanceAddress, uint64(1000000), uint64(2))
+	require.NoError(err)
+	require.Equal(uint64(1), into[uint64](result))
+
+	// program 2 should now have 2 balance
+	result, err = program.WithActor(newInstanceAddress).Call("balance")
+	require.NoError(err)
+	require.Equal(uint64(2), into[uint64](result))
+}
+
 func TestImportBalanceGetBalance(t *testing.T) {
 	require := require.New(t)
 
--- x/programs/test/programs/balance/src/lib.rs
+++ x/programs/test/programs/balance/src/lib.rs
@@ -2,7 +2,8 @@ use wasmlanche_sdk::{
     balance::{get, send},
     public,
     types::Address,
-    Context,
+    types::Gas,
+    Context, Program,
 };
 
 #[public]
@@ -13,4 +14,11 @@ pub fn balance(ctx: Context) -> u64 {
 #[public]
 pub fn send_balance(_: Context, recipient: Address) -> bool {
     send(recipient, 1).is_ok()
+}
+
+#[public]
+pub fn send_via_call(_: Context, target: Program, max_units: Gas, value: u64) -> u64 {
+    target
+        .call_function("balance", &[], max_units, value)
+        .unwrap()
 }
