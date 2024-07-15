59cb318069a61577cca1f98d4369bef1cae1d2be
samliok <samliok@stanford.edu>
2024-07-15 11:10:52 -0400

    StateKey -> StateKeys

--- x/programs/rust/examples/token/src/lib.rs
+++ x/programs/rust/examples/token/src/lib.rs
@@ -4,7 +4,7 @@ use wasmlanche_sdk::{public, state_keys, types::Address, Program};
 
 /// The program state keys.
 #[state_keys]
-pub enum StateKey {
+pub enum StateKeys {
     /// The total supply of the token. Key prefix 0x0.
     TotalSupply,
     /// The name of the token. Key prefix 0x1.
@@ -20,17 +20,17 @@ pub enum StateKey {
 }
 
 // Returns the owner of the token
-pub fn get_owner(program: &Program<StateKey>) -> Address {
+pub fn get_owner(program: &Program<StateKeys>) -> Address {
     program
         .state()
-        .get::<Address>(StateKey::Owner)
+        .get::<Address>(StateKeys::Owner)
         .expect("failure")
         .unwrap()
 }
 
 // Checks if the caller is the owner of the token
 // If the caller is not the owner, the program will panic
-pub fn owner_check(program: &Program<StateKey>, actor: Address) {
+pub fn owner_check(program: &Program<StateKeys>, actor: Address) {
     assert!(
         get_owner(program) == actor,
         "caller is required to be owner"
@@ -39,54 +39,54 @@ pub fn owner_check(program: &Program<StateKey>, actor: Address) {
 
 /// Initializes the program with a name, symbol, and total supply.
 #[public]
-pub fn init(context: Context<StateKey>, name: String, symbol: String) {
+pub fn init(context: Context<StateKeys>, name: String, symbol: String) {
     let program = context.program();
     let actor = context.actor();
 
     program
         .state()
-        .store_by_key(StateKey::Owner, &actor)
+        .store_by_key(StateKeys::Owner, &actor)
         .expect("failed to store owner");
     // initialize the owner, name, and symbol
     program
         .state()
-        .store([(StateKey::Name, &name), (StateKey::Symbol, &symbol)])
+        .store([(StateKeys::Name, &name), (StateKeys::Symbol, &symbol)])
         .expect("failed to store owner");
 }
 
 /// Returns the total supply of the token.
 #[public]
-pub fn total_supply(context: Context<StateKey>) -> u64 {
+pub fn total_supply(context: Context<StateKeys>) -> u64 {
     let program = context.program();
     _total_supply(program)
 }
 
-fn _total_supply(program: &Program<StateKey>) -> u64 {
+fn _total_supply(program: &Program<StateKeys>) -> u64 {
     program
         .state()
-        .get(StateKey::TotalSupply)
+        .get(StateKeys::TotalSupply)
         .expect("failure")
         .unwrap_or_default()
 }
 
 /// Transfers balance from the token owner to the recipient.
 #[public]
-pub fn mint(context: Context<StateKey>, recipient: Address, amount: u64) -> bool {
+pub fn mint(context: Context<StateKeys>, recipient: Address, amount: u64) -> bool {
     let program = context.program();
     let actor = context.actor();
 
     owner_check(program, actor);
     let balance = program
         .state()
-        .get::<u64>(StateKey::Balance(recipient))
+        .get::<u64>(StateKeys::Balance(recipient))
         .expect("failed to get balance")
         .unwrap_or_default();
     let total_supply = _total_supply(program);
     program
         .state()
         .store([
-            (StateKey::Balance(recipient), &(balance + amount)),
-            (StateKey::TotalSupply, &(total_supply + amount)),
+            (StateKeys::Balance(recipient), &(balance + amount)),
+            (StateKeys::TotalSupply, &(total_supply + amount)),
         ])
         .expect("failed to store balance");
 
@@ -95,7 +95,7 @@ pub fn mint(context: Context<StateKey>, recipient: Address, amount: u64) -> bool
 
 /// Burn the token from the recipient.
 #[public]
-pub fn burn(context: Context<StateKey>, recipient: Address, value: u64) -> u64 {
+pub fn burn(context: Context<StateKeys>, recipient: Address, value: u64) -> u64 {
     let program = context.program();
     let actor = context.actor();
 
@@ -107,7 +107,7 @@ pub fn burn(context: Context<StateKey>, recipient: Address, value: u64) -> u64 {
 
     program
         .state()
-        .store_by_key::<u64>(StateKey::Balance(recipient), &new_amount)
+        .store_by_key::<u64>(StateKeys::Balance(recipient), &new_amount)
         .expect("failed to burn recipient tokens");
 
     new_amount
@@ -115,30 +115,30 @@ pub fn burn(context: Context<StateKey>, recipient: Address, value: u64) -> u64 {
 
 /// Gets the balance of the recipient.
 #[public]
-pub fn balance_of(context: Context<StateKey>, account: Address) -> u64 {
+pub fn balance_of(context: Context<StateKeys>, account: Address) -> u64 {
     let program = context.program();
     _balance_of(program, account)
 }
 
-fn _balance_of(program: &Program<StateKey>, account: Address) -> u64 {
+fn _balance_of(program: &Program<StateKeys>, account: Address) -> u64 {
     program
         .state()
-        .get(StateKey::Balance(account))
+        .get(StateKeys::Balance(account))
         .expect("failure")
         .unwrap_or_default()
 }
 
 /// Returns the allowance of the spender for the owner's tokens.
 #[public]
-pub fn allowance(context: Context<StateKey>, owner: Address, spender: Address) -> u64 {
+pub fn allowance(context: Context<StateKeys>, owner: Address, spender: Address) -> u64 {
     let program = context.program();
     _allowance(program, owner, spender)
 }
 
-pub fn _allowance(program: &Program<StateKey>, owner: Address, spender: Address) -> u64 {
+pub fn _allowance(program: &Program<StateKeys>, owner: Address, spender: Address) -> u64 {
     program
         .state()
-        .get::<u64>(StateKey::Allowance(owner, spender))
+        .get::<u64>(StateKeys::Allowance(owner, spender))
         .expect("failure")
         .unwrap_or_default()
 }
@@ -146,27 +146,27 @@ pub fn _allowance(program: &Program<StateKey>, owner: Address, spender: Address)
 /// Approves the spender to spend the owner's tokens.
 /// Returns true if the approval was successful.
 #[public]
-pub fn approve(context: Context<StateKey>, spender: Address, amount: u64) -> bool {
+pub fn approve(context: Context<StateKeys>, spender: Address, amount: u64) -> bool {
     let program = context.program();
     let actor = context.actor();
 
     program
         .state()
-        .store_by_key(StateKey::Allowance(actor, spender), &amount)
+        .store_by_key(StateKeys::Allowance(actor, spender), &amount)
         .expect("failed to store allowance");
     true
 }
 
 /// Transfers balance from the sender to the the recipient.
 #[public]
-pub fn transfer(context: Context<StateKey>, recipient: Address, amount: u64) -> bool {
+pub fn transfer(context: Context<StateKeys>, recipient: Address, amount: u64) -> bool {
     let program = context.program();
     let actor = context.actor();
     _transfer(program, actor, recipient, amount)
 }
 
 fn _transfer(
-    program: &Program<StateKey>,
+    program: &Program<StateKeys>,
     sender: Address,
     recipient: Address,
     amount: u64,
@@ -176,7 +176,7 @@ fn _transfer(
     // ensure the sender has adequate balance
     let sender_balance = program
         .state()
-        .get::<u64>(StateKey::Balance(sender))
+        .get::<u64>(StateKeys::Balance(sender))
         .expect("failed to get sender balance")
         .unwrap_or_default();
 
@@ -184,7 +184,7 @@ fn _transfer(
 
     let recipient_balance = program
         .state()
-        .get::<u64>(StateKey::Balance(recipient))
+        .get::<u64>(StateKeys::Balance(recipient))
         .expect("failed to store recipient balance")
         .unwrap_or_default();
 
@@ -192,8 +192,8 @@ fn _transfer(
     program
         .state()
         .store([
-            (StateKey::Balance(sender), &(sender_balance - amount)),
-            (StateKey::Balance(recipient), &(recipient_balance + amount)),
+            (StateKeys::Balance(sender), &(sender_balance - amount)),
+            (StateKeys::Balance(recipient), &(recipient_balance + amount)),
         ])
         .expect("failed to store sender balance");
 
@@ -204,7 +204,7 @@ fn _transfer(
 /// The caller must be the owner of the tokens or have an allowance.
 #[public]
 pub fn transfer_from(
-    context: Context<StateKey>,
+    context: Context<StateKeys>,
     sender: Address,
     recipient: Address,
     amount: u64,
@@ -222,7 +222,7 @@ pub fn transfer_from(
     program
         .state()
         .store_by_key(
-            StateKey::Allowance(sender, actor),
+            StateKeys::Allowance(sender, actor),
             &(total_allowance - amount),
         )
         .expect("failed to store allowance");
@@ -230,36 +230,36 @@ pub fn transfer_from(
 }
 
 #[public]
-pub fn transfer_ownership(context: Context<StateKey>, new_owner: Address) -> bool {
+pub fn transfer_ownership(context: Context<StateKeys>, new_owner: Address) -> bool {
     let program = context.program();
     let actor = context.actor();
 
     owner_check(program, actor);
     program
         .state()
-        .store_by_key(StateKey::Owner, &new_owner)
+        .store_by_key(StateKeys::Owner, &new_owner)
         .expect("failed to store owner");
     true
 }
 
 #[public]
 // grab the symbol of the token
-pub fn symbol(context: Context<StateKey>) -> String {
+pub fn symbol(context: Context<StateKeys>) -> String {
     let program = context.program();
     program
         .state()
-        .get::<String>(StateKey::Symbol)
+        .get::<String>(StateKeys::Symbol)
         .expect("failed to get symbol")
         .unwrap_or_default()
 }
 
 #[public]
 // grab the name of the token
-pub fn name(context: Context<StateKey>) -> String {
+pub fn name(context: Context<StateKeys>) -> String {
     let program = context.program();
     program
         .state()
-        .get::<String>(StateKey::Name)
+        .get::<String>(StateKeys::Name)
         .expect("failed to get name")
         .unwrap_or_default()
 }
