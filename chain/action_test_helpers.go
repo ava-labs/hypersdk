f189b563ea41826f5c17522a62a703479728e500
Francois Hardrouyere <hello@franfran.dev>
2024-07-15 19:29:02 +0200

    remove morpheusvm test, simplify suite

--- chain/action_test_helpers.go
+++ chain/action_test_helpers.go
@@ -23,56 +23,24 @@ type ActionTest struct {
 	Actor     codec.Address
 	ActionID  ids.ID
 
-	ExpectedOutputs   [][]byte
-	ExpectedErr       error
-	ContainsErrString string
+	ExpectedOutputs [][]byte
+	ExpectedErr     error
 }
 
 type ActionTestSuite struct {
 	Tests map[string]ActionTest
-
-	Setup    func()
-	Teardown func()
 }
 
 func (suite *ActionTestSuite) Run(t *testing.T) {
-	if suite.Setup != nil {
-		suite.Setup()
-	}
-
 	for testName := range suite.Tests {
 		t.Run(testName, func(t *testing.T) {
 			require := require.New(t)
 			test := suite.Tests[testName]
 
-			// Ensure that only one of outputs or err is set
-			if test.ExpectedOutputs != nil && (test.ExpectedErr != nil || len(test.ContainsErrString) > 0) {
-				t.Fatalf("ExpectedOutputs and ExpectedErr are set in test %s", testName)
-			}
-			if test.ExpectedOutputs == nil && test.ExpectedErr == nil && len(test.ContainsErrString) == 0 {
-				t.Fatalf("neither ExpectedOutputs nor ExpectedErr is set in test %s", testName)
-			}
-
 			output, err := test.Action.Execute(context.TODO(), test.Rules, test.State, test.Timestamp, test.Actor, test.ActionID)
 
-			if err != nil {
-				if test.ExpectedErr == nil && len(test.ContainsErrString) == 0 {
-					t.Fatalf("got unexpected error: %w", err)
-				}
-
-				if test.ExpectedErr != nil {
-					require.Equal(err, test.ExpectedErr)
-				}
-				if len(test.ContainsErrString) > 0 {
-					require.Contains(err.Error(), test.ContainsErrString)
-				}
-			}
-
+			require.ErrorIs(err, test.ExpectedErr)
 			require.Equal(output, test.ExpectedOutputs)
 		})
-	}
-
-	if suite.Teardown != nil {
-		suite.Teardown()
 	}
 }
