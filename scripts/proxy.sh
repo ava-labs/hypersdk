c133d55fa1fa4e9243aa943eed8cf7e4424ae507
marun <maru.newby@avalabs.org>
2024-07-17 21:06:00 -0700

    Extract shared scripts (#1161)
    * chore: rebase on top of master
    
    * refactor: shellcheck
    
    * fix: update mockgen version to pass ci checks
    
    * fix: build script merge
    
    * fix: resolve issues in tests.lint.sh
    
    * fix: resolve imports and shellcheck issues
    
    * fix: shellcheck issues
    
    * fix: downgrade golangci-lint to pass ci
    
    * fix: separate unit testing scripts
    
    * fix: ci issues
    
    * fix: ci issues
    
    * fix: tokenvm static analysis
    
    * fixup: Remove merge conflicts
    
    ---------
    
    Co-authored-by: vikinatora <vikinatora@gmail.com>

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -6,8 +6,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+HYPERSDK_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  cd .. && pwd
+)
+# shellcheck source=/scripts/common/utils.sh
+source "$HYPERSDK_PATH"/scripts/common/utils.sh
+
 echo "installing proxy"
 go install -v github.com/sensiblecodeio/tiny-ssl-reverse-proxy
+
+# alert the user if they do not have $GOPATH properly configured
+check_command tiny-ssl-reverse-proxy
 
 # TODO: add ability to provide a cert
 echo "starting proxy for ${VM_API} on :9090"
