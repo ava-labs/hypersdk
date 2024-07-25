09d84b92175ea5b82a787ce500b408d0c0adbf88
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-07-25 06:09:46 +0000

    taiwind

--- examples/morpheusvm/frontend/web_wallet/src/App.tsx
+++ examples/morpheusvm/frontend/web_wallet/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +8,9 @@ function App() {
   return (
     <>
       <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
