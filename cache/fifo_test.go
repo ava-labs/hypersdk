7df4e00c5c06e7010c4ac3943814cd7a1edddb58
Francois Hardrouyere <hello@franfran.dev>
2024-07-29 12:24:45 +0200

    simplify test table and create new test for empty cache

--- cache/fifo_test.go
+++ cache/fifo_test.go
@@ -10,66 +10,169 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFIFOCache(t *testing.T) {
+func TestFIFOCacheInsertion(t *testing.T) {
+	limit := 10
+
+	type put struct {
+		i      int
+		exists bool
+	}
+
+	type get struct {
+		i  int
+		ok bool
+	}
+
 	tests := []struct {
-		name     string
-		limit    int
-		maxVal   int
-		expected error
-		check    func(t *testing.T, cache *FIFO[int, int])
+		name string
+		ops  []interface{}
 	}{
 		{
-			name:     "empty cache fails",
-			limit:    0,
-			expected: errors.New("maxSize must be greater than 0"),
+			name: "inserting 10 elements works",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 20)
+				for i := 0; i < limit; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				return ops
+			}(),
 		},
 		{
-			name:   "inserting up to limit works",
-			limit:  10,
-			maxVal: 9,
+			name: "inserting after limit cleans first",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 23)
+				for i := 0; i < limit; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				ops[2*limit] = put{
+					i:      10,
+					exists: false,
+				}
+				ops[2*limit+1] = get{
+					i:  0,
+					ok: false,
+				}
+				ops[2*limit+2] = get{
+					i:  1,
+					ok: true,
+				}
+				return ops
+			}(),
 		},
 		{
-			name:   "inserting after limit cleans first",
-			limit:  10,
-			maxVal: 10,
-			check: func(t *testing.T, cache *FIFO[int, int]) {
-				require := require.New(t)
-				_, ok := cache.Get(0)
-				require.False(ok)
-				v, ok := cache.Get(1)
-				require.True(ok)
-				require.Equal(1, v)
-			},
+			name: "no elements removed when cache is exactly full",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 20)
+				for i := 0; i < 9; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				ops[2*9] = put{
+					i:      9,
+					exists: false,
+				}
+				ops[2*9+1] = get{
+					i:  9,
+					ok: true,
+				}
+				return ops
+			}(),
 		},
 		{
-			name:   "inserting before limit can insert again",
-			limit:  10,
-			maxVal: 8,
-			check: func(t *testing.T, cache *FIFO[int, int]) {
-				require := require.New(t)
-				exists := cache.Put(9, 9)
-				require.False(exists)
-				exists = cache.Put(10, 10)
-				require.False(exists)
-				_, ok := cache.Get(0)
-				require.False(ok)
-			},
+			name: "no elements removed when the cache is less than full",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 12)
+				for i := 0; i < 5; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				ops[2*5] = put{
+					i:      5,
+					exists: false,
+				}
+				ops[2*5+1] = get{
+					i:  0,
+					ok: true,
+				}
+				return ops
+			}(),
 		},
 		{
-			name:   "inserting existing value doesn't free value",
-			limit:  10,
-			maxVal: 9,
-			check: func(t *testing.T, cache *FIFO[int, int]) {
-				require := require.New(t)
-				v, ok := cache.Get(0)
-				require.True(ok)
-				require.Equal(0, v)
-				exists := cache.Put(0, 10)
-				require.True(exists)
-				v, ok = cache.Get(0)
-				require.True(ok)
-				require.Equal(10, v)
-			},
+			name: "inserting existing value when full doesn't free value",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 20)
+				for i := 0; i < limit; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				ops[2*9] = put{
+					i:      0,
+					exists: true,
+				}
+				ops[2*9+1] = get{
+					i:  0,
+					ok: true,
+				}
+				return ops
+			}(),
+		},
+		{
+			name: "elements removed in FIFO order when cache overfills",
+			ops: func() []interface{} {
+				ops := make([]interface{}, 40)
+				for i := 0; i < limit; i++ {
+					ops[2*i] = put{
+						i:      i,
+						exists: false,
+					}
+					ops[2*i+1] = get{
+						i:  i,
+						ok: true,
+					}
+				}
+				for i := 0; i < limit; i++ {
+					ops[2*(i+limit)] = put{
+						i:      (2 * (i + limit)),
+						exists: false,
+					}
+					ops[2*(i+limit)+1] = get{
+						ok: false,
+					}
+				}
+				return ops
+			}(),
 		},
 	}
 
@@ -77,23 +180,28 @@ func TestFIFOCache(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			require := require.New(t)
 
-			cache, err := NewFIFO[int, int](tt.limit)
-			require.Equal(tt.expected, err)
-			if err != nil {
-				return
-			}
+			cache, err := NewFIFO[int, int](limit)
+			require.NoError(err)
 
-			for i := 0; i <= tt.maxVal; i++ {
-				exists := cache.Put(i, i)
-				require.False(exists)
-				v, ok := cache.Get(i)
-				require.True(ok)
-				require.Equal(i, v)
-			}
-
-			if tt.check != nil {
-				tt.check(t, cache)
+			for _, op := range tt.ops {
+				if put, ok := op.(put); ok {
+					exists := cache.Put(put.i, put.i)
+					require.Equal(put.exists, exists)
+				} else if get, ok := op.(get); ok {
+					val, ok := cache.Get(get.i)
+					require.Equal(get.ok, ok)
+					require.Equal(get.i, val)
+				} else {
+					require.Fail("op can only be a put or a get")
+				}
 			}
 		})
 	}
+}
+
+func TestEmptyCacheFails(t *testing.T) {
+	require := require.New(t)
+	_, err := NewFIFO[int, int](0)
+	expectedErr := errors.New("maxSize must be greater than 0")
+	require.Equal(expectedErr, err)
 }
