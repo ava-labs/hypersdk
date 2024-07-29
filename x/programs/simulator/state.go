47a147c3fa0ebdf39788cefc4db519a458ca16ef
samliok <samliok@stanford.edu>
2024-07-29 15:03:17 -0400

    clean up state interface code

---
+++ x/programs/simulator/state.go
@@ -0,0 +1,10 @@
+package simulator
+
+// import "github.com/ava-labs/hypersdk/x/programs/runtime"
+
+// // ensure SimulatorStateManager implements StateManager
+// var _ runtime.StateManager = &SimulatorStateManager{}
+
+// type programStateManager struct {
+//    state *SimulatorState
+// }
--- x/programs/simulator/storage.go
+++ x/programs/simulator/storage.go
@@ -9,7 +9,6 @@ import "C"
 
 import (
 	"context"
-	"fmt"
 	"unsafe"
 
 	"github.com/ava-labs/hypersdk/state"
@@ -19,66 +18,45 @@ import (
 var _ state.Mutable = &SimulatorState{}
 
 type SimulatorState struct {
-   // this is a pointer to the state passed in from rust
-   // it points to the state object on the rust side of the simulator
-   statePtr unsafe.Pointer
+	// this is a pointer to the state passed in from rust
+	// it points to the state object on the rust side of the simulator
+	statePtr unsafe.Pointer
 
-   // this is ptr to the get function
-   getFunc GetStateCallbackType
+	// this is ptr to the get function
+	getFunc C.GetStateCallback
 
-   // this is ptr to the insert function
-   insertFunc InsertStateCallbackType
+	// this is ptr to the insert function
+	insertFunc C.InsertStateCallback
 
-   // this is a ptr to the delete function
-   removeFunc RemoveStateCallbackType
+	// this is a ptr to the delete function
+	removeFunc C.RemoveStateCallback
 }
 
 func NewSimulatorState(db unsafe.Pointer) *SimulatorState {
 	// convert unsafe pointer to C.Mutable
-	mutable := (*Mutable)(db)
-   return &SimulatorState{
-		statePtr: mutable.stateObj,
-		getFunc: mutable.get_value_callback,
+	mutable := (*C.Mutable)(db)
+	return &SimulatorState{
+		statePtr:   mutable.stateObj,
+		getFunc:    mutable.get_value_callback,
 		insertFunc: mutable.insert_callback,
 		removeFunc: mutable.remove_callback,
-   }
-
+	}
 }
 
 func (s *SimulatorState) GetValue(ctx context.Context, key []byte) ([]byte, error) {
-	val := GetCallbackWrapper(s.getFunc, s.statePtr, key)
-	fmt.Println("Value returned*******: ", val)
-   return []byte{}, nil
+	return s.getValue(key)
 }
 
 func (s *SimulatorState) Insert(ctx context.Context, key []byte, value []byte) error {
-	InsertCallbackWrapper(s.insertFunc, s.statePtr, key, value)
-	return nil
+	return s.insert(key, value)
 }
 
 func (s *SimulatorState) Remove(ctx context.Context, key []byte) error {
-	fmt.Println("Removing key: ", key)
-	RemoveCallbackWrapper(s.removeFunc, s.statePtr, key)
-	return  nil
+	return s.remove(key)
 }
 
-// // ensure SimulatorStateManager implements StateManager
-// var _ runtime.StateManager = &SimulatorStateManager{}
-
-// type programStateManager struct {
-//    state *SimulatorState
-// }
-
-
-
-
-
-type GetStateCallbackType = C.GetStateCallback
-type InsertStateCallbackType = C.InsertStateCallback
-type RemoveStateCallbackType = C.RemoveStateCallback
-type Mutable = C.Mutable
-// In C, a function argument written as a fixed size array actually requires a 
-// pointer to the first element of the array. C compilers are aware of this calling 
+// In C, a function argument written as a fixed size array actually requires a
+// pointer to the first element of the array. C compilers are aware of this calling
 // convention and adjust the call accordingly, but Go cannot. In Go, you must pass
 // the pointer to the first element explicitly: C.f(&C.x[0]).
 //
@@ -86,19 +64,20 @@ type Mutable = C.Mutable
 // dbPtr is a pointer to the state object on the rust side of the simulator
 // key is the key to be used to get the value from the state object
 // this function returns the value grabbed from the state object
-func GetCallbackWrapper(getFuncPtr GetStateCallbackType, dbPtr unsafe.Pointer, key []byte) []byte {
+func (s *SimulatorState) getValue(key []byte) ([]byte, error) {
 	bytesPtr := C.CBytes(key)
 	defer C.free(bytesPtr)
 
 	bytesStruct := C.Bytes{
-		data: (*C.uint8_t)(bytesPtr),
+		data:   (*C.uint8_t)(bytesPtr),
 		length: C.uint(len(key)),
 	}
-	valueBytes := C.bridge_get_callback(getFuncPtr, dbPtr, bytesStruct)
-	return C.GoBytes(unsafe.Pointer(valueBytes.data), C.int(valueBytes.length))
+	valueBytes := C.bridge_get_callback(s.getFunc, s.statePtr, bytesStruct)
+	val := C.GoBytes(unsafe.Pointer(valueBytes.data), C.int(valueBytes.length))
+	return val, nil
 }
 
-func InsertCallbackWrapper(insertFuncPtr InsertStateCallbackType, db unsafe.Pointer, key []byte, value []byte) {
+func (s *SimulatorState) insert(key []byte, value []byte) error {
 	// this will malloc
 	keyBytes := C.CBytes(key)
 	defer C.free(keyBytes)
@@ -107,28 +86,28 @@ func InsertCallbackWrapper(insertFuncPtr InsertStateCallbackType, db unsafe.Poin
 	defer C.free(valueBytes)
 
 	keyStruct := C.Bytes{
-		data: (*C.uint8_t)(keyBytes),
+		data:   (*C.uint8_t)(keyBytes),
 		length: C.uint(len(key)),
 	}
 
 	valueStruct := C.Bytes{
-		data: (*C.uint8_t)(valueBytes),
+		data:   (*C.uint8_t)(valueBytes),
 		length: C.uint(len(value)),
 	}
-	fmt.Println("Inserting key blinggg: ", key)
-	fmt.Println("Inserting key blinggg: ", value)
-	C.bridge_insert_callback(insertFuncPtr, db, keyStruct, valueStruct)
-	fmt.Println("Inserted key: ", key)
+
+	C.bridge_insert_callback(s.insertFunc, s.statePtr, keyStruct, valueStruct)
+	return nil
 }
 
-func RemoveCallbackWrapper(removeCallbackPtr RemoveStateCallbackType, db unsafe.Pointer, key []byte) {
+func (s *SimulatorState) remove(key []byte) error {
 	keyBytes := C.CBytes(key)
 	defer C.free(keyBytes)
 
 	keyStruct := C.Bytes{
-		data: (*C.uint8_t)(keyBytes),
+		data:   (*C.uint8_t)(keyBytes),
 		length: C.uint(len(key)),
 	}
 
-	C.bridge_remove_callback(removeCallbackPtr, db, keyStruct)
+	C.bridge_remove_callback(s.removeFunc, s.statePtr, keyStruct)
+	return nil
 }
