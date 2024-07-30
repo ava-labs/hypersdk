cd881e027712ce2dfa5d643860259516c1f24ded
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-07-30 08:50:12 +0000

    faucet and rpc hosts

--- examples/morpheusvm/frontend/web_wallet/src/const.ts
+++ examples/morpheusvm/frontend/web_wallet/src/const.ts
@@ -1,6 +1,9 @@
 export const SNAP_ID = 'local:http://localhost:8080'
-export const DEVELOPMENT_MODE = true
+export const DEVELOPMENT_MODE = window.location.hostname === 'localhost'
 export const HRP = 'morpheus'
 export const COIN_SYMBOL = "RED"
 export const DECIMAL_PLACES = 9
 export const MAX_TRANSFER_FEE = 10000000n
+
+export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ''
+export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ''
