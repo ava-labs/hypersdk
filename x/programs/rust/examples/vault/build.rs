55d0bea52d096d2b6cc7be20e68625392099ead1
Francois Hardrouyere <hello@franfran.dev>
2024-07-30 14:26:46 +0200

    build token first then vault

--- x/programs/rust/examples/vault/build.rs
+++ x/programs/rust/examples/vault/build.rs
@@ -1,21 +1,28 @@
 use std::path::Path;
 
 fn main() {
-    wasmlanche_sdk::build::build_wasm_on_test();
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let package_name = std::env::var("CARGO_PKG_NAME").unwrap();
     let path = Path::new(&manifest_dir)
         .canonicalize()
         .expect("failed to canonicalize path");
     let token_package_name = "token";
     let token_manifest_dir = path
         .parent()
-        .and_then(|p| Some(p.join(token_package_name)))
+        .map(|p| p.join(token_package_name))
         .expect("failed to find token manifest path");
+
     // # Safety
     // This is safe to call in a single-threaded program
     unsafe {
         std::env::set_var("CARGO_MANIFEST_DIR", token_manifest_dir);
         std::env::set_var("CARGO_PKG_NAME", token_package_name);
+    }
+    wasmlanche_sdk::build::build_wasm_on_test();
+
+    unsafe {
+        std::env::set_var("CARGO_MANIFEST_DIR", manifest_dir);
+        std::env::set_var("CARGO_PKG_NAME", package_name);
     }
     wasmlanche_sdk::build::build_wasm_on_test();
 }
