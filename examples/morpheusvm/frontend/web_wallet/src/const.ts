7aea0119b5c4e8de9e623c837a487d49a57b1fca
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-07-30 09:42:33 +0000

    publish snap

--- examples/morpheusvm/frontend/web_wallet/src/const.ts
+++ examples/morpheusvm/frontend/web_wallet/src/const.ts
@@ -1,5 +1,4 @@
-export const SNAP_ID = 'local:http://localhost:8080'
-export const DEVELOPMENT_MODE = typeof window === 'undefined' || window.location.hostname === 'localhost'
+export const DEVELOPMENT_MODE = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 export const HRP = 'morpheus'
 export const COIN_SYMBOL = "RED"
 export const DECIMAL_PLACES = 9
@@ -7,3 +6,5 @@ export const MAX_TRANSFER_FEE = 10000000n
 
 export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ''
 export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ''
+
+export const SNAP_ID = DEVELOPMENT_MODE ? 'local:http://localhost:8080' : 'npm:sample-metamask-snap-for-hypersdk'
