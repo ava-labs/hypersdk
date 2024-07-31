3e4b7a8411bf3f13cf2bb798dfaf0773cddd42c6
Francois Hardrouyere <hello@franfran.dev>
2024-07-31 21:01:41 +0200

    basic snapshot managment

--- testvm/testvm.go
+++ testvm/testvm.go
@@ -4,6 +4,8 @@
 package testvm
 
 import (
+	"errors"
+
 	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/vm"
 	"github.com/ava-labs/hypersdk/workers"
@@ -18,9 +20,37 @@ const (
 	Trigger
 )
 
-type Snapshot struct{}
+var (
+	UnknownSnapshot = errors.New("this snapshot doesn't exist")
+)
+
+type Env struct {
+	currentBlock chain.StatelessBlock
+}
+
+func (e *Env) Height() uint64 {
+	return e.currentBlock.Height()
+}
+
+func (e *Env) Timestamp() int64 {
+	return e.currentBlock.Timestamp().Unix()
+}
+
+func (e *Env) SetHeight(height uint64) {
+	e.currentBlock.Hght = height
+}
+
+func (e *Env) SetTime(timestamp int64) {
+	e.currentBlock.Tmstmp = timestamp
+}
+
+type Snapshot struct {
+	Env
+}
 
 type TestVM struct {
+	Env
+
 	snapshots map[uint64]Snapshot
 }
 
@@ -37,11 +67,36 @@ func (vm *TestVM) SetTimestamp(timestamp uint64) {
 }
 
 func (vm *TestVM) SnapshotSave() uint64 {
-	return 0
+	var index uint64
+	for i := range vm.snapshots {
+		if _, ok := vm.snapshots[i]; ok {
+			index = i
+			break
+		}
+	}
+	return index
 }
 
-func (vm *TestVM) SnapshotRevert(id uint64) {
-	//
+func (vm *TestVM) SnapshotDelete(id uint64) error {
+	_, ok := vm.snapshots[id]
+	if !ok {
+		return UnknownSnapshot
+	}
+
+	delete(vm.snapshots, id)
+
+	return nil
+}
+
+func (vm *TestVM) SnapshotRevert(id uint64) error {
+	snapshot, ok := vm.snapshots[id]
+	if !ok {
+		return UnknownSnapshot
+	}
+
+	vm.Env = snapshot.Env
+
+	return nil
 }
 
 // VM methods
