d7ec6e44800f8e77da30f83598ac45ce7c87e5c0
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-07-31 11:43:40 +0000

    restore original logic

--- chain/action_marshal.go
+++ chain/action_marshal.go
@@ -11,8 +11,57 @@ import (
 
 func marshalValue(p *codec.Packer, v reflect.Value, kind reflect.Kind, typ reflect.Type) ([]byte, error) {
 	switch kind {
-	case reflect.Uint8:
-		p.PackByte(byte(v.Uint()))
+	case reflect.Struct:
+		info := getTypeInfo(typ)
+		for _, fi := range info {
+			field := v.Field(fi.index)
+			_, err := marshalValue(p, field, fi.kind, fi.typ)
+			if err != nil {
+				return nil, err
+			}
+		}
+	case reflect.Slice:
+		if typ.Elem().Kind() == reflect.Uint8 {
+			if v.IsNil() || v.Len() == 0 {
+				p.PackBytes(nil)
+			} else {
+				p.PackBytes(v.Bytes())
+			}
+		} else {
+			p.PackInt(v.Len())
+			for i := 0; i < v.Len(); i++ {
+				_, err := marshalValue(p, v.Index(i), typ.Elem().Kind(), typ.Elem())
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	case reflect.Map:
+		p.PackInt(v.Len())
+		for _, key := range v.MapKeys() {
+			_, err := marshalValue(p, key, typ.Key().Kind(), typ.Key())
+			if err != nil {
+				return nil, err
+			}
+			_, err = marshalValue(p, v.MapIndex(key), typ.Elem().Kind(), typ.Elem())
+			if err != nil {
+				return nil, err
+			}
+		}
+	case reflect.Int:
+		p.PackInt64(int64(v.Int()))
+	case reflect.Int8:
+		p.PackByte(byte(v.Int()))
+	case reflect.Int16, reflect.Int32:
+		p.PackInt(int(v.Int()))
+	case reflect.Int64:
+		p.PackInt64(v.Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32:
+		p.PackInt(int(v.Uint()))
+	case reflect.Uint64:
+		p.PackUint64(v.Uint())
+	case reflect.String:
+		p.PackString(v.String())
 	case reflect.Bool:
 		if v.Bool() {
 			p.PackByte(1)
@@ -20,7 +69,14 @@ func marshalValue(p *codec.Packer, v reflect.Value, kind reflect.Kind, typ refle
 			p.PackByte(0)
 		}
 	default:
-		return nil, fmt.Errorf("unsupported field type: %v", kind)
+		if typ == reflect.TypeOf(codec.Address{}) {
+			if v.Interface().(codec.Address) == codec.EmptyAddress {
+				return nil, fmt.Errorf("packer does not support empty addresses")
+			}
+			p.PackAddress(v.Interface().(codec.Address))
+		} else {
+			return nil, fmt.Errorf("unsupported field type: %v", kind)
+		}
 	}
 
 	return p.Bytes(), nil
@@ -46,13 +102,73 @@ func UnmarshalAction(data []byte, item interface{}) error {
 
 func unmarshalValue(r *codec.Packer, v reflect.Value, kind reflect.Kind, typ reflect.Type) error {
 	switch kind {
-	case reflect.Uint8:
-		v.SetUint(uint64(r.UnpackByte()))
+	case reflect.Struct:
+		info := getTypeInfo(typ)
+		for _, fi := range info {
+			field := v.Field(fi.index)
+			err := unmarshalValue(r, field, fi.kind, fi.typ)
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Slice:
+		if typ.Elem().Kind() == reflect.Uint8 {
+			var bytes []byte
+			r.UnpackBytes(-1, false, &bytes)
+			v.SetBytes(bytes)
+		} else {
+			length := r.UnpackInt(false)
+			slice := reflect.MakeSlice(typ, length, length)
+			for i := 0; i < length; i++ {
+				err := unmarshalValue(r, slice.Index(i), typ.Elem().Kind(), typ.Elem())
+				if err != nil {
+					return err
+				}
+			}
+			v.Set(slice)
+		}
+	case reflect.Map:
+		length := r.UnpackInt(false)
+		m := reflect.MakeMap(typ)
+		for i := 0; i < length; i++ {
+			key := reflect.New(typ.Key()).Elem()
+			err := unmarshalValue(r, key, typ.Key().Kind(), typ.Key())
+			if err != nil {
+				return err
+			}
+			value := reflect.New(typ.Elem()).Elem()
+			err = unmarshalValue(r, value, typ.Elem().Kind(), typ.Elem())
+			if err != nil {
+				return err
+			}
+			m.SetMapIndex(key, value)
+		}
+		v.Set(m)
+	case reflect.Int:
+		v.SetInt(r.UnpackInt64(false))
+	case reflect.Int8:
+		v.SetInt(int64(r.UnpackByte()))
+	case reflect.Int16, reflect.Int32:
+		v.SetInt(int64(r.UnpackInt(false)))
+	case reflect.Int64:
+		v.SetInt(r.UnpackInt64(false))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32:
+		v.SetUint(uint64(r.UnpackInt(false)))
+	case reflect.Uint64:
+		v.SetUint(r.UnpackUint64(false))
+	case reflect.String:
+		v.SetString(r.UnpackString(false))
 	case reflect.Bool:
 		b := r.UnpackByte()
 		v.SetBool(b != 0)
 	default:
-		return fmt.Errorf("unsupported field type: %v", kind)
+		if typ == reflect.TypeOf(codec.Address{}) {
+			var addr codec.Address
+			r.UnpackAddress(&addr)
+			v.Set(reflect.ValueOf(addr))
+		} else {
+			return fmt.Errorf("unsupported field type: %v", kind)
+		}
 	}
 
 	if r.Err() != nil {
--- chain/action_marshal_test.go
+++ chain/action_marshal_test.go
@@ -1,166 +1,377 @@
-package chain
+package chain_test
 
 import (
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/codec"
 	"github.com/ava-labs/hypersdk/consts"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMarshalUnmarshalAction(t *testing.T) {
-	type TestStruct struct {
-		Uint8Field uint8 `json:"uint8Field"`
-		BoolField  bool  `json:"boolField"`
+func requireNoErrorFast(tb testing.TB, err error) {
+	if err != nil {
+		tb.Fatal(err)
+	}
+}
+
+func TestMarshalTransfer(t *testing.T) {
+	type testStructure struct {
+		// To is the recipient of the [Value].
+		To codec.Address `json:"to"`
+
+		// Amount are transferred to [To].
+		Value uint64 `json:"value"`
+
+		// Optional message to accompany transaction.
+		Memo []byte `json:"memo"`
 	}
 
-	test := TestStruct{
-		Uint8Field: 8,
-		BoolField:  true,
+	transfer := testStructure{
+		To:    codec.Address{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		Value: 12876198273671286,
+		Memo:  []byte("Hello World"),
 	}
 
-	bytes, err := MarshalAction(test)
-	require.NoError(t, err)
+	p := codec.NewWriter(0, consts.NetworkSizeLimit)
+	//this is a copy of actions.Transfer.Marshal() logic
+	p.PackAddress(transfer.To)
+	p.PackUint64(transfer.Value)
+	p.PackBytes(transfer.Memo)
+	expectedBytes := p.Bytes()
 
-	var restoredStruct TestStruct
-	err = UnmarshalAction(bytes, &restoredStruct)
-	require.NoError(t, err)
+	actualBytes, err := chain.MarshalAction(transfer)
+	requireNoErrorFast(t, err)
+
+	require.Equal(t, expectedBytes, actualBytes)
+
+	//unmarshal
+	var restoredStruct testStructure
+	err = chain.UnmarshalAction(expectedBytes, &restoredStruct)
+	requireNoErrorFast(t, err)
+
+	require.Equal(t, transfer, restoredStruct)
+}
+
+func TestMarshalNegativeInts(t *testing.T) {
+	type NegativeIntStructure struct {
+		NegInt   int   `json:"negInt"`
+		NegInt8  int8  `json:"negInt8"`
+		NegInt16 int16 `json:"negInt16"`
+		NegInt32 int32 `json:"negInt32"`
+		NegInt64 int64 `json:"negInt64"`
+	}
+
+	test := NegativeIntStructure{
+		NegInt:   -42,
+		NegInt8:  -8,
+		NegInt16: -16,
+		NegInt32: -32,
+		NegInt64: -64,
+	}
+
+	bytes, err := chain.MarshalAction(test)
+	requireNoErrorFast(t, err)
+
+	var restoredStruct NegativeIntStructure
+	err = chain.UnmarshalAction(bytes, &restoredStruct)
+	requireNoErrorFast(t, err)
+
+	require.Equal(t, test, restoredStruct)
+}
+func TestMarshalFlatTypes(t *testing.T) {
+	type FlatStructure struct {
+		IntField       int           `json:"intField"`
+		Int8Field      int8          `json:"int8Field"`
+		Int16Field     int16         `json:"int16Field"`
+		Int32Field     int32         `json:"int32Field"`
+		Int64Field     int64         `json:"int64Field"`
+		UintField      uint          `json:"uintField"`
+		Uint8Field     uint8         `json:"uint8Field"`
+		Uint16Field    uint16        `json:"uint16Field"`
+		Uint32Field    uint32        `json:"uint32Field"`
+		Uint64Field    uint64        `json:"uint64Field"`
+		StringField    string        `json:"stringField"`
+		AddressField   codec.Address `json:"addressField"`
+		ByteArrayField []byte        `json:"byteArrayField"`
+	}
+
+	test := FlatStructure{
+		IntField:       42,
+		Int8Field:      8,
+		Int16Field:     16,
+		Int32Field:     32,
+		Int64Field:     64,
+		UintField:      42,
+		Uint8Field:     8,
+		Uint16Field:    16,
+		Uint32Field:    32,
+		Uint64Field:    64,
+		StringField:    "Test String",
+		AddressField:   codec.Address{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		ByteArrayField: []byte{10, 20, 30},
+	}
+
+	bytes, err := chain.MarshalAction(test)
+	requireNoErrorFast(t, err)
+
+	var restoredStruct FlatStructure
+	err = chain.UnmarshalAction(bytes, &restoredStruct)
+	requireNoErrorFast(t, err)
 
 	require.Equal(t, test, restoredStruct)
 }
 
-func TestMarshalUnmarshalEmptyStruct(t *testing.T) {
-	type EmptyStruct struct{}
+func TestMarshalEmptyFlatTypes(t *testing.T) {
+	type FlatStructure struct {
+		IntField       int    `json:"intField"`
+		Int8Field      int8   `json:"int8Field"`
+		Int16Field     int16  `json:"int16Field"`
+		Int32Field     int32  `json:"int32Field"`
+		Int64Field     int64  `json:"int64Field"`
+		UintField      uint   `json:"uintField"`
+		Uint8Field     uint8  `json:"uint8Field"`
+		Uint16Field    uint16 `json:"uint16Field"`
+		Uint32Field    uint32 `json:"uint32Field"`
+		Uint64Field    uint64 `json:"uint64Field"`
+		StringField    string `json:"stringField"`
+		ByteArrayField []byte `json:"byteArrayField"`
+	}
 
-	test := EmptyStruct{}
+	test := FlatStructure{
+		ByteArrayField: []byte{}, //codec would unmarshal nil to []byte{} anyway
+	}
 
-	bytes, err := MarshalAction(test)
-	require.NoError(t, err)
-	require.Empty(t, bytes)
+	bytes, err := chain.MarshalAction(test)
+	requireNoErrorFast(t, err)
 
-	var restoredStruct EmptyStruct
-	err = UnmarshalAction(bytes, &restoredStruct)
-	require.NoError(t, err)
+	var restoredStruct FlatStructure
+	err = chain.UnmarshalAction(bytes, &restoredStruct)
+	requireNoErrorFast(t, err)
 
 	require.Equal(t, test, restoredStruct)
 }
-
-func TestMarshalUnmarshalUnsupportedType(t *testing.T) {
-	type UnsupportedStruct struct {
-		FloatField float64 `json:"floatField"`
+func TestMarshalStructWithArrayOfStructs(t *testing.T) {
+	type SimpleStruct struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Value uint64 `json:"value"`
 	}
 
-	test := UnsupportedStruct{
-		FloatField: 3.14,
+	type ComplexStruct struct {
+		Title            string                  `json:"title"`
+		Description      string                  `json:"description"`
+		Items            []SimpleStruct          `json:"items"`
+		MapField         map[string]SimpleStruct `json:"mapField"`
+		EmptyTitle       string                  `json:"emptyTitle"`
+		EmptyDescription string                  `json:"emptyDescription"`
+		EmptyItems       []SimpleStruct          `json:"emptyItems"`
+		EmptyMapField    map[string]SimpleStruct `json:"emptyMapField"`
 	}
 
-	_, err := MarshalAction(test)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "unsupported field type")
+	test := ComplexStruct{
+		Title:       "Test Complex Struct",
+		Description: "This is a test of a struct containing an array of other structs",
+		Items: []SimpleStruct{
+			{ID: 1, Name: "Item 1", Value: 100},
+			{ID: 2, Name: "Item 2", Value: 200},
+			{ID: 3, Name: "Item 3", Value: 300},
+		},
+		MapField: map[string]SimpleStruct{
+			"key1": {ID: 4, Name: "Item 4", Value: 400},
+			"key2": {ID: 5, Name: "Item 5", Value: 500},
+		},
+		EmptyTitle:       "",
+		EmptyDescription: "",
+		EmptyItems:       []SimpleStruct{},
+		EmptyMapField:    map[string]SimpleStruct{},
+	}
+
+	bytes, err := chain.MarshalAction(test)
+	requireNoErrorFast(t, err)
+
+	var restoredStruct ComplexStruct
+	err = chain.UnmarshalAction(bytes, &restoredStruct)
+	requireNoErrorFast(t, err)
+
+	require.Equal(t, test, restoredStruct)
+
+	// Additional checks for nested structures
+	require.Equal(t, len(test.Items), len(restoredStruct.Items))
+	for i, item := range test.Items {
+		require.Equal(t, item, restoredStruct.Items[i])
+	}
+
+	require.Equal(t, len(test.MapField), len(restoredStruct.MapField))
+	for key, value := range test.MapField {
+		require.Equal(t, value, restoredStruct.MapField[key])
+	}
+
+	require.Empty(t, restoredStruct.EmptyTitle)
+	require.Empty(t, restoredStruct.EmptyDescription)
+	require.Empty(t, restoredStruct.EmptyItems)
+	require.Empty(t, restoredStruct.EmptyMapField)
 }
 
 func TestMakeSureMarshalUnmarshalIsNotTooSlow(t *testing.T) {
 	type TestStruct struct {
-		Uint8Field uint8
-		BoolField  bool
+		Uint64Field uint64
+		StringField string
+		BytesField  []byte
 	}
 
 	test := TestStruct{
-		Uint8Field: 42,
-		BoolField:  true,
+		Uint64Field: 42,
+		StringField: "Hello, World!",
+		BytesField:  []byte{1, 2, 3, 4, 5},
 	}
 
-	iterations := 100000
+	iterations := 100_000
 
 	// Time chain.MarshalAction and chain.UnmarshalAction
 	start := time.Now()
-	var reflectionBytes []byte
+
 	for i := 0; i < iterations; i++ {
-		bytes, err := MarshalAction(test)
-		require.NoError(t, err)
-		reflectionBytes = bytes
+		bytes, err := chain.MarshalAction(test)
+		requireNoErrorFast(t, err)
 
 		var restored TestStruct
-		err = UnmarshalAction(bytes, &restored)
-		require.NoError(t, err)
+		err = chain.UnmarshalAction(bytes, &restored)
+		requireNoErrorFast(t, err)
 	}
 	reflectionTime := time.Since(start)
 
-	// Time manual packing
-	start = time.Now()
-	var manualBytes []byte
-	for i := 0; i < iterations; i++ {
-		p := codec.NewWriter(0, consts.NetworkSizeLimit)
-		p.PackByte(test.Uint8Field)
-		p.PackByte(boolToByte(test.BoolField))
-		bytes := p.Bytes()
-		manualBytes = bytes
-
-		r := codec.NewReader(bytes, consts.NetworkSizeLimit)
-		var restored TestStruct
-		restored.Uint8Field = r.UnpackByte()
-		restored.BoolField = byteToBool(r.UnpackByte())
-		require.NoError(t, r.Err())
+	// Check if reflectionTime is more than 100ms
+	if reflectionTime > 100*time.Millisecond {
+		t.Fatalf("MarshalAction and UnmarshalAction took too long: %v", reflectionTime)
 	}
-	manualTime := time.Since(start)
 
-	// Compare bytes between the two methods
-	require.Equal(t, manualBytes, reflectionBytes, "Bytes from reflection and manual methods differ")
-
-	// Check if reflection is more than 5x as slow
-	if float64(reflectionTime) > float64(manualTime)*5 {
-		percentage := (float64(reflectionTime)/float64(manualTime) - 1) * 100
-		t.Errorf("%d iterations reflection-based marshal/unmarshal is %.2f%% slower than manual packing, takes %v instead of %v", iterations, percentage, reflectionTime, manualTime)
-	}
+	t.Logf("Reflection time: %v", reflectionTime)
 }
 
+// go test -bench=BenchmarkMarshalUnmarshal -benchmem ./chain
+
 func BenchmarkMarshalUnmarshal(b *testing.B) {
+	type InnerStruct struct {
+		Field1 int32
+		Field2 string
+		Field3 bool
+		Field5 []byte
+	}
+
 	type TestStruct struct {
-		Uint8Field uint8
-		BoolField  bool
+		Uint64Field uint64
+		StringField string
+		BytesField  []byte
+		IntField    int
+		BoolField   bool
+		Uint16Field uint16
+		Int8Field   int8
+		InnerField  []InnerStruct
 	}
 
 	test := TestStruct{
-		Uint8Field: 42,
-		BoolField:  true,
+		Uint64Field: 42,
+		StringField: "Hello, World!",
+		BytesField:  []byte{1, 2, 3, 4, 5},
+		IntField:    -100,
+		BoolField:   true,
+		Uint16Field: 65535,
+		Int8Field:   -128,
+		InnerField: []InnerStruct{
+			{
+				Field1: 12345,
+				Field2: "Inner string",
+				Field3: false,
+				Field5: []byte{6, 7, 8, 9, 10},
+			},
+			{
+				Field1: 222,
+				Field2: "Inner string 2",
+				Field3: true,
+				Field5: []byte{11, 12, 13, 14, 15},
+			},
+		},
 	}
 
-	b.Run("Reflection", func(b *testing.B) {
+	type Transfer struct {
+		To    codec.Address `json:"to"`
+		Value uint64        `json:"value"`
+		Memo  []byte        `json:"memo"`
+	}
+
+	transfer := Transfer{
+		To:    codec.Address{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		Value: 12876198273671286,
+		Memo:  []byte("Hello World"),
+	}
+
+	const iterationsInBatch = 100_000
+	const numWorkers = 8
+
+	runParallel := func(b *testing.B, f func()) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			bytes, err := MarshalAction(test)
-			require.NoError(b, err)
-			var restored TestStruct
-			err = UnmarshalAction(bytes, &restored)
-			require.NoError(b, err)
+			var wg sync.WaitGroup
+			wg.Add(numWorkers)
+			for w := 0; w < numWorkers; w++ {
+				go func() {
+					defer wg.Done()
+					for j := 0; j < iterationsInBatch/numWorkers; j++ {
+						f()
+					}
+				}()
+			}
+			wg.Wait()
 		}
+	}
+
+	b.Run("Reflection-Manual-Complex", func(b *testing.B) {
+		runParallel(b, func() {
+			bytes, err := chain.MarshalAction(test)
+			requireNoErrorFast(b, err)
+			var restored TestStruct
+			err = chain.UnmarshalAction(bytes, &restored)
+			requireNoErrorFast(b, err)
+		})
 	})
 
-	b.Run("Manual", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
+	b.Run("Reflection-Reflection-Complex", func(b *testing.B) {
+		runParallel(b, func() {
+			bytes, err := chain.MarshalAction(test)
+			requireNoErrorFast(b, err)
+			var restored TestStruct
+			err = chain.UnmarshalAction(bytes, &restored)
+			requireNoErrorFast(b, err)
+		})
+	})
+
+	b.Run("Reflection-Manual-Transfer", func(b *testing.B) {
+		runParallel(b, func() {
 			p := codec.NewWriter(0, consts.NetworkSizeLimit)
-			p.PackByte(test.Uint8Field)
-			p.PackByte(boolToByte(test.BoolField))
+			p.PackAddress(transfer.To)
+			p.PackUint64(transfer.Value)
+			p.PackBytes(transfer.Memo)
 			bytes := p.Bytes()
 
 			r := codec.NewReader(bytes, consts.NetworkSizeLimit)
-			var restored TestStruct
-			restored.Uint8Field = r.UnpackByte()
-			restored.BoolField = byteToBool(r.UnpackByte())
-			require.NoError(b, r.Err())
-		}
+			var restored Transfer
+			r.UnpackAddress(&restored.To)
+			restored.Value = r.UnpackUint64(false)
+			r.UnpackBytes(-1, false, &restored.Memo)
+			requireNoErrorFast(b, r.Err())
+		})
 	})
-}
 
-func boolToByte(b bool) byte {
-	if b {
-		return 1
-	}
-	return 0
-}
-
-func byteToBool(b byte) bool {
-	return b != 0
+	b.Run("Reflection-Reflection-Transfer", func(b *testing.B) {
+		runParallel(b, func() {
+			bytes, err := chain.MarshalAction(transfer)
+			requireNoErrorFast(b, err)
+			var restored Transfer
+			err = chain.UnmarshalAction(bytes, &restored)
+			requireNoErrorFast(b, err)
+		})
+	})
 }
