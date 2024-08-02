d7e3b3da0868d962f93fc386d08d6a96121b7e7f
samliok <samliok@stanford.edu>
2024-08-02 12:02:37 -0400

    build file comments

--- x/programs/simulator/build.rs
+++ x/programs/simulator/build.rs
@@ -3,8 +3,10 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-// build the go library via
+// builds the go library 
 // go build -buildmode=c-shared -o libsimulator.so ffi.go
+// generates the bindings for the C header file
+// writes the bindings to the $OUT_DIR/bindings.rs file
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let output = Path::new(&dir).join("libsimulator.so");
