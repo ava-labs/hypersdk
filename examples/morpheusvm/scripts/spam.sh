cfefb2d7b0114d06aeb484d2b5c16060d41aba32
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-03 23:28:38 +0000

    add log saving

--- examples/morpheusvm/scripts/spam.sh
+++ examples/morpheusvm/scripts/spam.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
+# See the file LICENSE for licensing terms.
 
-set -exu
+set -e
 
-go run ./cmd/morpheus-cli/ chain import-anr
+go run ./cmd/morpheus-cli/ chain import-anr; 
 
+date=$(date +%Y-%m-%d-%H-%M-%S)
+
+# Run the command and output to both the log file and console
 go run ./cmd/morpheus-cli/ spam run ed25519 \
     --accounts=20000 \
     --txs-per-second=10000 \
@@ -11,4 +16,4 @@ go run ./cmd/morpheus-cli/ spam run ed25519 \
     --step-size=50 \
     --s-zipf=1.01 \
     --v-zipf=2.7 \
-    --conns-per-host=10
+    --conns-per-host=10 2>&1 | tee "spam-${date}.log"
