e8331109a1b480a47d6f410e63bf7021f0a63cd8
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-08-08 15:30:07 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- examples/morpheusvm/cmd/morpheusvm/main.go
+++ examples/morpheusvm/cmd/morpheusvm/main.go
@@ -8,13 +8,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ava-labs/avalanchego/database/memdb"
+	"github.com/ava-labs/avalanchego/trace"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/utils/ulimit"
 	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
 	"github.com/spf13/cobra"
 
+	"github.com/ava-labs/hypersdk/api/indexer"
+	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/examples/morpheusvm/cmd/morpheusvm/version"
+	lconsts "github.com/ava-labs/hypersdk/examples/morpheusvm/consts"
 	"github.com/ava-labs/hypersdk/examples/morpheusvm/controller"
+	lrpc "github.com/ava-labs/hypersdk/examples/morpheusvm/rpc"
+	"github.com/ava-labs/hypersdk/rpc"
+	"github.com/ava-labs/hypersdk/vm"
 )
 
 var rootCmd = &cobra.Command{
@@ -43,11 +51,55 @@ func main() {
 }
 
 func runFunc(*cobra.Command, []string) error {
-	if err := ulimit.Set(ulimit.DefaultFDLimit, logging.NoLog{}); err != nil {
+	logFactory := logging.NewFactory(logging.Config{
+		DisplayLevel: logging.Debug,
+	})
+
+	log, err := logFactory.Make("main")
+	if err != nil {
+		return fmt.Errorf("failed to initialize log: %w", err)
+	}
+
+	if err := ulimit.Set(ulimit.DefaultFDLimit, log); err != nil {
 		return fmt.Errorf("%w: failed to set fd limit correctly", err)
 	}
 
-	controller, err := controller.New()
+	txDBIndexer := indexer.NewTxDBIndexer(memdb.New())
+	indexerFactory := indexer.NewSubscriptionFactory(txDBIndexer)
+	indexerAPI := indexer.NewAPIFactory(txDBIndexer, "morpheusvm", "indexer")
+
+	server, handler := rpc.NewWebSocketServer(
+		log,
+		trace.Noop,
+		lconsts.ActionRegistry,
+		lconsts.AuthRegistry,
+		10000000,
+	)
+
+	webSocketFactory := rpc.NewPubSubFactory(handler)
+	removeTxSubscription := rpc.SubscriptionFuncFactory[vm.TxRemovedEvent]{
+		AcceptF: func(event vm.TxRemovedEvent) error {
+			return server.RemoveTx(event.TxID, event.Err)
+		},
+	}
+
+	acceptBlockSubscription := &rpc.SubscriptionFuncFactory[*chain.StatelessBlock]{
+		AcceptF: func(event *chain.StatelessBlock) error {
+			return server.AcceptBlock(event)
+		},
+	}
+
+	controller, err := controller.New(
+		vm.WithBlockSubscriptions[*controller.Controller](indexerFactory, acceptBlockSubscription),
+		vm.WithRemoveTxSubscriptions[*controller.Controller](removeTxSubscription),
+		vm.WithVMAPIs[*controller.Controller](
+			rpc.JSONRPCServerFactory{},
+			indexerAPI,
+			webSocketFactory,
+		),
+		vm.WithControllerAPIs[*controller.Controller](&lrpc.JSONRPCServerFactory{}),
+	)
+
 	if err != nil {
 		return fmt.Errorf("failed to initialize controller: %w", err)
 	}
