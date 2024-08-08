a526ed9e1d36727c73a00cb08faf5f740e1ff061
Francois Hardrouyere <hello@franfran.dev>
2024-08-08 16:34:23 +0800

    remove registry

--- examples/morpheusvm/rpc/jsonrpc_client.go
+++ examples/morpheusvm/rpc/jsonrpc_client.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 
-	_ "github.com/ava-labs/hypersdk/examples/morpheusvm/registry" // ensure registry populated
-
 	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/examples/morpheusvm/consts"
 	"github.com/ava-labs/hypersdk/examples/morpheusvm/genesis"
