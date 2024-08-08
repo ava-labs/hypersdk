dcb7454dd0112e377af44a8286ca26de6fc1093b
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-08 11:21:41 +0900

    types names and IDs

--- codec/abigen_test.go
+++ codec/abigen_test.go
@@ -1,4 +1,4 @@
-package codec
+package codec_test
 
 import (
 	"encoding/json"
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ava-labs/hypersdk/codec"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,8 +16,14 @@ type ABIField struct {
 	Type string `json:"type"`
 }
 
-func GetABIString(t reflect.Type) ([]byte, error) {
-	var result map[string][]ABIField = make(map[string][]ABIField)
+type HavingTypeId interface {
+	GetTypeID() uint8
+}
+
+func GetABIString(action HavingTypeId) ([]byte, error) {
+	t := reflect.TypeOf(action)
+
+	var resultTypes map[string][]ABIField = make(map[string][]ABIField)
 
 	typesleft := []reflect.Type{t}
 	typesAlreadyProcessed := make(map[reflect.Type]bool)
@@ -40,11 +47,16 @@ func GetABIString(t reflect.Type) ([]byte, error) {
 			return nil, err
 		}
 
-		result[nextType.Name()] = fields
+		resultTypes[nextType.Name()] = fields
 		typesleft = append(typesleft, moreTypes...)
 
 		typesAlreadyProcessed[nextType] = true
 	}
+
+	var result map[string]interface{} = make(map[string]interface{})
+	result["id"] = action.GetTypeID()
+	result["name"] = t.Name()
+	result["types"] = resultTypes
 
 	return json.MarshalIndent(result, "", "  ")
 }
@@ -89,96 +101,153 @@ func describeStruct(t reflect.Type) ([]ABIField, []reflect.Type, error) { //refl
 	return fields, otherStructsSeen, nil
 }
 
-func TestGetABI(t *testing.T) {
+type Struct1 struct {
+	Field1 string
+	Field2 int32
+}
+
+func (s Struct1) GetTypeID() uint8 {
+	return 1
+}
+
+func TestGetABIBasic(t *testing.T) {
 	require := require.New(t)
 
-	type Struct1 struct {
-		Field1 string
-		Field2 int32
-	}
-
-	abiString, err := GetABIString(reflect.TypeOf(Struct1{}))
+	abiString, err := GetABIString(Struct1{})
 	require.NoError(err)
 	require.JSONEq(`
-		{
-				"Struct1": [
-					{ "name": "Field1", "type": "string" },
-					{ "name": "Field2", "type": "int32" }
-			]
-		}
+{
+	"id": 1,
+	"name": "Struct1",
+	"types": {
+		"Struct1": [
+			{ "name": "Field1", "type": "string" },
+			{ "name": "Field2", "type": "int32" }
+		]
+	}
+}
+	`, string(abiString))
+}
+
+type Transfer struct {
+	To    codec.Address `json:"to"`
+	Value uint64        `json:"value"`
+	Memo  []byte        `json:"memo,omitempty"`
+}
+
+func (t Transfer) GetTypeID() uint8 {
+	return 2
+}
+
+func TestGetABITransfer(t *testing.T) {
+	require := require.New(t)
+
+	abiString, err := GetABIString(Transfer{})
+	require.NoError(err)
+
+	require.JSONEq(`
+{
+	"id": 2,
+	"name": "Transfer",
+	"types": {
+		"Transfer": [
+			{ "name": "to", "type": "Address" },
+			{ "name": "value", "type": "uint64" },
+			{ "name": "memo", "type": "[]uint8" }
+		]
+	}
+}
 	`, string(abiString))
 
-	type Transfer struct {
-		To    Address `json:"to"`
-		Value uint64  `json:"value"`
-		Memo  []byte  `json:"memo,omitempty"`
-	}
+}
 
-	abiString, err = GetABIString(reflect.TypeOf(Transfer{}))
+type AllInts struct {
+	Int8   int8
+	Int16  int16
+	Int32  int32
+	Int64  int64
+	Uint8  uint8
+	Uint16 uint16
+	Uint32 uint32
+	Uint64 uint64
+}
+
+func (a AllInts) GetTypeID() uint8 {
+	return 3
+}
+
+func TestGetABIAllInts(t *testing.T) {
+	require := require.New(t)
+
+	abiString, err := GetABIString(AllInts{})
 	require.NoError(err)
 
 	require.JSONEq(`
-		{
-				"Transfer": [
-					{ "name": "to", "type": "Address" },
-					{ "name": "value", "type": "uint64" },
-					{ "name": "memo", "type": "[]uint8" }
-				]
-		}
+{
+	"id": 3,
+	"name": "AllInts",
+	"types": {
+		"AllInts": [
+			{ "name": "Int8", "type": "int8" },
+			{ "name": "Int16", "type": "int16" },
+			{ "name": "Int32", "type": "int32" },
+			{ "name": "Int64", "type": "int64" },
+			{ "name": "Uint8", "type": "uint8" },
+			{ "name": "Uint16", "type": "uint16" },
+			{ "name": "Uint32", "type": "uint32" },
+			{ "name": "Uint64", "type": "uint64" }
+		]
+	}
+}
 	`, string(abiString))
+}
 
-	type AllInts struct {
-		Int8   int8
-		Int16  int16
-		Int32  int32
-		Int64  int64
-		Uint8  uint8
-		Uint16 uint16
-		Uint32 uint32
-		Uint64 uint64
-	}
+type InnerStruct struct {
+	Field1 string
+	Field2 uint64
+}
 
-	abiString, err = GetABIString(reflect.TypeOf(AllInts{}))
+type OuterStruct struct {
+	SingleItem InnerStruct   `json:"single_item"`
+	ArrayItems []InnerStruct `json:"array_items"`
+}
+
+func (o OuterStruct) GetTypeID() uint8 {
+	return 4
+}
+
+func TestGetABIOuterStruct(t *testing.T) {
+	require := require.New(t)
+
+	abiString, err := GetABIString(OuterStruct{})
 	require.NoError(err)
 
 	require.JSONEq(`
-		{
-				"AllInts": [
-					{ "name": "Int8", "type": "int8" },
-					{ "name": "Int16", "type": "int16" },
-					{ "name": "Int32", "type": "int32" },
-					{ "name": "Int64", "type": "int64" },
-					{ "name": "Uint8", "type": "uint8" },
-					{ "name": "Uint16", "type": "uint16" },
-					{ "name": "Uint32", "type": "uint32" },
-					{ "name": "Uint64", "type": "uint64" }
-				]
-		}
-	`, string(abiString))
-
-	type InnerStruct struct {
-		Field1 string
-		Field2 uint64
-	}
-
-	type OuterStruct struct {
-		SingleItem InnerStruct   `json:"single_item"`
-		ArrayItems []InnerStruct `json:"array_items"`
-	}
-
-	abiString, err = GetABIString(reflect.TypeOf(OuterStruct{}))
-	require.NoError(err)
-
-	require.JSONEq(`
-		{
-				"OuterStruct": [
-					{ "name": "single_item", "type": "InnerStruct" },
-					{ "name": "array_items", "type": "[]InnerStruct" }
-				],
-				"InnerStruct": [
-					{ "name": "Field1", "type": "string" },
-					{ "name": "Field2", "type": "uint64" }
-				]
-		}
+{
+    "id": 4,
+    "name": "OuterStruct",
+    "types": {
+        "OuterStruct": [
+            {
+                "name": "single_item",
+                "type": "InnerStruct"
+            },
+            {
+                "name": "array_items",
+                "type": "[]InnerStruct"
+            }
+        ],
+        "InnerStruct": [
+            {
+                "name": "Field1",
+                "type": "string"
+            },
+            {
+                "name": "Field2",
+                "type": "uint64"
+            }
+        ]
+    }
+}
 	`, string(abiString))
 }
