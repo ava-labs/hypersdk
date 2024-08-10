182558e26fd6fa252ccc2efe864f15d7edcbb27a
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-10 13:30:33 +0900

    all numeric types

--- codec/auto_marshal_abi_spec_test.go
+++ codec/auto_marshal_abi_spec_test.go
@@ -66,70 +66,13 @@ func (s MockActionTransfer) GetTypeID() uint8 {
 	return 2
 }
 
-func TestMarshalEmptySpec(t *testing.T) {
-	require := require.New(t)
-
-	var err error
-
-	action1Instance := MockActionSingleNumber{
-		Field1: 0,
-	}
-	structJSON, err := json.Marshal(action1Instance)
-	require.NoError(err)
-
-	// This JSON will also be an input in TypeScript
-	expectedStructJSON := `
-	{
-		"Field1": 0
-	}`
-	require.JSONEq(expectedStructJSON, string(structJSON))
-
-	// This is the output of the combination of above JSONs
-	actionPacker := codec.NewWriter(action1Instance.Size(), consts.NetworkSizeLimit)
-	codec.AutoMarshalStruct(actionPacker, action1Instance)
-	require.NoError(actionPacker.Err())
-
-	actionDigest := actionPacker.Bytes()
-
-	require.Equal("0000", hex.EncodeToString(actionDigest))
-
-}
-
-func TestMarshalSimpleSpec(t *testing.T) {
-	require := require.New(t)
-
-	var err error
-
-	action1Instance := MockActionSingleNumber{
-		Field1: 12333,
-	}
-	structJSON, err := json.Marshal(action1Instance)
-	require.NoError(err)
-
-	// This JSON will also be an input in TypeScript
-	expectedStructJSON := `
-	{
-		"Field1": 12333
-	}`
-	require.JSONEq(expectedStructJSON, string(structJSON))
-
-	// This is the output of the combination of above JSONs
-	actionPacker := codec.NewWriter(action1Instance.Size(), consts.NetworkSizeLimit)
-	codec.AutoMarshalStruct(actionPacker, action1Instance)
-	require.NoError(actionPacker.Err())
-
-	actionDigest := actionPacker.Bytes()
-
-	require.Equal("302d", hex.EncodeToString(actionDigest))
-
-}
-
 func TestABISpec(t *testing.T) {
 	require := require.New(t)
 
 	VMActions := []codec.HavingTypeId{
 		MockActionSingleNumber{},
 		MockActionTransfer{},
+		MockActionAllNumbers{},
 	}
 	abiString, err := codec.GetVmABIString(VMActions)
 	require.NoError(err)
@@ -166,11 +109,165 @@ func TestABISpec(t *testing.T) {
         }
       ]
     }
+  },
+  {
+    "id": 3,
+    "name": "MockActionAllNumbers",
+    "types": {
+      "MockActionAllNumbers": [
+        {
+          "name": "uint8",
+          "type": "uint8"
+        },
+        {
+          "name": "uint16",
+          "type": "uint16"
+        },
+        {
+          "name": "uint32",
+          "type": "uint32"
+        },
+        {
+          "name": "uint64",
+          "type": "uint64"
+        },
+        {
+          "name": "int8",
+          "type": "int8"
+        },
+        {
+          "name": "int16",
+          "type": "int16"
+        },
+        {
+          "name": "int32",
+          "type": "int32"
+        },
+        {
+          "name": "int64",
+          "type": "int64"
+        }
+      ]
+    }
   }
 ]`
 	require.Equal(expectedABI, string(abiString))
 
 	abiHash := sha256.Sum256([]byte(abiString))
-	require.Equal("a92c32c95198ea6539871193f2f45187d89378de0c6bd0095f5ff3b79557f34e", hex.EncodeToString(abiHash[:]))
+	require.Equal("9ca568711bf22f818756c7c552e1aa012ffd728d55ea33241f9636173a532f07", hex.EncodeToString(abiHash[:]))
+
+}
+
+func TestMarshalEmptySpec(t *testing.T) {
+	require := require.New(t)
+
+	var err error
+
+	action1Instance := MockActionSingleNumber{
+		Field1: 0,
+	}
+	structJSON, err := json.Marshal(action1Instance)
+	require.NoError(err)
+
+	// This JSON will also be an input in TypeScript
+	expectedStructJSON := `
+	{
+		"Field1": 0
+	}`
+	require.JSONEq(expectedStructJSON, string(structJSON))
+
+	// This is the output of the combination of above JSONs
+	actionPacker := codec.NewWriter(action1Instance.Size(), consts.NetworkSizeLimit)
+	codec.AutoMarshalStruct(actionPacker, action1Instance)
+	require.NoError(actionPacker.Err())
+
+	actionDigest := actionPacker.Bytes()
+
+	require.Equal("0000", hex.EncodeToString(actionDigest))
+
+}
+
+func TestMarshalSingleNumberSpec(t *testing.T) {
+	require := require.New(t)
+
+	var err error
+
+	action1Instance := MockActionSingleNumber{
+		Field1: 12333,
+	}
+	structJSON, err := json.Marshal(action1Instance)
+	require.NoError(err)
+
+	// This JSON will also be an input in TypeScript
+	expectedStructJSON := `
+	{
+		"Field1": 12333
+	}`
+	require.JSONEq(expectedStructJSON, string(structJSON))
+
+	// This is the output of the combination of above JSONs
+	actionPacker := codec.NewWriter(action1Instance.Size(), consts.NetworkSizeLimit)
+	codec.AutoMarshalStruct(actionPacker, action1Instance)
+	require.NoError(actionPacker.Err())
+
+	actionDigest := actionPacker.Bytes()
+
+	require.Equal("302d", hex.EncodeToString(actionDigest))
+
+}
+
+type MockActionAllNumbers struct {
+	AbstractMockAction
+	Uint8  uint8  `json:"uint8"`
+	Uint16 uint16 `json:"uint16"`
+	Uint32 uint32 `json:"uint32"`
+	Uint64 uint64 `json:"uint64"`
+	Int8   int8   `json:"int8"`
+	Int16  int16  `json:"int16"`
+	Int32  int32  `json:"int32"`
+	Int64  int64  `json:"int64"`
+}
+
+func (s MockActionAllNumbers) GetTypeID() uint8 {
+	return 3
+}
+
+func TestMarshalAllNumbersSpec(t *testing.T) {
+	require := require.New(t)
+
+	action := MockActionAllNumbers{
+		Uint8:  254,
+		Uint16: 65534,
+		Uint32: 4294967294,
+		Uint64: 18446744073709551614,
+		Int8:   -127,
+		Int16:  -32767,
+		Int32:  -2147483647,
+		Int64:  -9223372036854775807,
+	}
+
+	structJSON, err := json.Marshal(action)
+	require.NoError(err)
+
+	expectedStructJSON := `
+	{
+		"uint8": 254,
+		"uint16": 65534,
+		"uint32": 4294967294,
+		"uint64": 18446744073709551614,
+		"int8": -127,
+		"int16": -32767,
+		"int32": -2147483647,
+		"int64": -9223372036854775807
+	}`
+	require.JSONEq(expectedStructJSON, string(structJSON))
+
+	actionPacker := codec.NewWriter(action.Size(), consts.NetworkSizeLimit)
+	codec.AutoMarshalStruct(actionPacker, action)
+	require.NoError(actionPacker.Err())
+
+	actionDigest := actionPacker.Bytes()
+
+	require.Equal("fefffefffffffefffffffffffffffe818001800000018000000000000001", hex.EncodeToString(actionDigest))
 
 }
--- examples/morpheusvm/frontend/web_wallet/src/chain/ABI.test.ts
+++ examples/morpheusvm/frontend/web_wallet/src/chain/ABI.test.ts
@@ -1,6 +1,5 @@
 import { expect, test } from 'vitest'
 import { bytesToHex } from '@noble/hashes/utils'
-import { ABI } from './ABI'
 
 const abiString = `[
   {
@@ -34,6 +33,46 @@ const abiString = `[
         }
       ]
     }
+  },
+  {
+    "id": 3,
+    "name": "MockActionAllNumbers",
+    "types": {
+      "MockActionAllNumbers": [
+        {
+          "name": "uint8",
+          "type": "uint8"
+        },
+        {
+          "name": "uint16",
+          "type": "uint16"
+        },
+        {
+          "name": "uint32",
+          "type": "uint32"
+        },
+        {
+          "name": "uint64",
+          "type": "uint64"
+        },
+        {
+          "name": "int8",
+          "type": "int8"
+        },
+        {
+          "name": "int16",
+          "type": "int16"
+        },
+        {
+          "name": "int32",
+          "type": "int32"
+        },
+        {
+          "name": "int64",
+          "type": "int64"
+        }
+      ]
+    }
   }
 ]`
 
@@ -41,7 +80,7 @@ const abiString = `[
 test('TestABISpec', () => {
   const abi = new ABI(abiString)
   expect(bytesToHex(abi.getHash()))
-    .toBe("a92c32c95198ea6539871193f2f45187d89378de0c6bd0095f5ff3b79557f34e")
+    .toBe("9ca568711bf22f818756c7c552e1aa012ffd728d55ea33241f9636173a532f07")
 })
 
 test('TestMarshalEmptySpec', () => {
@@ -63,3 +102,115 @@ test('TestMarshalSingleNumber', () => {
   expect(bytesToHex(binary))
     .toBe("302d")
 })
+
+test('TestMarshalAllNumbersSpec', () => {
+  const abi = new ABI(abiString)
+  const data = {
+    "uint8": 254,
+    "uint16": 65534,
+    "uint32": 4294967294,
+    "uint64": "18446744073709551614",
+    "int8": -127,
+    "int16": -32767,
+    "int32": -2147483647,
+    "int64": "-9223372036854775807"
+  }
+  const binary = abi.getActionBinary("MockActionAllNumbers", data)
+  expect(bytesToHex(binary))
+    .toBe("fefffefffffffefffffffffffffffe818001800000018000000000000001")
+})
+
+import { sha256 } from '@noble/hashes/sha256';
+
+type ABIField = {
+  name: string
+  type: string
+}
+
+type SingleActionABI = {
+  id: number
+  name: string
+  types: Record<string, ABIField[]>
+}
+
+export class ABI {
+  private abi: SingleActionABI[]
+
+  constructor(private abiString: string) {
+    this.abi = JSON.parse(abiString)
+  }
+
+  getHash(): Uint8Array {
+    return sha256(this.abiString)
+  }
+
+  getActionBinary(actionName: string, data: Record<string, unknown>): Uint8Array {
+    const actionABI = this.abi.find(abi => abi.name === actionName)
+    if (!actionABI) throw new Error(`No action ABI found: ${actionName}`)
+
+    const structABI = actionABI.types[actionName]
+    if (!structABI) throw new Error(`No struct ${actionName} found in action ${actionName} ABI`)
+
+    let resultingBinary = new Uint8Array()
+    for (const field of structABI) {
+      const value = data[field.name]
+      const fieldBinary = encodeField(field.type, value)
+      resultingBinary = new Uint8Array([...resultingBinary, ...fieldBinary])
+    }
+
+    return resultingBinary
+  }
+}
+
+function encodeField(type: string, value: unknown): Uint8Array {
+  const bigValue = BigInt(value as string)
+  let buffer: ArrayBuffer
+  let dataView: DataView
+
+  switch (type) {
+    case "uint8":
+      buffer = new ArrayBuffer(1)
+      dataView = new DataView(buffer)
+      dataView.setUint8(0, Number(bigValue))
+      break
+    case "uint16":
+      buffer = new ArrayBuffer(2)
+      dataView = new DataView(buffer)
+      dataView.setUint16(0, Number(bigValue), false)
+      break
+    case "uint32":
+      buffer = new ArrayBuffer(4)
+      dataView = new DataView(buffer)
+      dataView.setUint32(0, Number(bigValue), false)
+      break
+    case "uint64":
+      buffer = new ArrayBuffer(8)
+      dataView = new DataView(buffer)
+      dataView.setBigUint64(0, bigValue, false)
+      break
+    case "int8":
+      buffer = new ArrayBuffer(1)
+      dataView = new DataView(buffer)
+      dataView.setInt8(0, Number(bigValue))
+      break
+    case "int16":
+      buffer = new ArrayBuffer(2)
+      dataView = new DataView(buffer)
+      dataView.setInt16(0, Number(bigValue), false)
+      break
+    case "int32":
+      buffer = new ArrayBuffer(4)
+      dataView = new DataView(buffer)
+      dataView.setInt32(0, Number(bigValue), false)
+      break
+    case "int64":
+      buffer = new ArrayBuffer(8)
+      dataView = new DataView(buffer)
+      dataView.setBigInt64(0, bigValue, false)
+      break
+    default:
+      throw new Error(`Type ${type} marshaling is not implemented yet`)
+  }
+
+  return new Uint8Array(buffer)
+}
