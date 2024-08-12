4c25d0f9e5d4fec98a0e9104be534c03390321a1
samliok <samliok@stanford.edu>
2024-08-12 14:03:41 -0400

    morph tests

--- scripts/mock.gen.sh
+++ scripts/mock.gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+# Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
 set -e
