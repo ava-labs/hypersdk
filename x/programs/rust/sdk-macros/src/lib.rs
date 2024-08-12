d0ffd1a3baa7d49e37d2f827937b4ea96f930f6a
Richard Pringle <richard.pringle@avalabs.org>
2024-08-12 15:50:09 -0400

    Dev-container for find-out Friday

--- x/programs/rust/sdk-macros/src/lib.rs
+++ x/programs/rust/sdk-macros/src/lib.rs
@@ -287,6 +287,39 @@ impl Parse for KeyPair {
     }
 }
 
+/// A procedural macro that generates a state schema for a program.
+/// ```
+/// # use wasmlanche_sdk::{state_schema, Address};
+/// #
+/// state_schema! {
+///     /// Unit-struct style key with a cardinality of 1
+///     Key1 => u32,
+///     /// Newtype wrapper style key with a cardinality of N
+///     /// where N is the carginality of the inner type
+///     Key2(Address) => u32,
+///     /// Tuple-struct style key with a cardinality of M * N
+///     /// where M and N are the cardinalities of the inner types (can be the same)
+///     Allowance(Address, Address) => u32,
+/// }
+/// ```
+///
+/// The above example will create the following types: `Key1`, `Key2`, and `Allowance`.
+///
+/// `Key1` is a [ZST (zero sized type)](https://doc.rust-lang.org/nomicon/exotic-sizes.html#zero-sized-types-zsts).
+///
+/// `Key2` is a [newtype wrapper](https://doc.rust-lang.org/book/ch19-04-advanced-types.html#using-the-newtype-pattern-for-type-safety-and-abstraction) around `Address`.
+///
+/// `Allowance` is a tuple struct with two fields, both of type `Address`
+/// and can be used similar manner to a composite key in a relational database.
+///
+/// All Keys must be stack values and pass the rules for implementing the `bytemuck::NoUninit` trait.
+///
+/// The types to the right of the `=>` are the value types associated with the given key.
+/// The macro does not create types here, they have to be valid types already and have to implement the `BorshSerialize` and `BorshDeserialize` traits.
+///
+/// Each pair is used to automatically generate a `Schema` implementation for the key type with the value-type ass the associated `Schema::Value`.
+/// For now, the keys are prefixed with `u8` to prevent collisions. This will likely change in the future, but that means you
+/// absolutely should not use [state_schema!] more than one for a particular program.
 #[proc_macro]
 pub fn state_schema(input: TokenStream) -> TokenStream {
     let key_pairs =
