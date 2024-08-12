28784440b1161969996fba9ab1b98486f20784d7
samliok <samliok@stanford.edu>
2024-08-12 14:06:07 -0400

    go license

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+# Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
 # Set the CGO flags to use the portable version of BLST
