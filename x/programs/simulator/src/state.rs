9d3113626798ed00701ec5ac8c9dc239d9b3742c
Sam Liokumovich <65994425+samliok@users.noreply.github.com>
2024-08-13 08:20:06 -0400

    Simulator Interactions via FFI (#1205)
    * remove client + builder structs
    * break out context
    * token program tests
    * reformat tests
    * making progress on simple.Mutable
    * ok cool, now need to return bytes
    * length + ptr into one struct
    * full get state method
    * mutable interface
    * insert
    * remove function
    * clean up state interface code
    * errors on state
    * errors on get, not the best way
    * program manager interface outline
    * finished with the programStateManager interface
    * simulator struct + create program
    * cleanup execute and create
    * counter example working
    * token example working
    * remove old simulator
    * counter tests working
    * external counter tests
    * bindgen
    * build go library inside the cargo.toml
    * build file comments
    * remove execution request
    * simplify types
    * pass fuel back to simulator
    * update gitignore
    * cleanup state.rs
    * more descriptive errors
    * Put the `.so` file in the target directory (#1254)
    * Put the `.so` file in the target directory
    * Delete simulator main.rs
    * Remove types and wrap generated code in a mod
    * set the build search directory
    * fix dangling ptr bug
    * Inject state into the simulator (#1288)
    * Inject state into the simulator
    * Use references instead of pointers
    
    ---------
    
    Signed-off-by: Sam Liokumovich <65994425+samliok@users.noreply.github.com>
    Co-authored-by: Richard Pringle <richard.pringle@avalabs.org>

---
+++ x/programs/simulator/src/state.rs
@@ -0,0 +1,100 @@
+use crate::bindings::{Bytes, BytesWithError};
+use libc::c_char;
+use std::{collections::HashMap, ffi::CString};
+
+// define constant error messages
+// TODO: Would love a less-hardcodey way of representing errors between rust <-> go
+pub const ERR_NOT_FOUND: &str = "not found";
+
+#[repr(transparent)]
+pub struct SimpleState {
+    state: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+impl SimpleState {
+    pub fn new() -> SimpleState {
+        SimpleState {
+            state: HashMap::new(),
+        }
+    }
+    pub fn get_value(&self, key: &[u8]) -> Option<&Vec<u8>> {
+        self.state.get(key)
+    }
+
+    pub fn insert(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        self.state.insert(key, value);
+    }
+
+    pub fn remove(&mut self, key: Vec<u8>) {
+        self.state.remove(&key);
+    }
+}
+impl Default for SimpleState {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+// We re-define this mutable in rust for more control over the pointer types
+// mute clippy warnings
+#[repr(C)]
+pub struct Mutable<'a> {
+    pub state: &'a mut SimpleState,
+    pub get_state: GetStateCallback,
+    pub insert_state: InsertStateCallback,
+    pub remove_state: RemoveStateCallback,
+}
+
+impl<'a> Mutable<'a> {
+    pub fn new(state: &'a mut SimpleState) -> Self {
+        Mutable {
+            state,
+            get_state: get_state_callback,
+            insert_state: insert_state_callback,
+            remove_state: remove_state_callback,
+        }
+    }
+}
+
+pub extern "C" fn get_state_callback(state: &mut SimpleState, key: Bytes) -> BytesWithError {
+    // let obj = unsafe { &mut *obj_ptr };
+    let value = state.get_value(&key);
+
+    match value {
+        Some(v) => BytesWithError {
+            bytes: Bytes {
+                data: v.as_ptr(),
+                length: v.len() as u32,
+            },
+            error: std::ptr::null(),
+        },
+        None => BytesWithError {
+            bytes: Bytes {
+                data: std::ptr::null_mut(),
+                length: 0,
+            },
+            error: CString::new(ERR_NOT_FOUND).unwrap().into_raw(),
+        },
+    }
+}
+
+pub extern "C" fn insert_state_callback(
+    state: &mut SimpleState,
+    key: Bytes,
+    value: Bytes,
+) -> *const c_char {
+    state.insert(key.to_vec(), value.to_vec());
+    std::ptr::null()
+}
+
+pub extern "C" fn remove_state_callback(state: &mut SimpleState, key: Bytes) -> *const c_char {
+    state.remove(key.to_vec());
+    std::ptr::null()
+}
+
+pub type GetStateCallback =
+    extern "C" fn(simObjectPtr: &mut SimpleState, key: Bytes) -> BytesWithError;
+pub type InsertStateCallback =
+    extern "C" fn(objectPtr: &mut SimpleState, key: Bytes, value: Bytes) -> *const c_char;
+pub type RemoveStateCallback =
+    extern "C" fn(objectPtr: &mut SimpleState, key: Bytes) -> *const c_char;
