0ae0869f981d2ec053f060c13a4b5ca543b12296
samliok <samliok@stanford.edu>
2024-08-14 13:39:04 -0400

    return program

--- x/programs/test/programs/deploy_program/src/lib.rs
+++ x/programs/test/programs/deploy_program/src/lib.rs
@@ -1,9 +1,9 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-use wasmlanche_sdk::{public, Address, Context, Id};
+use wasmlanche_sdk::{public, Address, Context, Id, Program};
 
 #[public]
-pub fn deploy(ctx: &mut Context, program_id: Id) -> Address {
+pub fn deploy(ctx: &mut Context, program_id: Id) -> Program {
     ctx.deploy(program_id, &[])
 }
