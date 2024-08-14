0e6bddfd7484a15ffdf53089685fcce81039736e
samliok <samliok@stanford.edu>
2024-08-14 13:48:15 -0400

    remove unused import

--- x/programs/test/programs/deploy_program/src/lib.rs
+++ x/programs/test/programs/deploy_program/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-use wasmlanche_sdk::{public, Address, Context, Id, Program};
+use wasmlanche_sdk::{public, Context, Id, Program};
 
 #[public]
 pub fn deploy(ctx: &mut Context, program_id: Id) -> Program {
