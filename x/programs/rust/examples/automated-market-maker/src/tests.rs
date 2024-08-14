3152f945d078edb58a62cb7ef9749deff4fe9e0c
samliok <samliok@stanford.edu>
2024-08-14 15:38:48 -0400

    init test

--- x/programs/rust/examples/automated-market-maker/src/tests.rs
+++ x/programs/rust/examples/automated-market-maker/src/tests.rs
@@ -15,6 +15,27 @@ mod tests {
 
     #[test]
     fn amm_init() {
+        let mut state = SimpleState::new();
+        let simulator = Simulator::new(&mut state);
+        let gas = 1000000;
+        let token_path = PROGRAM_PATH
+        .replace("automated_market_maker", "token")
+        .replace("automated_market_maker", "token");
+
+        // Setup the tokens
+        // TODO: would be a good simulator test if we check token_x and token_y ID to be the same
+        let token_x = simulator.create_program(&token_path);
+        let token_y = simulator.create_program(&token_path).program().unwrap();
+        let token_id = token_x.program_id().unwrap();
+        let token_x = token_x.program().unwrap();
+
+        // initialize tokens
+        let result = simulator.call_program(token_x, "init", ("CoinX", "CX"), gas);
+        let result = simulator.call_program(token_x, "init", ("YCoin", "YC"), gas);
+
+        let amm_program = simulator.create_program(PROGRAM_PATH).program().unwrap();
+        let result = simulator.call_program(program, "init", (token_x, token_y, token_id), gas);
+        assert!(!result.has_error(), "Init AMM errored");
     }
 
     #[test]
