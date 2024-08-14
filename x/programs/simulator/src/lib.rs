541368f44eb69710e4edc6ea3fb74ead90202c95
Sam Liokumovich <65994425+samliok@users.noreply.github.com>
2024-08-14 13:43:39 -0400

    Update x/programs/simulator/src/lib.rs
    Co-authored-by: Richard Pringle <richard.pringle@avalabs.org>
    Signed-off-by: Sam Liokumovich <65994425+samliok@users.noreply.github.com>

--- x/programs/simulator/src/lib.rs
+++ x/programs/simulator/src/lib.rs
@@ -17,7 +17,7 @@ mod bindings {
 
         fn deref(&self) -> &Self::Target {
             // # Safety:
-            // These bytes were mallocated by CGo
+            // These bytes were allocated by CGo
             // They are guaranteed to be valid for the length of the slice
             unsafe { std::slice::from_raw_parts(self.data, self.length as usize) }
         }
