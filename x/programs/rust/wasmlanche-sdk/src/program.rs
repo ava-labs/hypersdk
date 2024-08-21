aba66b3477a53f40b30c4396991dc7bca9e9efca
Francois Hardrouyere <hello@franfran.dev>
2024-08-21 09:52:55 +0200

    update doc example

--- x/programs/rust/wasmlanche-sdk/src/program.rs
+++ x/programs/rust/wasmlanche-sdk/src/program.rs
@@ -66,7 +66,7 @@ impl Program {
     /// The caller must ensure that `function_name` + `args` point to valid memory locations.
     /// # Examples
     /// ```no_run
-    /// # use wasmlanche_sdk::{Address, Program};
+    /// # use wasmlanche_sdk::{Address, Program, Gas};
     /// #
     /// # let program_id = [0; Address::LEN];
     /// # let target: Program = borsh::from_slice(&program_id).unwrap();
@@ -74,7 +74,7 @@ impl Program {
     /// let params = borsh::to_vec(&increment).unwrap();
     /// let max_units = 1000000;
     /// let value = 0;
-    /// let has_incremented: bool = target.call_function("increment", &params, max_units, value)?;
+    /// let has_incremented: bool = target.call_function("increment", &params, &Gas::PassAll, value)?;
     /// assert!(has_incremented);
     /// # Ok::<(), wasmlanche_sdk::ExternalCallError>(())
     /// ```
