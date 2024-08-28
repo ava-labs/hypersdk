b9406f3186c8aabd1da83cfd0ddfd9ba7728ebcd
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-28 03:56:36 +0000

    remove unused errors

--- codec/errors.go
+++ codec/errors.go
@@ -6,14 +6,10 @@ package codec
 import "errors"
 
 var (
-	ErrTooManyItems         = errors.New("too many items")
-	ErrDuplicateItem        = errors.New("duplicate item")
-	ErrFieldNotPopulated    = errors.New("field is not populated")
-	ErrInvalidBitset        = errors.New("invalid bitset")
-	ErrIncorrectHRP         = errors.New("incorrect hrp")
-	ErrInsufficientLength   = errors.New("insufficient length")
-	ErrInvalidSize          = errors.New("invalid size")
-	ErrStringTooLong        = errors.New("string length exceeds maximum allowed")
-	ErrUnsupportedFieldType = errors.New("unsupported field type")
-	ErrEmptyAddress         = errors.New("empty codec.Address value is not allowed")
+	ErrTooManyItems       = errors.New("too many items")
+	ErrDuplicateItem      = errors.New("duplicate item")
+	ErrFieldNotPopulated  = errors.New("field is not populated")
+	ErrIncorrectHRP       = errors.New("incorrect hrp")
+	ErrInsufficientLength = errors.New("insufficient length")
+	ErrInvalidSize        = errors.New("invalid size")
 )
