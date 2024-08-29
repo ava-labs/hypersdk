9eb414ad272d2de6937a7f92de2e319ab3f4acfe
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-08-29 16:06:46 -0400

    Get integration test working

--- examples/morpheusvm/tests/integration/integration_test.go
+++ examples/morpheusvm/tests/integration/integration_test.go
@@ -7,18 +7,12 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/hypersdk/auth"
 	"github.com/ava-labs/hypersdk/crypto/ed25519"
-	"github.com/ava-labs/hypersdk/event"
 	"github.com/ava-labs/hypersdk/examples/morpheusvm/controller"
-	"github.com/ava-labs/hypersdk/extension/externalsubscriber"
 	"github.com/ava-labs/hypersdk/tests/integration"
-	"github.com/ava-labs/hypersdk/tests/workload"
-	"github.com/ava-labs/hypersdk/vm"
 
 	lconsts "github.com/ava-labs/hypersdk/examples/morpheusvm/consts"
 	morpheusWorkload "github.com/ava-labs/hypersdk/examples/morpheusvm/tests/workload"
@@ -29,33 +23,11 @@ func TestIntegration(t *testing.T) {
 	ginkgo.RunSpecs(t, "morpheusvm integration test suites")
 }
 
-func generateConfigs(require *require.Assertions) [][]byte {
-	config := vm.NewConfig()
-	config.Config = make(map[string]any)
-	config.Config["externalSubscriber"] = externalsubscriber.Config{
-		Enabled:       true,
-		ServerAddress: "localhost:9001",
-	}
-	cb, err := json.Marshal(config)
-	require.NoError(err)
-
-	cbDefault, err := json.Marshal(vm.NewConfig())
-	require.NoError(err)
-
-	configBytes := make([][]byte, 3)
-	configBytes[0] = cb
-	configBytes[1] = cbDefault
-	configBytes[2] = cbDefault
-
-	return configBytes
-}
-
 var _ = ginkgo.BeforeSuite(func() {
 	require := require.New(ginkgo.GinkgoT())
 	gen, workloadFactory, err := morpheusWorkload.New(0 /* minBlockGap: 0ms */)
 	require.NoError(err)
 
-	parser := controller.NewParser(0, ids.Empty, gen)
 	genesisBytes, err := json.Marshal(gen)
 	require.NoError(err)
 
@@ -64,34 +36,14 @@ var _ = ginkgo.BeforeSuite(func() {
 
 	randomEd25519AuthFactory := auth.NewED25519Factory(randomEd25519Priv)
 
-	logFactory := logging.NewFactory(logging.Config{
-		DisplayLevel: logging.Debug,
-	})
-	log, err := logFactory.Make("integrationTest")
-	require.NoError(err)
-
-	testSubscriber := workload.NewWorkloadTestSubscriber()
-	externalsubscriberServer := externalsubscriber.NewExternalSubscriberServer(
-		log,
-		controller.NewParserFromBytes,
-		[]event.Subscription[*externalsubscriber.ExternalSubscriberSubscriptionData]{testSubscriber},
-	)
-	grpcHandler, err := externalsubscriber.NewGRPCHandler(externalsubscriberServer, log, ":9001")
-	require.NoError(err)
-
-	externalSubscriberWorkload := workload.NewExternalSubscriber(*grpcHandler, testSubscriber)
-	configs := generateConfigs(require)
-
 	// Setup imports the integration test coverage
 	integration.Setup(
 		controller.New,
 		genesisBytes,
-		configs,
 		lconsts.ID,
-		parser,
+		controller.NewParserFromBytes,
 		controller.JSONRPCEndpoint,
 		workloadFactory,
 		randomEd25519AuthFactory,
-		externalSubscriberWorkload,
 	)
 })
