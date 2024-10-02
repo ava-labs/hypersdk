3974523bd039e8c10e379fbee9fdb5d15fa4d391
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-02 18:40:55 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/network_client.go
+++ dsmr/network_client.go
@@ -4,17 +4,8 @@ import (
 	"context"
 
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow/engine/common"
-	"github.com/ava-labs/avalanchego/utils/set"
 )
 
-type Client struct {
-	sender common.AppSender
-
-	requestID uint32
-}
-
-func (c *Client) GetChunk(ctx context.Context, nodeID ids.NodeID, chunkID ids.ID) ([]byte, error) {
-	c.sender.SendAppRequest(ctx, set.Of(nodeID), c.requestID, chunkID[:])
-	return nil, nil
+type Client[T Tx] interface {
+	GetChunk(ctx context.Context, nodeID ids.NodeID, chunkID ids.ID) (Chunk[T], error)
 }
