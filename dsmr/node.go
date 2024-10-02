b198ed3f07a0cccf36050b9108660feba8c5fc48
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-02 18:24:48 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/node.go
+++ dsmr/node.go
@@ -1,13 +1,55 @@
 package dsmr
 
-func New[T Tx](chunkThreshold int) *Node[T] {
+func New[T Tx](mempool Mempool[T]) *Node[T] {
 	return &Node[T]{
-		chunkBuilder: chunkBuilder[T]{
-			threshold: chunkThreshold,
-		},
+		mempool:      mempool,
+		chunkBuilder: chunkBuilder[T]{},
 	}
 }
 
 type Node[T Tx] struct {
-	chunkBuilder[T]
+	mempool          Mempool[T]
+	chunkBuilder     chunkBuilder[T]
+	chunkCertBuilder chunkCertBuilder[T]
+	blockBuilder     blockBuilder[T]
+}
+
+func (n Node[_]) Run(blks chan<- *Block) error {
+	txs := n.mempool.GetTxsChan()
+
+	for {
+		select {
+		case tx := <-txs:
+			chunk, err := n.chunkBuilder.Add(tx, 0)
+			if err != nil {
+				return err
+			}
+
+			if chunk == nil {
+				continue
+			}
+
+			chunkCert := n.chunkCertBuilder.NewCert(chunk)
+			blk := n.blockBuilder.Add(chunkCert)
+			if blk == nil {
+				continue
+			}
+
+			blks <- blk
+		}
+	}
+}
+
+// consumes chunks and aggregates signtures to generate chunk certs
+type chunkCertBuilder[T Tx] struct{}
+
+// TODO implement
+func (c *chunkCertBuilder[T]) NewCert(chunk *Chunk[T]) ChunkCertificate {
+	return ChunkCertificate{}
+}
+
+type blockBuilder[T Tx] struct{}
+
+func (b *blockBuilder[T]) Add(chunk ChunkCertificate) *Block {
+	return &Block{}
 }
