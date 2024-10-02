404e74fcfd1ea061e1108733f336e3c664dc2501
aaronbuchwald <aaron.buchwald56@gmail.com>
2024-10-02 14:07:04 -0400

    Add JSON marshalling for fee dimensions (#1622)

--- fees/dimension_test.go
+++ fees/dimension_test.go
@@ -25,3 +25,21 @@ func TestDimensionsMarshalText(t *testing.T) {
 	require.NoError(parsedDimText.UnmarshalText(dimTextBytes))
 	require.Equal(dim, parsedDimText)
 }
+
+func TestDimensionsMarshalJSON(t *testing.T) {
+	require := require.New(t)
+	var dim Dimensions
+	dim[Bandwidth] = 1
+	dim[Compute] = 2
+	dim[StorageRead] = 3
+	dim[StorageAllocate] = 4
+	dim[StorageWrite] = 5
+
+	dimJSONBytes, err := dim.MarshalJSON()
+	require.NoError(err)
+	require.JSONEq(`{"bandwidth":1,"compute":2,"storageRead":3,"storageAllocate":4,"storageWrite":5}`, string(dimJSONBytes))
+
+	var parsedDimJSON Dimensions
+	require.NoError(parsedDimJSON.UnmarshalJSON(dimJSONBytes))
+	require.Equal(dim, parsedDimJSON)
+}
