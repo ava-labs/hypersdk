d1b0a1af1f81b05b972426667e8d3cc8de5b1de3
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-02 17:38:01 -0400

    rename Backend -> VM
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/dependencies.go
+++ dsmr/dependencies.go
@@ -2,8 +2,8 @@ package dsmr
 
 import "context"
 
-// TODO: implement Backend
-type Backend interface {
+// TODO: implement VM
+type VM interface {
 	Verify(context.Context, *ExecutionBlock) error
 	Accept(context.Context, *ExecutionBlock) error
 	Reject(context.Context, *ExecutionBlock) error
