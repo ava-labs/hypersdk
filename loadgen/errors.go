e18d659c2c151dc0a618f4cbf00f6a6b70a588d6
samliok <samliok@stanford.edu>
2024-10-03 14:49:23 -0400

    remove unused errors

--- loadgen/errors.go
+++ loadgen/errors.go
@@ -3,15 +3,6 @@ package loadgen
 import "errors"
 
 var (
-	// ErrInputEmpty           = errors.New("input is empty")
-	// ErrInputTooLarge        = errors.New("input is too large")
-	// ErrInvalidChoice        = errors.New("invalid choice")
-	// ErrIndexOutOfRange      = errors.New("index out-of-range")
-	// ErrInsufficientBalance  = errors.New("insufficient balance")
-	// ErrDuplicate            = errors.New("duplicate")
-	// ErrNoChains             = errors.New("no available chains")
-	// ErrNoKeys               = errors.New("no available keys")
 	ErrTxFailed = errors.New("tx failed on-chain")
-	// ErrInsufficientAccounts = errors.New("insufficient accounts")
 	ErrInvalidKeyType = errors.New("invalid key type")
 )
