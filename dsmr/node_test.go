c2173758f6f07730b10023c440cf70ca4deb799f
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-03 14:16:11 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/node_test.go
+++ dsmr/node_test.go
@@ -1,6 +1,13 @@
 package dsmr
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/stretchr/testify/require"
+)
 
 // TODO
 // TODO block building heuristics other than # tx/chunks like time
@@ -12,4 +19,28 @@ import "testing"
 //
 // (txsPerChunk=1, chunksPerBlock=2, peer respond) Read tx -> block not built
 // (txsPerChunk=1, chunksPerBlock=2, peer no respond) Read tx -> block not built
-func TestNode(t *testing.T) {}
+func TestNode(t *testing.T) {
+	n := New[tx](client{}, 1)
+
+	require.NoError(t, n.AddTx(tx{}))
+}
+
+var _ Tx = (*tx)(nil)
+
+type tx struct{}
+
+func (t tx) GetID() ids.ID {
+	return ids.Empty
+}
+
+func (t tx) GetExpiry() time.Time {
+	return time.Time{}
+}
+
+var _ Client[tx] = (*client)(nil)
+
+type client struct{}
+
+func (c client) GetChunk(context.Context, ids.NodeID, ids.ID) (Chunk[tx], error) {
+	return Chunk[tx]{}, nil
+}
