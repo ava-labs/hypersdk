cb8599a1e1fa73cbe1af4176a4796bf02d64a9c0
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-08 16:48:51 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/node_test.go
+++ dsmr/node_test.go
@@ -20,9 +20,20 @@ import (
 // (txsPerChunk=1, chunksPerBlock=2, peer respond) Read tx -> block not built
 // (txsPerChunk=1, chunksPerBlock=2, peer no respond) Read tx -> block not built
 func TestNode(t *testing.T) {
-	n := New[tx](client{}, 1)
+	n := New[tx](client{}, 3)
+	blks := make(chan Block)
 
-	require.NoError(t, n.AddTx(tx{}))
+	go func() {
+		_ = n.Run(blks)
+	}()
+
+	go func() {
+		require.NoError(t, n.AddTx(tx{}))
+		require.NoError(t, n.AddTx(tx{}))
+		require.NoError(t, n.AddTx(tx{}))
+	}()
+
+	<-blks
 }
 
 var _ Tx = (*tx)(nil)
