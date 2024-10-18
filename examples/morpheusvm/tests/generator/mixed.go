a8add017903ecf2d3f6b08477d3083ad90dec4e2
samliok <samliok@stanford.edu>
2024-10-18 15:49:17 -0400

    lint mixed auth generator

--- examples/morpheusvm/tests/generator/mixed.go
+++ examples/morpheusvm/tests/generator/mixed.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ava-labs/hypersdk/api/jsonrpc"
 	"github.com/ava-labs/hypersdk/auth"
 	"github.com/ava-labs/hypersdk/chain"
-	"github.com/ava-labs/hypersdk/codec"
 	"github.com/ava-labs/hypersdk/crypto/bls"
 	"github.com/ava-labs/hypersdk/crypto/ed25519"
 	"github.com/ava-labs/hypersdk/crypto/secp256r1"
@@ -25,10 +24,10 @@ import (
 var _ workload.TxGenerator = (*multiAuthTxGenerator)(nil)
 
 type multiAuthTxGenerator struct {
-	txCheckInterval     time.Duration
-	addressAndFactories []addressAndFactory
-	balance             uint64
-	count               int
+	txCheckInterval time.Duration
+	factories       []chain.AuthFactory
+	balance         uint64
+	count           int
 }
 
 func NewMultiAuthTxGenerator(key *fixture.Ed25519TestKey, txCheckInterval time.Duration) (workload.TxGenerator, error) {
@@ -36,44 +35,32 @@ func NewMultiAuthTxGenerator(key *fixture.Ed25519TestKey, txCheckInterval time.D
 	if err != nil {
 		return nil, err
 	}
-	blsPub := bls.PublicFromPrivateKey(blsPriv)
-	blsAddr := auth.NewBLSAddress(blsPub)
 	blsFactory := auth.NewBLSFactory(blsPriv)
 
 	secpPriv, err := secp256r1.GeneratePrivateKey()
 	if err != nil {
 		return nil, err
 	}
-	secpPub := secpPriv.PublicKey()
-	secpAddr := auth.NewSECP256R1Address(secpPub)
 	secpFactory := auth.NewSECP256R1Factory(secpPriv)
+	ed25519Factory := auth.NewED25519Factory(key.PrivKey)
 
-	factories := []addressAndFactory{
-		{address: key.Addr, authFactory: auth.NewED25519Factory(key.PrivKey)},
-		{address: blsAddr, authFactory: blsFactory},
-		{address: secpAddr, authFactory: secpFactory},
-	}
+	factories := []chain.AuthFactory{ed25519Factory, blsFactory, secpFactory}
 
 	return &multiAuthTxGenerator{
-		txCheckInterval:     txCheckInterval,
-		addressAndFactories: factories,
-		balance:             fixture.InitialBalance,
+		txCheckInterval: txCheckInterval,
+		factories:       factories,
+		balance:         fixture.InitialBalance,
 	}, nil
-}
-
-type addressAndFactory struct {
-	address     codec.Address
-	authFactory chain.AuthFactory
 }
 
 func (g *multiAuthTxGenerator) GenerateTx(ctx context.Context, uri string) (*chain.Transaction, workload.TxAssertion, error) {
 	cli := jsonrpc.NewJSONRPCClient(uri)
 	lcli := vm.NewJSONRPCClient(uri)
 	defer func() { g.count++ }()
-	if g.count >= len(g.addressAndFactories) {
+	if g.count >= len(g.factories) {
 		g.count = 0
 	}
-	sender := g.addressAndFactories[g.count]
+	sender := g.factories[g.count]
 	other, err := ed25519.GeneratePrivateKey()
 	if err != nil {
 		return nil, nil, err
@@ -92,7 +79,7 @@ func (g *multiAuthTxGenerator) GenerateTx(ctx context.Context, uri string) (*cha
 			To:    receiver,
 			Value: 1,
 		}},
-		sender.authFactory,
+		sender,
 	)
 	if err != nil {
 		return nil, nil, err
