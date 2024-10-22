0edb07009cb65a20c2c79d2bcdd08d3669413f97
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-10-22 11:01:13 -0400

    wip
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- dsmr/p2p.go
+++ dsmr/p2p.go
@@ -18,10 +18,7 @@ import (
 	"github.com/ava-labs/hypersdk/proto/pb/dsmr"
 )
 
-var (
-	_ p2p.Handler = (*GetChunkHandler[Tx])(nil)
-	_ p2p.Handler = (*GetChunkSignatureHandler[Tx])(nil)
-)
+var _ p2p.Handler = (*GetChunkHandler[Tx])(nil)
 
 type GetChunkHandler[T Tx] struct {
 	storage *chunkStorage[T]
@@ -123,108 +120,11 @@ func (c GetChunkClient[T]) GetChunk(
 	})
 }
 
+// TODO rename, this is a storage type
 type ChunkSignature struct {
 	ChunkID    ids.ID
 	ProducerID ids.NodeID
 	Expiry     time.Time
 	Signer     [bls.PublicKeyLen]byte
 	Signature  [bls.SignatureLen]byte
-}
-
-// Receives a chunk, persists it, signs it, and replies w/ a signature
-// Producer sends chunks to peers for replication + collect signatures
-type GetChunkSignatureHandler[T Tx] struct {
-	storage *chunkStorage[T]
-}
-
-func (c *GetChunkSignatureHandler[_]) AppGossip(context.Context, ids.NodeID, []byte) {
-	return
-}
-
-func (c *GetChunkSignatureHandler[T]) AppRequest(
-	_ context.Context,
-	_ ids.NodeID,
-	_ time.Time,
-	appRequestBytes []byte,
-) ([]byte, *common.AppError) {
-	request := &dsmr.GetChunkSignatureRequest{}
-	if err := proto.Unmarshal(appRequestBytes, request); err != nil {
-		panic(err)
-	}
-
-	chunk, err := parseChunkProto[T](request.Chunk)
-	if err != nil {
-		panic(err)
-	}
-
-	//TODO persist + sign
-	//TODO conflicting?
-	signature, err := c.storage.VerifyRemoteChunk(chunk)
-	if err != nil {
-		panic(err)
-	}
-
-	p := wrappers.Packer{MaxSize: consts.NetworkSizeLimit}
-	if err := codec.LinearCodec.MarshalInto(signature, &p); err != nil {
-		panic(err)
-	}
-
-	response := &dsmr.GetChunkSignatureResponse{
-		Signature: &dsmr.ChunkSignature{
-			ChunkId:   chunk.id[:],
-			Producer:  chunk.Producer[:],
-			Expiry:    chunk.Expiry,
-			Signer:    nil,
-			Signature: nil,
-		},
-	}
-
-	responseBytes, err := proto.Marshal(response)
-	if err != nil {
-		panic(err)
-	}
-
-	return responseBytes, nil
-}
-
-type ChunkSignatureClient[T Tx] struct {
-	client *p2p.Client
-}
-
-func (c ChunkSignatureClient[T]) GetChunkSignature(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	chunk Chunk[T],
-	onSignatureResponse func(context.Context, ChunkSignature, error),
-) error {
-	request := dsmr.GetChunkSignatureRequest{}
-
-	requestBytes, err := proto.Marshal(&request)
-	if err != nil {
-		return err
-	}
-
-	return c.client.AppRequest(ctx, set.Of(nodeID), requestBytes, func(
-		ctx context.Context,
-		nodeID ids.NodeID,
-		responseBytes []byte,
-		err error,
-	) {
-
-		response := dsmr.GetChunkSignatureResponse{}
-		if err := proto.Unmarshal(responseBytes, &response); err != nil {
-			panic(err)
-		}
-
-		chunkSignature := ChunkSignature{
-			//Producer:    response.Producer,
-			//Expiry:      response.Expiry,
-			//Beneficiary: response.Beneficiary,
-			//Txs:         response.Transactions,
-			//Signer:    response.Signer,
-			//Signature: response.Signature,
-		}
-
-		onSignatureResponse(ctx, chunkSignature, err)
-	})
 }
