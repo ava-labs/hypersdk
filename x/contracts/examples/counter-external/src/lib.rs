661dc9d0fc19b9444af52474e00e970acd1fc699
Richard Pringle <richard.pringle@avalabs.org>
2024-10-23 14:37:36 -0400

    Force test-context to be initialized with an actor

--- x/contracts/examples/counter-external/src/lib.rs
+++ x/contracts/examples/counter-external/src/lib.rs
@@ -35,9 +35,8 @@ mod tests {
 
     #[test]
     fn get_value_exists() {
-        let mut ctx = Context::new();
-        let external = Address::new([0; 33]);
-        let of = Address::new([1; 33]);
+        let [external, of] = [0, 1].map(|i| Address::new([i; 33]));
+        let mut ctx = Context::with_actor(external);
 
         // mock `get_value` external contract call to return `value`
         let value = 5_u64;
