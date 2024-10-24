20ca2a645891dd9bc70edee189d334d8e5ebe825
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-10-24 06:21:05 +0000

    reverse consts

--- consts/consts.go
+++ consts/consts.go
@@ -21,13 +21,14 @@ const (
 	// no more than 50 KiB of overhead but is likely much less)
 	NetworkSizeLimit = 2_044_723 // 1.95 MiB
 
-	MaxUint8              = math.MaxUint8
-	MaxUint16             = math.MaxUint16
+	// FIXME: should use the standard math.MaxUint8, etc.
+	MaxUint8              = ^uint8(0)
+	MaxUint16             = ^uint16(0)
 	MaxUint8Offset        = 7
-	MaxUint               = math.MaxUint
-	MaxInt                = int(math.MaxUint >> 1)
+	MaxUint               = ^uint(0)
+	MaxInt                = int(MaxUint >> 1)
 	MaxUint64Offset       = 63
-	MaxUint64             = math.MaxUint64
+	MaxUint64             = ^uint64(0)
 	MaxFloat64            = math.MaxFloat64
 	MillisecondsPerSecond = 1000
 	Decimals              = 9
