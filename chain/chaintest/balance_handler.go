a3df727b1c62e9e1f1f4f4a86f57d423bdb3a344
Rodrigo Villar <rodrigo.villar@avalabs.org>
2024-10-30 18:11:00 -0400

    nit

--- chain/chaintest/balance_handler.go
+++ chain/chaintest/balance_handler.go
@@ -14,7 +14,7 @@ import (
 	"github.com/ava-labs/hypersdk/state/tstate"
 )
 
-// TestBalanceHandler tests [b] by doing the following:
+// TestBalanceHandler tests b by doing the following:
 //
 // - Creating a new instance of tstate with permissions from SponsorStateKeys().
 //   - The test account balance is initialized to 0.
@@ -26,14 +26,14 @@ import (
 // - Call GetBalance() and require that the test account balance is 0.
 //
 // TestBalanceHandler fails if any of the above method calls returns an error.
-func TestBalanceHandler(t *testing.T, b chain.BalanceHandler) {
+func TestBalanceHandler(ctx context.Context, t *testing.T, b chain.BalanceHandler) {
 	r := require.New(t)
 	addr := codectest.NewRandomAddress()
 	amount := uint64(1)
 
 	// Initialize key-value store for addr
 	store := NewInMemoryStore()
-	r.NoError(b.AddBalance(context.Background(), addr, store, uint64(0), true))
+	r.NoError(b.AddBalance(ctx, addr, store, uint64(0), true))
 
 	stateKeys := b.SponsorStateKeys(addr)
 	ts := tstate.New(1)
@@ -42,15 +42,15 @@ func TestBalanceHandler(t *testing.T, b chain.BalanceHandler) {
 		store.Storage,
 	)
 
-	r.NoError(b.AddBalance(context.Background(), addr, tsv, amount, true))
-	balance, err := b.GetBalance(context.Background(), addr, tsv)
+	r.NoError(b.AddBalance(ctx, addr, tsv, amount, true))
+	balance, err := b.GetBalance(ctx, addr, tsv)
 	r.NoError(err)
 	r.Equal(amount, balance)
 
-	r.NoError(b.CanDeduct(context.Background(), addr, tsv, amount))
+	r.NoError(b.CanDeduct(ctx, addr, tsv, amount))
 
-	r.NoError(b.Deduct(context.Background(), addr, tsv, amount))
-	balance, err = b.GetBalance(context.Background(), addr, tsv)
+	r.NoError(b.Deduct(ctx, addr, tsv, amount))
+	balance, err = b.GetBalance(ctx, addr, tsv)
 	r.NoError(err)
 	r.Equal(uint64(0), balance)
 }
