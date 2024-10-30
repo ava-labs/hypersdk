a8457693b4312aa467743c3bb3062f0328686d25
Rodrigo Villar <rodrigo.villar@avalabs.org>
2024-10-30 13:54:48 -0400

    implement feedback

--- chain/chaintest/balance_handler_test_helpers.go
+++ chain/chaintest/balance_handler_test_helpers.go
@@ -10,100 +10,35 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/hypersdk/chain"
-	"github.com/ava-labs/hypersdk/codec"
-	"github.com/ava-labs/hypersdk/state"
+	"github.com/ava-labs/hypersdk/codec/codectest"
 	"github.com/ava-labs/hypersdk/state/tstate"
 )
 
-const (
-	TestCanDeduct BalanceHandlerMethod = iota
-	TestDeduct
-	TestAddBalance
-)
+// TestBalanceHandler tests [b] by calling the following methods (in order):
+// - SponsorStateKeys()
+// - AddBalance()
+// - CanDeduct()
+func TestBalanceHandler(t *testing.T, b chain.BalanceHandler) {
+	r := require.New(t)
+	addr := codectest.NewRandomAddress()
+	amount := uint64(1)
 
-type BalanceHandlerMethod uint8
+	stateKeys := b.SponsorStateKeys(addr)
+	ts := tstate.New(1)
+	tsv := ts.NewView(
+		stateKeys,
+		NewInMemoryStore().Storage,
+	)
 
-// BalanceHandlerStateKeyTest is a single parameterized test. It calls
-// AddBalance(),
-// CanDeduct(), and
-// Deduct() on [BalanceHandler] in that order and checks that no errors are returned
-type BalanceHandlerStateKeyTest struct {
-	Name string
+	r.NoError(b.AddBalance(context.Background(), addr, tsv, amount, true))
+	balance, err := b.GetBalance(context.Background(), addr, tsv)
+	r.NoError(err)
+	r.Equal(amount, balance)
 
-	BalanceHandler chain.BalanceHandler
+	r.NoError(b.CanDeduct(context.Background(), addr, tsv, amount))
 
-	Actor  codec.Address
-	Amount uint64
-}
-
-// Run executes the [BalanceHandlerStateKeyTest] test and makes sure that no
-// errors are returned
-//
-// Run creates a new state with state keys from SponsorStateKeys()
-func (test *BalanceHandlerStateKeyTest) Run(ctx context.Context, t *testing.T) {
-	t.Run(test.Name, func(t *testing.T) {
-		r := require.New(t)
-
-		ms := NewInMemoryStore()
-
-		// Add balance
-		r.NoError(test.BalanceHandler.AddBalance(ctx, test.Actor, ms, test.Amount, true))
-
-		stateKeys := test.BalanceHandler.SponsorStateKeys(test.Actor)
-		ts := tstate.New(1)
-		tsv := ts.NewView(stateKeys, ms.Storage)
-
-		// Add balance
-		r.NoError(test.BalanceHandler.AddBalance(ctx, test.Actor, tsv, test.Amount, true))
-		// Check if can deduct
-		r.NoError(test.BalanceHandler.CanDeduct(ctx, test.Actor, tsv, test.Amount))
-		// Deduct balance
-		r.NoError(test.BalanceHandler.Deduct(ctx, test.Actor, tsv, test.Amount))
-	})
-}
-
-// BalanceHandlerFundsTest is a single parameterized test. It calls the
-// specified method with the passed parameters and checks that all assertions pass.
-type BalanceHandlerFundsTest struct {
-	Name string
-
-	BalanceHandler chain.BalanceHandler
-
-	Actor         codec.Address
-	Amount        uint64
-	State         state.Mutable
-	CreateAccount bool
-
-	Method          BalanceHandlerMethod
-	ExpectedBalance uint64
-	ExpectedError   error
-
-	Assertion func(context.Context, *testing.T, state.Mutable)
-}
-
-// Run executes the [BalanceHandlerFundsTest] and makes sure all assertions pass.
-func (test *BalanceHandlerFundsTest) Run(ctx context.Context, t *testing.T) {
-	t.Run(test.Name, func(t *testing.T) {
-		r := require.New(t)
-
-		switch test.Method {
-		case TestCanDeduct:
-			r.ErrorIs(test.BalanceHandler.CanDeduct(ctx, test.Actor, test.State, test.Amount), test.ExpectedError)
-		case TestDeduct:
-			r.ErrorIs(test.BalanceHandler.Deduct(ctx, test.Actor, test.State, test.Amount), test.ExpectedError)
-		case TestAddBalance:
-			r.ErrorIs(test.BalanceHandler.AddBalance(ctx, test.Actor, test.State, test.Amount, test.CreateAccount), test.ExpectedError)
-		default:
-			r.Fail("no test method specified")
-		}
-
-		balance, err := test.BalanceHandler.GetBalance(ctx, test.Actor, test.State)
-		r.NoError(err)
-
-		r.Equal(test.ExpectedBalance, balance)
-
-		if test.Assertion != nil {
-			test.Assertion(ctx, t, test.State)
-		}
-	})
+	r.NoError(b.Deduct(context.Background(), addr, tsv, amount))
+	balance, err = b.GetBalance(context.Background(), addr, tsv)
+	r.NoError(err)
+	r.Equal(uint64(0), balance)
 }
--- examples/morpheusvm/storage/state_manager_test.go
+++ examples/morpheusvm/storage/state_manager_test.go
@@ -1,101 +1,14 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package storage_test
+package storage
 
 import (
-	"context"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/ava-labs/hypersdk/chain/chaintest"
-	"github.com/ava-labs/hypersdk/codec/codectest"
-	"github.com/ava-labs/hypersdk/examples/morpheusvm/storage"
-	"github.com/ava-labs/hypersdk/state"
 )
 
-func TestBalanceHandlerStateKeys(t *testing.T) {
-	addr := codectest.NewRandomAddress()
-
-	test := chaintest.BalanceHandlerStateKeyTest{
-		Name:           "SimpleStateKeys",
-		BalanceHandler: &storage.BalanceHandler{},
-		Actor:          addr,
-		Amount:         1,
-	}
-
-	test.Run(context.Background(), t)
-}
-
-func TestBalanceHandlerFunds(t *testing.T) {
-	addr := codectest.NewRandomAddress()
-	r := require.New(t)
-
-	tests := []chaintest.BalanceHandlerFundsTest{
-		{
-			Name:           "Insufficient funds",
-			BalanceHandler: &storage.BalanceHandler{},
-			Actor:          addr,
-			State:          chaintest.NewInMemoryStore(),
-			Amount:         1,
-			Method:         chaintest.TestDeduct,
-			ExpectedError:  storage.ErrInvalidBalance,
-		},
-		{
-			Name:            "SimpleAddBalance",
-			BalanceHandler:  &storage.BalanceHandler{},
-			Actor:           addr,
-			State:           chaintest.NewInMemoryStore(),
-			Amount:          1,
-			CreateAccount:   true,
-			Method:          chaintest.TestAddBalance,
-			ExpectedBalance: uint64(1),
-			ExpectedError:   nil,
-		},
-		{
-			Name:           "SimpleCanDeduct",
-			BalanceHandler: &storage.BalanceHandler{},
-			Actor:          addr,
-			State: func() state.Mutable {
-				s := chaintest.NewInMemoryStore()
-				_, err := storage.AddBalance(
-					context.Background(),
-					s,
-					addr,
-					1,
-					true,
-				)
-				r.NoError(err)
-				return s
-			}(),
-			Method:          chaintest.TestCanDeduct,
-			Amount:          1,
-			ExpectedBalance: 1,
-		},
-		{
-			Name:           "SimpleDeduct",
-			BalanceHandler: &storage.BalanceHandler{},
-			Actor:          addr,
-			State: func() state.Mutable {
-				s := chaintest.NewInMemoryStore()
-				_, err := storage.AddBalance(
-					context.Background(),
-					s,
-					addr,
-					1,
-					true,
-				)
-				r.NoError(err)
-				return s
-			}(),
-			Amount:          uint64(1),
-			Method:          chaintest.TestDeduct,
-			ExpectedBalance: 0,
-		},
-	}
-
-	for _, tt := range tests {
-		tt.Run(context.Background(), t)
-	}
+func TestBalanceHandler(t *testing.T) {
+	chaintest.TestBalanceHandler(t, &BalanceHandler{})
 }
