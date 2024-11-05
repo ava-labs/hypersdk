20ae489c609002bfddeba1db61a6a0293a170272
Rodrigo Villar <rodrigo.villar@avalabs.org>
2024-11-05 11:44:00 -0500

    nit

--- codec/address_test.go
+++ codec/address_test.go
@@ -52,22 +52,20 @@ func TestAddressStringIdentity(t *testing.T) {
 	require.Equal(addr, originalAddr)
 }
 
-func TestAddressToString(t *testing.T) {
-	t.Run("zeroAddressChecksum", func(t *testing.T) {
-		require := require.New(t)
-		expectedStr := "0x000000000000000000000000000000000000000000000000000000000000000000a7396ce9"
-		require.Equal(expectedStr, EmptyAddress.String())
-	})
+func TestZeroAddressToString(t *testing.T) {
+	require := require.New(t)
+	expectedStr := "0x000000000000000000000000000000000000000000000000000000000000000000a7396ce9"
+	require.Equal(expectedStr, EmptyAddress.String())
+}
 
-	t.Run("arbitraryAddressChecksum", func(t *testing.T) {
-		require := require.New(t)
-		expectedStr := "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ab"
-		addr := CreateAddress(
-			byte(0),
-			[32]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
-		)
-		require.Equal(expectedStr, addr.String())
-	})
+func TestAddressToString(t *testing.T) {
+	require := require.New(t)
+	expectedStr := "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ab"
+	addr := CreateAddress(
+		byte(0),
+		[32]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+	)
+	require.Equal(expectedStr, addr.String())
 }
 
 func TestStringToAddress(t *testing.T) {
@@ -75,6 +73,12 @@ func TestStringToAddress(t *testing.T) {
 		byte(0),
 		[32]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
 	)
+	addrStr := "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ab"
+
+	// Remove checksum from address string
+	addrStrWithNoChecksum := addrStr[:len(addrStr)-4]
+	// Manipulate last character of address string
+	addrStrWithBadChecksum := addrStr[:len(addrStr)-1] + "c"
 
 	tests := []struct {
 		name            string
@@ -84,19 +88,19 @@ func TestStringToAddress(t *testing.T) {
 	}{
 		{
 			name:            "simpleChecksummedAddress",
-			inputStr:        "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ab",
+			inputStr:        addrStr,
 			expectedAddress: addr,
 			expectedErr:     nil,
 		},
 		{
 			name:            "simpleChecksummedAddressWithNoPrefix",
-			inputStr:        "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ab",
+			inputStr:        addrStr[2:],
 			expectedAddress: addr,
 			expectedErr:     nil,
 		},
 		{
 			name:            "addressWithNoChecksum",
-			inputStr:        "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
+			inputStr:        addrStrWithNoChecksum,
 			expectedAddress: EmptyAddress,
 			expectedErr:     ErrBadChecksum,
 		},
@@ -108,7 +112,7 @@ func TestStringToAddress(t *testing.T) {
 		},
 		{
 			name:            "addressWithBadChecksum",
-			inputStr:        "0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20a10df6ac",
+			inputStr:        addrStrWithBadChecksum,
 			expectedAddress: EmptyAddress,
 			expectedErr:     ErrBadChecksum,
 		},
