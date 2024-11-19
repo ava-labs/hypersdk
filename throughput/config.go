8e913894969289d279a0a4568941902a7e5e348b
furkan-ux <furkan.boyraz@avalabs.org>
2024-11-20 00:32:04 +0300

    lower numAccounts

--- throughput/config.go
+++ throughput/config.go
@@ -57,7 +57,7 @@ func NewThroughputConfig(uris []string, keyHex string) (*Config, error) {
 		minTxsPerSecond:  2000,
 		txsPerSecondStep: 2000,
 		numClients:       10,
-		numAccounts:      100000,
+		numAccounts:      5000,
 	}, nil
 }
 
