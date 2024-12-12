383a7ef0850b56238fa054c5ba473301c6c14ee8
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-12-12 17:08:36 -0500

    update

--- internal/validitywindow/syncer.go
+++ internal/validitywindow/syncer.go
@@ -39,11 +39,11 @@ func (s *Syncer[Container]) start(ctx context.Context, lastAcceptedBlock Executi
 		seenValidityWindow = false
 		validityWindow     = s.getValidityWindow(lastAcceptedBlock.Timestamp())
 		err                error
-		hasBlock           bool
+		ok                 bool
 	)
 	for {
-		parent, hasBlock, err = s.chainIndex.GetExecutionBlock(ctx, parent.Parent())
-		if err != nil || !hasBlock {
+		parent, ok, err = s.chainIndex.GetExecutionBlock(ctx, parent.Parent())
+		if err != nil || !ok {
 			break // If we can't fetch far enough back or we've gone past genesis, execute what we can
 		}
 		parents = append(parents, parent)
