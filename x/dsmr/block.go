fd96a3841ce8b7a43b3eb8a91122bd1429890fdc
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-12-13 13:26:45 -0500

    undo.

--- x/dsmr/block.go
+++ x/dsmr/block.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/ava-labs/hypersdk/codec"
 	"github.com/ava-labs/hypersdk/consts"
-	"github.com/ava-labs/hypersdk/fees"
 	"github.com/ava-labs/hypersdk/utils"
 )
 
@@ -21,7 +20,6 @@ type Tx interface {
 	GetID() ids.ID
 	GetExpiry() int64
 	GetSponsor() codec.Address
-	GetFees() fees.Dimensions
 }
 
 type UnsignedChunk[T Tx] struct {
