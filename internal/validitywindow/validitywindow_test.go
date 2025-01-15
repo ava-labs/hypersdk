e0a910cfa920353f402a968752bfd250d0834bf9
Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>
2025-01-15 10:43:35 -0500

    Update internal/validitywindow/validitywindow_test.go
    Co-authored-by: aaronbuchwald <aaron.buchwald56@gmail.com>
    Signed-off-by: Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>

--- internal/validitywindow/validitywindow_test.go
+++ internal/validitywindow/validitywindow_test.go
@@ -176,7 +176,6 @@ func TestValidityWindowIsRepeat(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			r := require.New(t)
-			r.NoError(nil)
 
 			chainIndex := &testChainIndex{}
 			tvw := NewTimeValidityWindow(&logging.NoLog{}, trace.Noop, chainIndex)
