dc02119952dc3a52b4ff5385cddb625a361dfc25
aaronbuchwald <aaron.buchwald56@gmail.com>
2025-02-04 10:41:28 -0500

    Remove contracts (#1914)

--- x/contracts/sdk-macros/tests/ui/fail/first-param.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-use sdk_macros::public;
-
-#[public]
-pub fn test(a: u32) {}
-
-fn main() {}
