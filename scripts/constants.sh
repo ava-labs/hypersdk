db2080ad9ec767ad090d10aff81e67e35a23fdc3
Maru Newby <maru.newby@avalabs.org>
2025-03-15 18:31:06 -0700

    [ci] Enable monitoring of the e2e job
    Adds monitoring of avalanchego nodes started by tmpnet
    
    This required:
    
     - adding a flake file to enable the availability of monitoring
     dependencies like prometheus and promtail
    
     - updating how the target avalanchego version is managed to ensure
     consistency across go.mod, flake file and github workflow
    
     - adding bin/tmpnetctl to enable local collectors to be
     started/stopped via --start-collectors/--stop-collectors

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -4,9 +4,42 @@
 # Ignore warnings about variables appearing unused since this file is not the consumer of the variables it defines.
 # shellcheck disable=SC2034
 
-set -euo pipefail
+if [[ -z ${AVALANCHE_VERSION:-} ]]; then
+  # Get module details from go.mod
+  MODULE_DETAILS="$(go list -m "github.com/ava-labs/avalanchego" 2>/dev/null)"
 
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'6e7a7d51954c094e4b748748ad1fb1a36b4e5695'}
+  # Extract the version part
+  AVALANCHE_VERSION="$(echo "${MODULE_DETAILS}" | awk '{print $2}')"
+
+  # Check if the version matches the pattern where the last part is the module hash
+  # v*YYYYMMDDHHMMSS-abcdef123456
+  #
+  # If not, the value is assumed to represent a tag
+  if [[ "${AVALANCHE_VERSION}" =~ ^v.*[0-9]{14}-[0-9a-f]{12}$ ]]; then
+    # Extract module hash from version
+    MODULE_HASH="$(echo "${AVALANCHE_VERSION}" | cut -d'-' -f3)"
+
+    # The first 8 chars of the hash is used as the tag of avalanchego images
+    AVALANCHE_VERSION="${MODULE_HASH::8}"
+
+    # The full SHA is required for versioning the run-monitored-tmpnet-cmd custom
+    # action. It is only done if explicitly requested to avoid unnecessary API
+    # calls.
+    if [[ -n "${SET_FULL_AVALANCHE_VERSION:-}" ]]; then
+      CURL_HEADER=
+      if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+        # Using an auth token avoids being rate limited when run in CI
+        CURL_HEADER="-H 'Authorization: token ${GITHUB_TOKEN}'"
+      fi
+      CURL_URL="https://api.github.com/repos/ava-labs/avalanchego/commits/${MODULE_HASH}"
+      FULL_AVALANCHE_VERSION="$(curl -s "${CURL_HEADER}" "${CURL_URL}" | grep '"sha":' | head -n1 | cut -d'"' -f4)"
+    fi
+  elif [[ -n "${SET_FULL_AVALANCHE_VERSION:-}" ]]; then
+    # Assume AVALANCHEGO_VERSION is a tag.
+    FULL_AVALANCHE_VERSION="${AVALANCHE_VERSION}"
+  fi
+fi
+
 # Optionally specify a separate version of AvalancheGo for building docker images
 # Added to support the case there's no such docker image for the specified commit of AvalancheGo
 AVALANCHE_DOCKER_VERSION=${AVALANCHE_DOCKER_VERSION:-'v1.12.2'}
--- scripts/update_avalanchego_version.sh
+++ scripts/update_avalanchego_version.sh
@@ -4,56 +4,48 @@
 
 set -euo pipefail
 
-# Check if a version argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <version>"
-    exit 1
-fi
-
 if ! [[ "$0" =~ scripts/update_avalanchego_version.sh ]]; then
   echo "must be run from repository root"
   exit 255
 fi
 
-# Function to validate that the first argument matches the XX.XX.XX format
-validate_version_format() {
-    local version=$1
-
-    # Regular expression to match the XX.XX.XX format
-    local regex="^[0-9]+\.[0-9]+\.[0-9]+$"
-
-    if ! [[ $version =~ $regex ]]; then
-        echo "Error: Version must be in XX.XX.XX format."
-        exit 1
-    fi
-}
-
-validate_version_format "$1"
-VERSION=$1
+# If version is not provided, the existing version in go.mod is assumed
+VERSION="${1:-}"
 
 # Function to update version in go.mod and run go get
 update_avalanchego_mod_version() {
     local path=$1
     local version=$2
 
-    # Set the working directory to the provided path and update the AvalancheGo dependency
-    (cd "$path" && go get "github.com/ava-labs/avalanchego@v$version" && go mod tidy)
+    echo "Ensuring AvalancheGo version $version in $path/go.mod"
+    (cd "$path" && go get "github.com/ava-labs/avalanchego@$version" && go mod tidy)
 }
 
-# Function to update the version in the format "VERSION=vXX.XX.XX" in the provided file
-# Intended to run on the given run.sh files for each example VM.
-update_avalanchego_run_version() {
-    local file_path=$1
-    local version=$2
+if [[ -n "${VERSION}" ]]; then
+  echo "Ensuring AvalancheGo version to $VERSION in go.mod"
+  update_avalanchego_mod_version "$PWD" "${VERSION}"
+fi
 
-    # Use sed to find and replace the version in the file
-    # macOS requires an empty string after -i, but this may not work on Linux
-    sed -i '' "s/^VERSION=v[0-9]*\.[0-9]*\.[0-9]*/VERSION=v$version/" "$file_path"
-}
+# - Configure constants.sh to determine the full avalanche SHA if the version is not a tag.
+# - Determination of the full SHA requires querying the github API and is disabled by default
+#   to avoid unnecessary network calls.
+# - A full SHA is required for versioning a github custom action like the run-monitored-tmpnet-cmd
+SET_FULL_AVALANCHE_VERSION=1
 
-# Update version in the root directory
-update_avalanchego_mod_version "$PWD" "$VERSION"
+# Discover AVALANCHE_VERSION and FULL_AVALANCHE_VERSION
+. scripts/constants.sh
 
 # Update AvalancheGo version in examples/morpheusvm
-update_avalanchego_mod_version "$PWD/examples/morpheusvm" "$VERSION"
-update_avalanchego_run_version "$PWD/examples/morpheusvm/scripts/run.sh" "$VERSION"
+update_avalanchego_mod_version "$PWD/examples/morpheusvm" "$AVALANCHE_VERSION"
+
+# Ensure the custom action version matches the avalanche version
+WORKFLOW_PATH=".github/workflows/hypersdk-ci.yml"
+CUSTOM_ACTION="ava-labs/avalanchego/.github/actions/run-monitored-tmpnet-cmd"
+echo "Ensuring AvalancheGo version ${FULL_AVALANCHE_VERSION} in ${WORKFLOW_PATH} for ${CUSTOM_ACTION}"
+sed -i "s|\(uses: ${CUSTOM_ACTION}\)@.*|\1@${FULL_AVALANCHE_VERSION}|g" "${WORKFLOW_PATH}"
+
+# Ensure the flake version is the same as the avalanche version
+FLAKE_DEP="github:ava-labs/avalanchego"
+FLAKE_FILE=flake.nix
+echo "Ensuring AvalancheGo version ${FULL_AVALANCHE_VERSION} for ${FLAKE_DEP} in ${FLAKE_FILE}"
+sed -i "s|\(${FLAKE_DEP}?ref=\).*|\1${FULL_AVALANCHE_VERSION}\";|g" "${FLAKE_FILE}"
