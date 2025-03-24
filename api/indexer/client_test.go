10153190fba410d52a5f059c0ae82bbc026dddf6
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2025-03-24 12:27:56 -0400

    part 1

--- api/indexer/client_test.go
+++ api/indexer/client_test.go
@@ -9,35 +9,46 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/trace"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/hypersdk/api"
+	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/chain/chaintest"
+	"github.com/ava-labs/hypersdk/codec"
 )
 
 func TestIndexerClientBlocks(t *testing.T) {
 	const (
-		numExecutedBlocks = 4
-		blockWindow       = 2
-		numTxs            = 3
+		blockWindow = 2
+		numTxs      = 3
 	)
 
 	testCases := []struct {
-		name      string
-		blkHeight uint64
-		err       error
+		name              string
+		blkHeight         uint64
+		blkHeightErr      error
+		numExecutedBlocks int
 	}{
 		{
-			name:      "success",
-			blkHeight: numExecutedBlocks - 1,
-			err:       nil,
+			name:              "success",
+			blkHeight:         3,
+			blkHeightErr:      nil,
+			numExecutedBlocks: 4,
 		},
 		{
-			name:      "missing block",
-			blkHeight: 0,
-			err:       errBlockNotFound,
+			name:              "missing block",
+			blkHeight:         0,
+			blkHeightErr:      errBlockNotFound,
+			numExecutedBlocks: 4,
+		},
+		{
+			name:              "no blocks",
+			blkHeight:         0,
+			blkHeightErr:      errBlockNotFound,
+			numExecutedBlocks: 0,
 		},
 	}
 
@@ -45,7 +56,7 @@ func TestIndexerClientBlocks(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			r := require.New(t)
 			ctx := context.Background()
-			indexer, executedBlocks, _ := createTestIndexer(t, ctx, numExecutedBlocks, blockWindow, numTxs)
+			indexer, executedBlocks, _ := createTestIndexer(t, ctx, tt.numExecutedBlocks, blockWindow, numTxs)
 
 			jsonHandler, err := api.NewJSONRPCHandler(Name, NewServer(trace.Noop, indexer))
 			r.NoError(err)
@@ -59,26 +70,36 @@ func TestIndexerClientBlocks(t *testing.T) {
 
 			client := NewClient(httpServer.URL)
 
-			expectedBlock := executedBlocks[tt.blkHeight]
-			executedBlock, err := client.GetBlockByHeight(ctx, expectedBlock.Block.Hght, parser)
-			if tt.err == nil {
+			executedBlock, err := client.GetBlockByHeight(ctx, tt.blkHeight, parser)
+			if tt.blkHeightErr == nil {
 				r.NoError(err)
+				expectedBlock := executedBlocks[tt.blkHeight-1]
 				r.Equal(expectedBlock.Block, executedBlock.Block)
 			} else {
-				r.ErrorContains(err, tt.err.Error())
+				r.ErrorContains(err, tt.blkHeightErr.Error())
 			}
 
-			executedBlock, err = client.GetBlock(ctx, expectedBlock.Block.GetID(), parser)
-			if tt.err == nil {
+			expectedBlkID := ids.GenerateTestID()
+			if tt.blkHeight > 0 {
+				expectedBlock := executedBlocks[tt.blkHeight-1]
+				expectedBlkID = expectedBlock.Block.GetID()
+			}
+
+			executedBlock, err = client.GetBlock(ctx, expectedBlkID, parser)
+			if tt.blkHeightErr == nil {
 				r.NoError(err)
-				r.Equal(expectedBlock.Block, executedBlock.Block)
+				r.Equal(executedBlocks[tt.blkHeight-1].Block, executedBlock.Block)
 			} else {
-				r.ErrorContains(err, tt.err.Error())
+				r.ErrorContains(err, tt.blkHeightErr.Error())
 			}
 
 			executedBlock, err = client.GetLatestBlock(ctx, parser)
-			r.NoError(err)
-			r.Equal(executedBlocks[numExecutedBlocks-1].Block, executedBlock.Block)
+			if tt.numExecutedBlocks == 0 {
+				r.ErrorContains(err, database.ErrNotFound.Error())
+			} else {
+				r.NoError(err)
+				r.Equal(executedBlocks[tt.numExecutedBlocks-1].Block, executedBlock.Block)
+			}
 		})
 	}
 }
@@ -167,7 +188,7 @@ func TestIndexerClientWaitForTransaction(t *testing.T) {
 	)
 	r := require.New(t)
 	ctx := context.Background()
-	indexer, _, _ := createTestIndexer(t, ctx, numExecutedBlocks, blockWindow, numTxs)
+	indexer, executedBlocks, _ := createTestIndexer(t, ctx, numExecutedBlocks, blockWindow, numTxs)
 
 	jsonHandler, err := api.NewJSONRPCHandler(Name, NewServer(trace.Noop, indexer))
 	r.NoError(err)
@@ -183,4 +204,79 @@ func TestIndexerClientWaitForTransaction(t *testing.T) {
 	defer ctxCancel()
 	_, _, err = client.WaitForTransaction(timeoutCtx, 1*time.Millisecond, ids.GenerateTestID())
 	r.ErrorIs(err, context.DeadlineExceeded)
+
+	// wait for a past transaction.
+	lastExecutedBlock := executedBlocks[numExecutedBlocks-1]
+	lastTx := lastExecutedBlock.Block.Txs[numTxs-1]
+	lastTxResult := lastExecutedBlock.ExecutionResults.Results[numTxs-1]
+	success, fee, err := client.WaitForTransaction(context.Background(), 1*time.Millisecond, lastTx.GetID())
+	r.NoError(err)
+	r.Equal(lastTxResult.Success, success)
+	r.Equal(lastTxResult.Fee, fee)
+}
+
+func TestIndexerClientMalformedBlock(t *testing.T) {
+	const (
+		numExecutedBlocks = 2
+		blockWindow       = 1
+		numTxs            = 3
+	)
+	r := require.New(t)
+	ctx := context.Background()
+	indexer, executedBlocks, _ := createTestIndexer(t, ctx, numExecutedBlocks, blockWindow, numTxs)
+
+	jsonHandler, err := api.NewJSONRPCHandler(Name, NewServer(trace.Noop, indexer))
+	r.NoError(err)
+
+	httpServer := httptest.NewServer(jsonHandler)
+	t.Cleanup(func() {
+		httpServer.Close()
+	})
+
+	lastExecutedBlock := executedBlocks[numExecutedBlocks-1]
+	// "damage" the last executed block by removing the last result.
+	lastExecutedBlock.ExecutionResults.Results = lastExecutedBlock.ExecutionResults.Results[1:]
+	r.NoError(indexer.Notify(context.Background(), lastExecutedBlock))
+
+	client := NewClient(httpServer.URL)
+
+	txID := lastExecutedBlock.Block.Txs[numTxs-1].GetID()
+	_, _, _, err = client.GetTx(context.Background(), txID, chaintest.NewTestParser())
+	// see that we get the expected error.
+	r.ErrorContains(err, errTxResultNotFound.Error())
+
+	_, _, err = client.GetTxResults(context.Background(), txID)
+	r.ErrorContains(err, errTxResultNotFound.Error())
+
+	_, _, err = client.WaitForTransaction(context.Background(), 1*time.Millisecond, txID)
+	r.ErrorContains(err, errTxResultNotFound.Error())
+}
+
+func TestIndexerClientFailingParser(t *testing.T) {
+	const (
+		numExecutedBlocks = 2
+		blockWindow       = 1
+		numTxs            = 3
+	)
+	r := require.New(t)
+	ctx := context.Background()
+	indexer, executedBlocks, _ := createTestIndexer(t, ctx, numExecutedBlocks, blockWindow, numTxs)
+
+	jsonHandler, err := api.NewJSONRPCHandler(Name, NewServer(trace.Noop, indexer))
+	r.NoError(err)
+
+	httpServer := httptest.NewServer(jsonHandler)
+	t.Cleanup(func() {
+		httpServer.Close()
+	})
+
+	client := NewClient(httpServer.URL)
+	lastExecutedBlock := executedBlocks[numExecutedBlocks-1]
+	lastTx := lastExecutedBlock.Block.Txs[numTxs-1]
+	parser := &chain.TxTypeParser{
+		ActionRegistry: codec.NewTypeParser[chain.Action](),
+		AuthRegistry:   codec.NewTypeParser[chain.Auth](),
+	}
+	_, _, _, err = client.GetTx(context.Background(), lastTx.GetID(), parser)
+	r.ErrorIs(err, errTxUnmarshalingFailed)
 }
