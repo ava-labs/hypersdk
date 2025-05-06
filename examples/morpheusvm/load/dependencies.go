1f02837901526d03beb1fc563959978e7a196bee
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-05-06 15:21:34 +0200

    Narrower tracker interface for morpheusvm load package

--- examples/morpheusvm/load/dependencies.go
+++ examples/morpheusvm/load/dependencies.go
@@ -6,23 +6,8 @@ package load
 import "github.com/ava-labs/avalanchego/ids"
 
 // Tracker keeps track of the status of transactions.
-// This must be thread-safe, so it can be called in parallel by the issuer(s) or orchestrator.
 type Tracker interface {
 	// Issue records a transaction that was submitted, but whose final status is
 	// not yet known.
 	Issue(ids.ID)
-	// ObserveConfirmed records a transaction that was confirmed.
-	ObserveConfirmed(ids.ID)
-	// ObserveFailed records a transaction that failed (e.g. expired)
-	ObserveFailed(ids.ID)
-
-	// GetObservedIssued returns the number of transactions that the tracker has
-	// confirmed were issued.
-	GetObservedIssued() uint64
-	// GetObservedConfirmed returns the number of transactions that the tracker has
-	// confirmed were accepted.
-	GetObservedConfirmed() uint64
-	// GetObservedFailed returns the number of transactions that the tracker has
-	// confirmed failed.
-	GetObservedFailed() uint64
 }
